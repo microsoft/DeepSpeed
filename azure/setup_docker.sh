9b856694e8a85868ae12d0a296725af1a86e143d
Ubuntu <deepspeed@DeepSpeedTest1.ekk3ld5we5ae3bqyhs23c0xy2g.bx.internal.cloudapp.net>
2020-02-07 07:53:40 +0000

    checkout tmp branch

--- azure/setup_docker.sh
+++ azure/setup_docker.sh
@@ -7,5 +7,5 @@ for node_id in `seq 0 $((num_vms - 1))`; do
     ip_addr=`az vm list-ip-addresses | jq .[${node_id}].virtualMachine.network.publicIpAddresses[0].ipAddress | sed 's/"//g'`
     addr=${username}@${ip_addr}
     ssh $addr "docker pull deepspeed/deepspeed:latest"
-    ssh $addr "cd workdir/DeepSpeed; git pull; bash azure/start_container.sh"
+    ssh $addr "cd workdir/DeepSpeed; git pull; git checkout jeffra/azure; bash azure/start_container.sh"
 done
