542f0d551ff329017536d3cac68969a97e541a9d
Jeff Rasley <jerasley@microsoft.com>
2020-02-06 22:25:25 -0800

    update container run

--- azure/start_container.sh
+++ azure/start_container.sh
@@ -8,4 +8,4 @@ docker run -d -t --name $name \
         -v ${HOME}/workdir:/home/deepspeed/workdir \
         -v ${HOME}/.ssh:/home/deepspeed/.ssh \
         -v /job/hostfile:/job/hostfile \
-        --gpus all $image 'sudo service ssh start && bash'
+        --gpus all $image "/bin/bash -c 'sudo service ssh start && sleep infinity'"
