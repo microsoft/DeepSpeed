fecf9fcac3ca32416673fcd837284b67e24992ef
Jeff Rasley <jerasley@microsoft.com>
2020-03-10 21:47:50 -0700

    updates

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ The wheel will be located at: dist/*.whl
 """
 
 import os
+import sys
 import torch
 from deepspeed import __version__ as ds_version
 from setuptools import setup, find_packages
@@ -16,7 +17,6 @@ from torch.utils.cpp_extension import CUDAExtension, BuildExtension
 
 cmdclass = {}
 ext_modules = []
-cmdclass['build_ext'] = BuildExtension
 
 TORCH_MAJOR = int(torch.__version__.split('.')[0])
 TORCH_MINOR = int(torch.__version__.split('.')[1])
@@ -42,17 +42,23 @@ if (TORCH_MAJOR > 1) or (TORCH_MAJOR == 1 and TORCH_MINOR > 4):
     version_ge_1_5 = ['-DVERSION_GE_1_5']
 version_dependent_macros = version_ge_1_1 + version_ge_1_3 + version_ge_1_5
 
-ext_modules.append(
-    CUDAExtension(name='fused_lamb_cuda',
-                  sources=['csrc/fused_lamb_cuda.cpp',
-                           'csrc/fused_lamb_cuda_kernel.cu'],
-                  extra_compile_args={
-                      'cxx': [
-                          '-O3',
-                      ] + version_dependent_macros,
-                      'nvcc': ['-O3',
-                               '--use_fast_math'] + version_dependent_macros
-                  }))
+if "--python_only" in sys.argv:
+    print("[WARNING] Building DeepSpeed without cpp/cuda support!")
+else:
+    print("Building cpp/cuda components...")
+    cmdclass['build_ext'] = BuildExtension
+    ext_modules.append(
+        CUDAExtension(name='fused_lamb_cuda',
+                      sources=['csrc/fused_lamb_cuda.cpp',
+                               'csrc/fused_lamb_cuda_kernel.cu'],
+                      extra_compile_args={
+                          'cxx': [
+                              '-O3',
+                          ] + version_dependent_macros,
+                          'nvcc': ['-O3',
+                                   '--use_fast_math'] + version_dependent_macros
+                      }))
+
 
 setup(name='deepspeed',
       version=ds_version,
