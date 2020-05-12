9d0b194ae9cbe3a8182d098fada06084fee0bf09
Tunji Ruwase <olruwase@microsoft.com>
2020-05-12 00:51:37 +0000

    Fix bug

--- deepspeed/pt/deepspeed_zero_optimizer.py
+++ deepspeed/pt/deepspeed_zero_optimizer.py
@@ -1313,17 +1313,16 @@ class FP16_DeepSpeedZeroOptimizer(object):
         # constructed in the same way as the one whose state_dict we are loading, the same master params
         # are guaranteed to exist, so we can just copy_() from the saved master params.
 
-        saved_partition_count = state_dict[
-            'partition_count'] if 'partition_count' in state_dict else self.partition_count
-        if saved_partition_count != self.partition_count:
+        if 'partition_count' in state_dict and state_dict[
+                'partition_count'] == self.partition_count:
+            # Use option 2
+            for current, saved in zip(self.single_partition_of_fp32_groups, state_dict['single_partition_of_fp32_groups']):
+                current.data.copy_(saved.data)
+        else:
             # Use option 1
             partition_id = dist.get_rank(group=self.dp_process_group)
             for fp16_partitions, fp32_partition in zip(self.parallel_partitioned_fp16_groups, self.single_partition_of_fp32_groups):
                 fp32_partition.data.copy_(fp16_partitions[partition_id].data)
-        else:
-            # Use option 2
-            for current, saved in zip(self.single_partition_of_fp32_groups, state_dict['single_partition_of_fp32_groups']):
-                current.data.copy_(saved.data)
 
 
 def _handle_overflow(cpu_sum, x, i):
