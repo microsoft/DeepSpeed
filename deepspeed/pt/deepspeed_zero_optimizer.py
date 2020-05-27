b0ad5989d18e4202d6ce3f0479c33f7727d52256
Jeff Rasley <jerasley@microsoft.com>
2020-05-27 16:18:24 +0000

    fix asserts

--- deepspeed/pt/deepspeed_zero_optimizer.py
+++ deepspeed/pt/deepspeed_zero_optimizer.py
@@ -156,8 +156,8 @@ class FP16_DeepSpeedZeroOptimizer(object):
         self.postscale_gradients = postscale_gradients
 
         if self.reduce_scatter:
-            assert self.allreduce_always_fp32, "allreduce_always_fp32 is not yet supported with ZeRO-2 with reduce scatter enabled"
-            assert self.gradient_predivide_factor != 1.0, "gradient_predivide_factor != 1.0 is not yet supported with ZeRO-2 with reduce scatter enabled"
+            assert not self.allreduce_always_fp32, "allreduce_always_fp32 is not yet supported with ZeRO-2 with reduce scatter enabled"
+            assert self.gradient_predivide_factor == 1.0, "gradient_predivide_factor != 1.0 is not yet supported with ZeRO-2 with reduce scatter enabled"
             assert not self.postscale_gradients, "pre-scale gradients is not yet supported with ZeRO-2 with reduce scatter enabled"
 
         # param flattened by groups
