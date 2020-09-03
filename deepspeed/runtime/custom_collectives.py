ff002c421f388dc29b93efe58eddd27282b6a65d
Ammar Ahmad Awan <ammar.awan@microsoft.com>
2020-09-03 15:18:56 -0700

    Cleanup custom collectives.

--- deepspeed/runtime/custom_collectives.py
+++ deepspeed/runtime/custom_collectives.py
@@ -1,7 +1,7 @@
 from mpi4py import MPI
 
 
-def myIgather(rank, size, comm, sendbuf, recbuf, root):
+def my_igather(rank, size, comm, sendbuf, recbuf, root):
     req = []
     if rank == root:
         for idx in range(size):
@@ -14,37 +14,6 @@ def myIgather(rank, size, comm, sendbuf, recbuf, root):
     return req
 
 
-def myIgather_new(rank, size, comm, sendbuf, recbuf, root):
-    req = []
-    if rank == root:
-        for idx in range(size):
-            if idx != rank:
-                req.append(comm.Irecv(recbuf[idx], source=idx))
-    else:
-        comm.Send(sendbuf, dest=root)
-    return req
-
-
-def myGather(rank, size, comm, sendbuf, recbuf, root):
-    #req = []
-    req = 1
-    if rank == root:
-        for idx in range(size):
-            if idx != rank:
-                #req.append(comm.Irecv(recbuf[idx], source=idx))
-                req = comm.Irecv(recbuf[idx], source=idx)
-                req.wait()
-            else:
-                recbuf[rank] = sendbuf
-    else:
-        #req.append(comm.Isend(sendbuf, dest=root))
-        #req = comm.Isend(sendbuf, dest=root)
-        comm.Send(sendbuf, dest=root)
-        #req.wait()
-
-    return req
-
-
 def gather_cuda(rank,
                 world_size,
                 comm,
@@ -54,26 +23,24 @@ def gather_cuda(rank,
                 cupy_recvbuf_scale):
     requests = []
     for idx in range(world_size):
-        req_sign = myIgather(rank,
-                             world_size,
-                             comm,
-                             cupy_sign_list_packed[idx],
-                             cupy_recvbuf_sign,
-                             root=idx)
+        req_sign = my_igather(rank,
+                              world_size,
+                              comm,
+                              cupy_sign_list_packed[idx],
+                              cupy_recvbuf_sign,
+                              root=idx)
         requests += req_sign
 
     for idx in range(world_size):
-        req_scale = myIgather(rank,
-                              world_size,
-                              comm,
-                              cupy_worker_scale,
-                              cupy_recvbuf_scale,
-                              root=idx)
+        req_scale = my_igather(rank,
+                               world_size,
+                               comm,
+                               cupy_worker_scale,
+                               cupy_recvbuf_scale,
+                               root=idx)
         requests += req_scale
 
     MPI.Request.Waitall(requests)
-
-    #return cupy_sign_list_packed, cupy_recvbuf_sign, cupy_worker_scale, cupy_recvbuf_scale
 
 
 def gather_host(rank,
@@ -99,28 +66,25 @@ def gather_host(rank,
 
     cupy.cuda.get_current_stream().synchronize()
 
-    #print ("calling igather at rank ", rank, flush=True)
-
     # 2. use numpy buffers for communication
     requests = []
 
     for idx in range(world_size):
-        #print ("igather1 queued at idx, rank, sizes,", idx, rank, cupy_sign_list_packed[idx].size, cupy_recvbuf_sign.size, flush=True)
-        req_sign = myIgather(rank,
-                             world_size,
-                             comm,
-                             numpy_sign_list_packed[idx],
-                             numpy_recvbuf_sign,
-                             root=idx)
+        req_sign = my_igather(rank,
+                              world_size,
+                              comm,
+                              numpy_sign_list_packed[idx],
+                              numpy_recvbuf_sign,
+                              root=idx)
         requests += req_sign
 
     for idx in range(world_size):
-        req_scale = myIgather(rank,
-                              world_size,
-                              comm,
-                              numpy_worker_scale,
-                              numpy_recvbuf_scale,
-                              root=idx)
+        req_scale = my_igather(rank,
+                               world_size,
+                               comm,
+                               numpy_worker_scale,
+                               numpy_recvbuf_scale,
+                               root=idx)
         requests += req_scale
 
     MPI.Request.Waitall(requests)
@@ -134,10 +98,6 @@ def gather_host(rank,
     cupy_recvbuf_scale = cupy.array(numpy_recvbuf_scale)
     cupy.cuda.get_current_stream().synchronize()
 
-    #print("igather and conversion completed at rank ", rank, flush=True)
-
-    #return cupy_sign_list_packed, cupy_recvbuf_sign, cupy_worker_scale, cupy_recvbuf_scale
-
 
 def gather(rank,
            world_size,
@@ -148,7 +108,6 @@ def gather(rank,
            cupy_recvbuf_scale):
     cuda_aware = True
     if cuda_aware:
-        #cupy_sign_list_packed, cupy_recvbuf_sign, cupy_worker_scale, cupy_recvbuf_scale =
         gather_cuda(rank,
                     world_size,
                     comm,
@@ -157,7 +116,6 @@ def gather(rank,
                     cupy_worker_scale,
                     cupy_recvbuf_scale)
     else:
-        #cupy_sign_list_packed, cupy_recvbuf_sign, cupy_worker_scale, cupy_recvbuf_scale =
         gather_host(rank,
                     world_size,
                     comm,
@@ -165,8 +123,6 @@ def gather(rank,
                     cupy_recvbuf_sign,
                     cupy_worker_scale,
                     cupy_recvbuf_scale)
-
-    #return cupy_sign_list_packed, cupy_recvbuf_sign, cupy_worker_scale, cupy_recvbuf_scale
 
 
 def allgather(comm,
@@ -194,12 +150,8 @@ def allgather(comm,
         cupy.cuda.get_current_stream().synchronize()
 
         # 2. Communicate numpy buffers
-
-        #print("allgather 1 called: ", flush=True)
         comm.Allgather(numpy_server_sign_packed, numpy_recvbuf_sign_server)
-        #print("allgather 2 called", flush=True)
         comm.Allgather(numpy_server_scale, numpy_recvbuf_scale_server)
-        #print("allgather 2 finished", flush=True)
         comm.Barrier()
 
         # 3. Convert numpy back to cupy
@@ -208,5 +160,3 @@ def allgather(comm,
         cupy_server_scale = cupy.array(numpy_server_scale)
         cupy_recvbuf_scale_server = cupy.array(numpy_recvbuf_scale_server)
         cupy.cuda.get_current_stream().synchronize()
-
-    #return cupy_server_sign_packed, cupy_recvbuf_sign_server, cupy_server_scale, cupy_recvbuf_scale_server
