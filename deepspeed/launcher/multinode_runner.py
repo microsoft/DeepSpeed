2fdb79d1821ca0df66ccf6991522020259a9bd1f
Jeff Rasley <jerasley@microsoft.com>
2020-09-08 16:29:27 -0700

    cleanup

--- deepspeed/launcher/multinode_runner.py
+++ deepspeed/launcher/multinode_runner.py
@@ -80,9 +80,8 @@ class OpenMPIRunner(MultiNodeRunner):
         self.add_export('UCX_TLS', 'tcp')
 
     def backend_exists(self):
-        #TODO: check for openmpi existance, not just mpirun
         #TODO: if IB is available we should suggestion mvapich
-        return shutil.which('mpirun')
+        return shutil.which('ompi_info')
 
     def get_cmd(self, environment, active_resources):
         #FIXME: Allow for include/exclude at node-level but not gpu-level
@@ -119,17 +118,26 @@ class MVAPICHRunner(MultiNodeRunner):
         super(MVAPICHRunner, self).__init__(args, world_info_base64)
         self.resource_pool = resource_pool
 
-        self.add_export('MV2_USE_GDRCOPY', '0')
+        # Disable the CMA kernel module, not available on Ubuntu systems
         self.add_export('MV2_SMP_USE_CMA', '0')
+
+        # If we fail this will output more verbose logging
         self.add_export('MV2_DEBUG_SHOW_BACKTRACE', '1')
+
+        # Enabled cuda-aware communication
         self.add_export('MV2_USE_CUDA', '1')
+
+        # Support deep learning frameworks: http://hidl.cse.ohio-state.edu/userguide/horovod/
         self.add_export('MV2_SUPPORT_DL', '1')
+
+        # Support MPI_THREAD_MULTIPLE
         self.add_export('MV2_ENABLE_AFFINITY', '0')
+
+        # Performance tuning flags for allgather
         self.add_export('MV2_INTER_ALLGATHER_TUNING', '5')
         self.add_export('MV2_CUDA_USE_NAIVE', '0')
 
     def backend_exists(self):
-        #TODO: check for openmpi existance, not just mpirun
         #TODO: if IB is available we should suggestion mvapich
         mpiname_exists = shutil.which('mpiname')
         exists = False
