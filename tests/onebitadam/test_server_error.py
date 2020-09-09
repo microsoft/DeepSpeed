efe71b47839cf3a72b856b90bf69e612f49f902e
Ammar Ahmad Awan <ammar.awan@microsoft.com>
2020-09-09 01:33:50 +0000

    Fix formatting.

--- tests/onebitadam/test_server_error.py
+++ tests/onebitadam/test_server_error.py
@@ -20,6 +20,7 @@ dummy_optim = OnebitAdam(dummy_model, cuda_aware=False)
 
 device = torch.device('cuda', rank % torch.cuda.device_count())
 
+
 def torch_sim(a):
     a_sign = a.sign().add_(1).bool().float().add_(-0.5).mul_(2.0)
     scale = a.norm() / np.sqrt(a.numel())
@@ -40,19 +41,20 @@ def torch_sim(a):
     torch.distributed.barrier()
     return a_server_compressed, worker_error, server_error
 
+
 # Input Tensor size
 tensor_size = 100 * 2**20
 
 server_size = int(tensor_size / size)
-if tensor_size % ( 8 * size ) != 0:
-    right_tensor_size = tensor_size + ( 8*size - (tensor_size % ( 8 * size )) )
+if tensor_size % (8 * size) != 0:
+    right_tensor_size = tensor_size + (8 * size - (tensor_size % (8 * size)))
 else:
     right_tensor_size = tensor_size
 
 right_server_size = right_tensor_size // size
 
 # The -0.5 is required for avoiding sign flips/errors
-a = torch.rand(tensor_size, device=device)  - 0.5 
+a = torch.rand(tensor_size, device=device) - 0.5
 
 worker_error = torch.zeros(right_tensor_size, device=device)
 server_error = torch.zeros(right_server_size, device=device)
@@ -75,8 +77,11 @@ threshold = 1e-6
 diff_pos = ((a_after - a_torch) > threshold)
 
 if rank == 0:
-    before_diff = torch.chunk(a_after - a_torch, size)[rank] + server_error  - server_error_torch
-    if torch.norm(before_diff)/torch.norm(torch.chunk(a_after, size)[rank]) < threshold:
+    before_diff = torch.chunk(a_after - a_torch,
+                              size)[rank] + server_error - server_error_torch
+    if torch.norm(before_diff) / torch.norm(torch.chunk(a_after,
+                                                        size)[rank]) < threshold:
         print('Successfully passed the test')
     else:
-        print('The difference for the tensor before allgather is {}'.format(torch.norm(before_diff)))
+        print('The difference for the tensor before allgather is {}'.format(
+            torch.norm(before_diff)))
