8482962ce77cbe0f1aeff18f6230604c1c58257c
Jeff Rasley <jerasley@microsoft.com>
2020-09-09 15:06:11 -0700

    formatting

--- deepspeed/runtime/engine.py
+++ deepspeed/runtime/engine.py
@@ -558,7 +558,9 @@ class DeepSpeedEngine(Module):
         initial_dynamic_scale = self.initial_dynamic_scale()
         dynamic_loss_args = self.dynamic_loss_scale_args()
         clip_grad = self.gradient_clipping()
-        if isinstance(optimizer, apex.optimizers.FusedAdam) or self.optimizer_name() == ONEBIT_ADAM_OPTIMIZER:
+        if isinstance(optimizer,
+                      apex.optimizers.FusedAdam) or self.optimizer_name(
+                      ) == ONEBIT_ADAM_OPTIMIZER:
             if self.dynamic_loss_scale():
                 logger.info('Creating fp16 optimizer with dynamic loss scale')
                 timers = self.timers if self.wall_clock_breakdown() else None
