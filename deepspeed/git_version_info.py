f1dea531a86aca8faebcb60cf6722227bd4ca4ac
Shaden Smith <Shaden.Smith@microsoft.com>
2020-09-14 16:15:37 -0700

    version info redirect

--- deepspeed/git_version_info.py
+++ deepspeed/git_version_info.py
@@ -1,9 +1,4 @@
-version = '0.3.0'
-git_hash = '[none]'
-git_branch = '[none]'
-installed_ops = {
-    'lamb': False,
-    'transformer': False,
-    'sparse-attn': False,
-    'cpu-adam': False
-}
+try:
+    from .git_version_info_installed import *
+except ImportError:
+    from .git_version_info_sample import *
---
+++ deepspeed/git_version_info_template.py
@@ -0,0 +1,9 @@
+version = '0.3.0+[none]'
+git_hash = '[none]'
+git_branch = '[none]'
+installed_ops = {
+    'lamb': False,
+    'transformer': False,
+    'sparse-attn': False,
+    'cpu-adam': False
+}
