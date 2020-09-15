c3316407366d463ed86ea3b1e615cd4c36880c5c
Shaden Smith <Shaden.Smith@microsoft.com>
2020-09-14 17:24:19 -0700

    version info cleanup

--- deepspeed/git_version_info.py
+++ deepspeed/git_version_info.py
@@ -3,4 +3,12 @@ try:
     from .git_version_info_installed import *
 except ModuleNotFoundError:
     # Will be missing from checkouts that haven't been installed (e.g., readthedocs)
-    from .git_version_info_template import *
+    version = '0.3.0+[none]'
+    git_hash = '[none]'
+    git_branch = '[none]'
+    installed_ops = {
+        'lamb': False,
+        'transformer': False,
+        'sparse-attn': False,
+        'cpu-adam': False
+    }
