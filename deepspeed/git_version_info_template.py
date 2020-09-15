cd460d195b7971e16b4487872d7987e8cc0a3fc5
Shaden Smith <Shaden.Smith@microsoft.com>
2020-09-14 17:22:52 -0700

    version info cleanup

--- deepspeed/git_version_info_template.py
+++
@@ -1,9 +0,0 @@
-version = '0.3.0+[none]'
-git_hash = '[none]'
-git_branch = '[none]'
-installed_ops = {
-    'lamb': False,
-    'transformer': False,
-    'sparse-attn': False,
-    'cpu-adam': False
-}
