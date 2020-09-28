5c2b37d77f18f46ba90181379fbb5354e7c10a5e
Tunji Ruwase <olruwase@microsoft.com>
2020-09-28 18:13:07 +0000

    Disable test_pipe.py for CI debugging

--- tests/unit/test_pipe.py
+++ tests/unit/test_pipe.py
@@ -10,6 +10,8 @@ import pytest
 import deepspeed
 import deepspeed.runtime.utils as ds_utils
 
+pytest.skip("Disabling test_pipe.py for CI debugging", allow_module_level=True)
+
 from deepspeed.runtime.pipe.topology import PipeDataParallelTopology, PipeModelDataParallelTopology
 PipeTopo = PipeDataParallelTopology
 import deepspeed.runtime.pipe.module as PipelineModule
