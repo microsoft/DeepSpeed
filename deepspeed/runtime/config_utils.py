a2689d943dff11bde3ec79a925b157f5218c47c2
Samyam Rajbhandari <samyamr@microsoft.com>
2020-11-16 22:04:07 +0000

    Starting to add config modifications. Currently in incomplete state

--- deepspeed/runtime/config_utils.py
+++ deepspeed/runtime/config_utils.py
@@ -12,6 +12,9 @@ from collections import Counter
 def get_scalar_param(param_dict, param_name, param_default_value):
     return param_dict.get(param_name, param_default_value)
 
+def get_list_param(param_dict, param_name, param_default_value):
+    return param_dict.get(param_name, param_default_value)
+
 
 def dict_raise_error_on_duplicate_keys(ordered_pairs):
     """Reject duplicate keys."""
--- deepspeed/utils/__init__.py
+++ deepspeed/utils/__init__.py
@@ -1,2 +1,3 @@
 from deepspeed.utils.logging import logger, log_dist
 from deepspeed.runtime.dataloader import RepeatingLoader
+from deepspeed.utils.elasticity import get_compatible_gpus
