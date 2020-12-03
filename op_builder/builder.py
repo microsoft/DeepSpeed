ff58fa7e5a4f637a21d11daad0192683fe50ed15
Stas Bekman <stas00@users.noreply.github.com>
2020-12-02 21:20:16 -0800

    [build] build against installed cuda-11.1 while torch built w/ cuda-11.0 (#570)

--- op_builder/builder.py
+++ op_builder/builder.py
@@ -44,6 +44,9 @@ def assert_no_cuda_mismatch():
     torch_cuda_version = ".".join(torch.version.cuda.split('.')[:2])
     # This is a show-stopping error, should probably not proceed past this
     if sys_cuda_version != torch_cuda_version:
+        if sys_cuda_version == "11.1" and torch_cuda_version == "11.0":
+            # it works to build against installed cuda-11.1 while torch was built with cuda-11.0
+            return
         raise Exception(
             f"Installed CUDA version {sys_cuda_version} does not match the "
             f"version torch was compiled with {torch.version.cuda}, unable to compile "
