60f3344b52260d4d49de30765062b2e41107ade5
Ammar Ahmad Awan <ammar.awan@microsoft.com>
2020-12-09 00:48:48 +0000

    modify nccl perf test.

--- tests/onebitadam/test_nccl_perf.py
+++ tests/onebitadam/test_nccl_perf.py
@@ -10,6 +10,8 @@ from deepspeed.runtime.comm.nccl import NcclBackend
 # Configure wall clock timer
 from deepspeed.utils.timer import SynchronizedWallClockTimer
 
+from statistics import mean 
+
 timers = SynchronizedWallClockTimer()
 
 comm = MPI.COMM_WORLD
@@ -26,7 +28,7 @@ backend = NcclBackend()
 
 device = torch.device('cuda', rank % torch.cuda.device_count())
 
-tensor_size = 100 * 2**20
+tensor_size = 300 * 2**20
 server_size = int(tensor_size / size)
 if tensor_size % (8 * size) != 0:
     right_tensor_size = tensor_size + (8 * size - (tensor_size % (8 * size)))
@@ -41,12 +43,13 @@ a = (torch.rand(tensor_size, device=device) - 0.5) + 0.01 * rank
 worker_error = torch.zeros(right_tensor_size, device=device)
 server_error = torch.zeros(right_server_size, device=device)
 
+warmup = 10
 iters = 100
 
 local_rank = rank % torch.cuda.device_count()
 
 # Warmup
-for i in range(iters):
+for i in range(warmup):
     backend.compressed_allreduce(a, worker_error, server_error, local_rank)
 
 time_list = []
@@ -55,9 +58,21 @@ for i in range(iters):
     timers('compressed_allreduce').start()
     backend.compressed_allreduce(a, worker_error, server_error, local_rank)
     timers('compressed_allreduce').stop()
-    time_list += timers('compressed_allreduce').elapsed()
+    time_list.append(timers('compressed_allreduce').elapsed())
 
 timer_names = ['compressed_allreduce']
-timers.log(names=timer_names, normalizer=iters, memory_breakdown=None)
+timers.log(names=timer_names, normalizer=1, memory_breakdown=None)
 
-print(time_list)
+places = 2
+convert = 1e3
+float_size = 4
+
+if rank == 0:
+    for i in range(iters):
+        lat = time_list[i]
+        print("latency = ", lat * convert)
+
+minlat = round(min(time_list) * convert)
+maxlat = round(max(time_list) * convert)
+meanlat = round(mean(time_list) * convert, places)
+print("min, max, and mean = {} ms, {} ms, {} ms".format(minlat, maxlat, meanlat))
