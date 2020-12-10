3e85a17ba48a934b06af93c949540daf91434d29
Ammar Ahmad Awan <ammar.awan@microsoft.com>
2020-12-10 11:49:07 -0800

    NCCL based 1-bit Implementation + Refactor to add communication backends (#593)
    * add nccl 1-bit optim.
    
    * temporary commit to save stuff.
    
    * Use dist collectives instead of mpi routines.
    
    * remove old code for comm.
    
    * Fix bugs. still does not work.
    
    * modify to test the nccl side code path
    
    * Initial gather impl. Works intra-node.
    
    * Updates to comm. phase 2. nccl comm. passed the tests.
    
    * refactor code to introduce nccl/mpi as backends for onebit adam.
    
    * Refactor updates to test/engine.
    
    * Fix compile/runtime errors.
    
    * simplify support for nccl/mpi backends.
    
    * Add missign file
    
    * Add compression backend in constructor. Revert later.
    
    * modify test with some perf counting.
    
    * Implement a true non-blocking gather for nccl side.
    
    * Revert "Add compression backend in constructor. Revert later."
    
    This reverts commit df8c40d3105e9f2542a8aa6619e80d675a09753f.
    
    * improve the 1-bit adam test.
    
    * Refactor comm. and compression backend in 1-bit adam.
    
    * Fix the test.
    
    * Fix runtime errors and typos in nccl backend
    
    * fix mpi backend. modify tests.
    
    * modify nccl perf test.
    
    * fix mpi side errors.
    
    * Add an mpi perf test
    
    * Sync DSE.

--- tests/onebitadam/test_mpi_backend.py
+++ tests/onebitadam/test_mpi_backend.py
@@ -4,7 +4,8 @@ import torch
 import torch.distributed as dist
 import numpy as np
 import deepspeed
-from deepspeed.runtime.fp16.onebit_adam import OnebitAdam
+
+from deepspeed.runtime.comm.mpi import MpiBackend
 
 comm = MPI.COMM_WORLD
 size = comm.Get_size()
@@ -12,18 +13,17 @@ rank = comm.Get_rank()
 
 #TODO: Detect the hostname we are running on automatically
 torch.distributed.init_process_group(backend='nccl',
-                                     init_method='tcp://worker-1:2245',
+                                     init_method='tcp://worker-0:2245',
                                      world_size=size,
                                      rank=rank)
 
-dummy_model = [torch.nn.Parameter(torch.ones(10))]
-
-# Set cuda_aware to True to use CUDA buffers for communication
-dummy_optim = OnebitAdam(dummy_model, cuda_aware=True)
+# Change cuda_aware to True to test out CUDA-Aware MPI communication
+backend = MpiBackend(cuda_aware=False)
 
 device = torch.device('cuda', rank % torch.cuda.device_count())
 
 
+# A simulated compression function using torch.distributed
 def torch_sim(a):
     a_sign = a.sign().add_(1).bool().float().add_(-0.5).mul_(2.0)
     scale = a.norm() / np.sqrt(a.numel())
@@ -52,21 +52,20 @@ if tensor_size % (8 * size) != 0:
 else:
     right_tensor_size = tensor_size
 right_server_size = right_tensor_size // size
+
 # Adding bias to the initialization of the gradient we are communicating
 # In order to get rid of the case where some elements in the gradient are too small
 a = (torch.rand(tensor_size, device=device) - 0.5) + 0.01 * rank
+
 worker_error = torch.zeros(right_tensor_size, device=device)
 server_error = torch.zeros(right_server_size, device=device)
+
 a_torch, worker_error_torch, server_error_torch = torch_sim(a)
 torch.cuda.empty_cache()
 local_rank = rank % torch.cuda.device_count()
-a_after = dummy_optim.Compressed_Allreduce(a,
-                                           worker_error,
-                                           server_error,
-                                           rank,
-                                           size,
-                                           comm,
-                                           local_rank)
+
+a_after = backend.compressed_allreduce(a, worker_error, server_error, local_rank)
+
 threshold = 1e-6
 magnitude_threshold = 1e-6
 diff_mask = (a_after - a_torch) > threshold
@@ -74,13 +73,16 @@ diff_server_mask = torch.chunk(diff_mask, size)[rank]
 mpi_server = torch.chunk(a_after, size)[rank] + server_error
 torch_server = torch.chunk(a_torch, size)[rank] + server_error_torch
 
+test_correctness = True
+
 # If the number in the compensated_server_m is too small (e.g 1e-8), then calling sign() might be problematic
 # The test would skip those numbers that are too small in compensated_server_m
-if torch.sum(diff_server_mask) == 0:
-    print('Successfully passed the test for 1bit Adam at Rank {}'.format(rank))
-else:
-    check_mag_mask = mpi_server[diff_mask] > magnitude_threshold
-    if torch.sum(check_mag_mask) == 0:
-        print('Successfully passed the test for 1bit Adam at Rank {}'.format(rank))
+if test_correctness:
+    if torch.sum(diff_server_mask) == 0:
+        print('Successfully passed the test for MPI Backend at Rank {}'.format(rank))
     else:
-        print('Fails at {} of positions'.format(torch.sum(check_mag_mask)))
+        check_mag_mask = mpi_server[diff_mask] > magnitude_threshold
+        if torch.sum(check_mag_mask) == 0:
+            print('Successfully passed the test for MPI Backend at Rank {}'.format(rank))
+        else:
+            print('Fails at {} of positions'.format(torch.sum(check_mag_mask)))
