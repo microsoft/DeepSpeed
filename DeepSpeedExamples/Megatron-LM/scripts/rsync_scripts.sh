f19f28cd1d1ecea9b9cfd4842300c67c6f1b1f85
kooyunmo <mpbb03@snu.ac.kr>
2021-01-12 04:47:28 +0000

    multinode launch scripts rsync

---
+++ DeepSpeedExamples/Megatron-LM/scripts/rsync_scripts.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SSH_LIST=(35.208.173.14 35.208.115.196 35.208.92.231)
+
+for ip in "${SSH_LIST[@]}"; do
+  rsync -avz --progress multinode_ds_zero2_pretrain_gpt2XL_model_parallel_config.json yunmokoo@"$ip":/home/yunmokoo/workspace/DeepSpeed/DeepSpeedExamples/Megatron-LM/scripts
+  rsync -avz --progress multinode_ds_zero2_pretrain_gpt2XL_model_parallel.sh yunmokoo@"$ip":/home/yunmokoo/workspace/DeepSpeed/DeepSpeedExamples/Megatron-LM/scripts
+done
+
