e2d9ad52159551cdef4f050f0c87c7b21f471d89
Young Jin Kim <youki@microsoft.com>
2021-02-02 09:50:30 -0800

    Added top-1 gating

--- deepspeed/moe/sharded_moe.py
+++ deepspeed/moe/sharded_moe.py
@@ -125,6 +125,89 @@ class Experts(nn.Module):
 
 # gating network
 
+class Top1Gating(nn.Module):
+    def __init__(
+        self,
+        dim,
+        num_gates,
+        outer_expert_dims = tuple(),
+        capacity_factor_train = 1.25,
+        capacity_factor_eval = 2.):
+        super().__init__()
+
+        self.num_gates = num_gates
+        self.w_gating = nn.Parameter(torch.randn(*outer_expert_dims, dim, num_gates))
+
+        self.capacity_factor_train = capacity_factor_train
+        self.capacity_factor_eval = capacity_factor_eval
+
+    def forward(self, x, importance = None):
+        *_, b, group_size, dim = x.shape
+        num_gates = self.num_gates
+
+        if self.training:
+            capacity_factor = self.capacity_factor_train
+        else:
+            capacity_factor = self.capacity_factor_eval
+
+        raw_gates = torch.einsum('...bnd,...de->...bne', x, self.w_gating)
+        raw_gates = raw_gates.softmax(dim=-1)
+
+        # FIND TOP 1 EXPERTS PER POSITON
+        # Find the top expert for each position. shape=[batch, group]
+
+        gate_1, index_1 = top1(raw_gates)
+        mask_1 = F.one_hot(index_1, num_gates).float()
+        density_1_proxy = raw_gates
+
+        if importance is not None:
+            equals_one_mask = (importance == 1.).float()
+            mask_1 *= equals_one_mask[..., None]
+            gate_1 *= equals_one_mask
+            density_1_proxy *= equals_one_mask[..., None]
+            del equals_one_mask
+
+        # BALANCING LOSSES
+        # shape = [batch, experts]
+        # We want to equalize the fraction of the batch assigned to each expert
+        density_1 = mask_1.mean(dim=-2)
+        # Something continuous that is correlated with what we want to equalize.
+        density_1_proxy = density_1_proxy.mean(dim=-2)
+        loss = (density_1_proxy * density_1).mean() * float(num_gates ** 2)
+
+        # Each sequence sends (at most?) expert_capacity positions to each expert.
+        # Static expert_capacity dimension is needed for expert batch sizes
+        expert_capacity = min(group_size, int((group_size * capacity_factor) / num_gates))
+        expert_capacity = max(expert_capacity, MIN_EXPERT_CAPACITY)
+        expert_capacity_f = float(expert_capacity)
+
+        # COMPUTE ASSIGNMENT TO EXPERTS
+        # [batch, group, experts]
+        # This is the position within the expert's mini-batch for this sequence
+        position_in_expert_1 = cumsum_exclusive(mask_1, dim=-2) * mask_1
+        # Remove the elements that don't fit. [batch, group, experts]
+        mask_1 *= (position_in_expert_1 < expert_capacity_f).float()
+        # [batch, experts]
+        # How many examples in this sequence go to this expert
+        mask_1_count = mask_1.sum(dim=-2, keepdim=True)
+        # [batch, group] - mostly ones, but zeros where something didn't fit
+        mask_1_flat = mask_1.sum(dim=-1)
+        # [batch, group]
+        position_in_expert_1 = position_in_expert_1.sum(dim=-1)
+        # Weight assigned to first expert.  [batch, group]
+        gate_1 *= mask_1_flat
+
+        # [batch, group, experts, expert_capacity]
+        combine_tensor = (
+            gate_1[..., None, None]
+            * mask_1_flat[..., None, None]
+            * F.one_hot(index_1, num_gates)[..., None]
+            * safe_one_hot(position_in_expert_1.long(), expert_capacity)[..., None, :]
+        )
+
+        dispatch_tensor = combine_tensor.bool().to(combine_tensor)
+        return dispatch_tensor, combine_tensor, loss
+
 class Top2Gating(nn.Module):
     def __init__(
         self,
@@ -271,10 +354,10 @@ class ShardedMoE(nn.Module):
         num_experts = 16,
         hidden_dim = None,
         activation = nn.ReLU,
-        second_policy_train = 'random',
-        second_policy_eval = 'random',
-        second_threshold_train = 0.2,
-        second_threshold_eval = 0.2,
+        # second_policy_train = 'random',
+        # second_policy_eval = 'random',
+        # second_threshold_train = 0.2,
+        # second_threshold_eval = 0.2,
         capacity_factor_train = 1.25,
         capacity_factor_eval = 2.,
         loss_coef = 1e-2,
@@ -283,8 +366,11 @@ class ShardedMoE(nn.Module):
 
         self.num_experts = num_experts
 
-        gating_kwargs = {'second_policy_train': second_policy_train, 'second_policy_eval': second_policy_eval, 'second_threshold_train': second_threshold_train, 'second_threshold_eval': second_threshold_eval, 'capacity_factor_train': capacity_factor_train, 'capacity_factor_eval': capacity_factor_eval}
-        self.gate = Top2Gating(dim, num_gates = num_experts, **gating_kwargs)
+        # change to top-1 gating
+        gating_kwargs = {'capacity_factor_train': capacity_factor_train, 'capacity_factor_eval': capacity_factor_eval}
+        self.gate = Top1Gating(dim, num_gates = num_experts, **gating_kwargs)
+        # gating_kwargs = {'second_policy_train': second_policy_train, 'second_policy_eval': second_policy_eval, 'second_threshold_train': second_threshold_train, 'second_threshold_eval': second_threshold_eval, 'capacity_factor_train': capacity_factor_train, 'capacity_factor_eval': capacity_factor_eval}
+        # self.gate = Top2Gating(dim, num_gates = num_experts, **gating_kwargs)
         self.experts = default(experts, lambda: Experts(dim, num_experts = num_experts, hidden_dim = hidden_dim, activation = activation))
         self.loss_coef = loss_coef
 
