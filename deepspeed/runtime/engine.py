e2544a9cdbd62c5855054391d9b43aabd85a0c74
Shaden Smith <shaden.smith@microsoft.com>
2021-02-04 08:08:22 +0000

    enable apex.optimizers.FusedAdam and deepspeed fused fp16 optimizer

--- deepspeed/runtime/engine.py
+++ deepspeed/runtime/engine.py
@@ -632,7 +632,10 @@ class DeepSpeedEngine(Module):
         dynamic_loss_args = self.dynamic_loss_scale_args()
         clip_grad = self.gradient_clipping()
         if isinstance(optimizer,
-                      FusedAdam) or self.optimizer_name() == ONEBIT_ADAM_OPTIMIZER:
+                      FusedAdam) or self.optimizer_name() in [
+                          'FusedAdam',
+                          ONEBIT_ADAM_OPTIMIZER
+                      ]:
             if self.dynamic_loss_scale():
                 logger.info('Creating fp16 optimizer with dynamic loss scale')
                 timers = self.timers if self.wall_clock_breakdown() else None
