ac9266eef286235eb593ed0110fb7231dbd2c936
Samyam <samyamr@microsoft.com>
2021-03-10 23:11:49 +0000

    Formatting fix

--- deepspeed/runtime/zero/partition_parameters.py
+++ deepspeed/runtime/zero/partition_parameters.py
@@ -807,7 +807,10 @@ class Init(InsertPostInitMethodToModuleSubClasses):
         if start < param.ds_numel:
             elements = min(param.ds_numel - start, partition_size)
 
-            dest_tensor_full_buffer = partition_buffer.view(-1).narrow(0, 0, partition_size)
+            dest_tensor_full_buffer = partition_buffer.view(-1).narrow(
+                0,
+                0,
+                partition_size)
 
             dest_tensor = dest_tensor_full_buffer.narrow(0, 0, elements)
             src_tensor = param.grad.view(-1).narrow(0, start, elements)
