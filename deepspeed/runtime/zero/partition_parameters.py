bd9c78126e8ad323a9353fc15f6b8c5ed10d62f1
Stas Bekman <stas00@users.noreply.github.com>
2021-03-19 11:36:06 -0700

    zero.Init() clarification
    clarify that if `model.half()` can't fit into gpu memory `zero.Init()` is a must.
    
    this proposal is via @samyam's clarification shared elsewhere.
    
    Thank you.

--- deepspeed/runtime/zero/partition_parameters.py
+++ deepspeed/runtime/zero/partition_parameters.py
@@ -278,6 +278,9 @@ class Init(InsertPostInitMethodToModuleSubClasses):
         allows for a linear increase in model size with the aggregate system memory.
         For example, if a node has 1TB of memory and 8 GPUs, we could fit a trillion
         parameter model with 4 nodes and 32 GPUs.
+        
+        Important: If the fp16 weights of the model can't fit onto the gpu memory this 
+        feature must be used.
 
         .. note::
             Initializes ``torch.distributed`` if it has not already been done so.
