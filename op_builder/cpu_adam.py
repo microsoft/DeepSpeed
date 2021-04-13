37651f3d167c5a0c2df2b869e79aaa6e06c6427b
Jithun Nair <37884920+jithunnair-amd@users.noreply.github.com>
2021-04-13 13:42:26 -0500

    Disable AVX512 for ROCm to enable same build of DeepSpeed to work on Intel and AMD CPUs (#7)

--- op_builder/cpu_adam.py
+++ op_builder/cpu_adam.py
@@ -42,7 +42,7 @@ class CPUAdamBuilder(CUDAOpBuilder):
         result = subprocess.check_output('lscpu', shell=True)
         result = result.decode('utf-8').strip().lower()
         if 'genuineintel' in result:
-            if 'avx512' in result:
+            if not is_rocm_pytorch and 'avx512' in result:
                 return '-D__AVX512__'
             elif 'avx2' in result:
                 return '-D__AVX256__'
