4d3316b55551d6d6bddef2015f207dab63801dc0
Reza Yazdani <reyazda@microsoft.com>
2021-04-16 00:41:44 +0000

    fix unit test for higher version of PyTorch (1.8)

--- deepspeed/ops/transformer/transformer.py
+++ deepspeed/ops/transformer/transformer.py
@@ -523,6 +523,10 @@ class DeepSpeedTransformerLayer(nn.Module):
             v = initial_weights[2].data
 
             self.attn_qkvw = nn.Parameter(torch.cat((q, k, v)))
+            #self.attn_qkvw[i * self.config.hidden_size:(i + 1) * self.config.hidden_size] = \
+            #    initial_weights[i].clone()
+            #torch.empty_like(initial_weights[i]).data.copy_(initial_weights[i].data)
+
             self.attn_qkvb = nn.Parameter(torch.Tensor(self.config.hidden_size * 3))
             self.attn_qkvb.data.zero_()
             self.attn_ow = initial_weights[3]
