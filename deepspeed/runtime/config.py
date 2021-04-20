2eda1616512fb3f26b0fa77476864d32fcef49f2
Samyam <samyamr@microsoft.com>
2021-04-20 22:50:51 +0000

    Adding fp32 and tf32 support for ZeRO Stage 2

--- deepspeed/runtime/config.py
+++ deepspeed/runtime/config.py
@@ -759,7 +759,7 @@ class DeepSpeedConfig(object):
             GRADIENT_ACCUMULATION_STEPS)
 
         if self.zero_enabled:
-            if self.zero_optimization_stage != MAX_STAGE_ZERO_OPTIMIZATION:
+            if self.zero_optimization_stage < ZERO_OPTIMIZATION_GRADIENTS:
                 assert self.fp16_enabled, "DeepSpeedConfig: ZeRO is only supported if fp16 is enabled"
             assert self.zero_optimization_stage <= MAX_STAGE_ZERO_OPTIMIZATION, "DeepSpeedConfig: Maximum supported ZeRO stage is {}".format(MAX_STAGE_ZERO_OPTIMIZATION)
             #if self.zero_config.cpu_offload is True:
