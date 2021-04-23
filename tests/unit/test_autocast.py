3002983cf7110f6ba3732c47ee2c5b75ce52498c
Olatunji Ruwase <olruwase@microsoft.com>
2021-04-23 07:34:28 +0000

    Format fixes

--- tests/unit/test_autocast.py
+++ tests/unit/test_autocast.py
@@ -3,6 +3,7 @@ import torch
 import deepspeed
 from deepspeed.runtime.zero.linear import LinearModuleForZeroStage3
 
+
 def _skip_autocast_test():
     try:
         import torch.cuda.amp.custom_fwd
@@ -13,9 +14,7 @@ def _skip_autocast_test():
     return True
 
 
-
-@pytest.mark.parametrize('half_op',
-                         [False, True])
+@pytest.mark.parametrize('half_op', [False, True])
 def test_missing_amp_autocast(tmpdir, half_op):
     hidden_dim = 4
     if half_op:
@@ -25,13 +24,11 @@ def test_missing_amp_autocast(tmpdir, half_op):
         input = torch.randn(hidden_dim).cuda()
         ds_linear = LinearModuleForZeroStage3(hidden_dim, hidden_dim).cuda()
 
-
     output = ds_linear(input)
     assert output.dtype == ds_linear.weight.dtype
 
 
-@pytest.mark.parametrize('half_op',
-                         [False, True])
+@pytest.mark.parametrize('half_op', [False, True])
 def test_disable_autocast_linear(tmpdir, half_op):
     if _skip_autocast_test():
         pytest.skip("amp autocast is not availalbe")
@@ -44,15 +41,20 @@ def test_disable_autocast_linear(tmpdir, half_op):
         input = torch.randn(hidden_dim).cuda()
         ds_linear = LinearModuleForZeroStage3(hidden_dim, hidden_dim).cuda()
 
-
     with torch.cuda.amp.autocast(False):
         output = ds_linear(input)
         assert output.dtype == ds_linear.weight.dtype
 
 
 @pytest.mark.parametrize('half_input, half_weight',
-                          [(False, False), (False, True), (True, False), (True, True)]
-                          )
+                         [(False,
+                           False),
+                          (False,
+                           True),
+                          (True,
+                           False),
+                          (True,
+                           True)])
 def test_autocast_linear(tmpdir, half_input, half_weight):
     if _skip_autocast_test():
         pytest.skip("amp autocast is not availalbe")
@@ -70,5 +72,3 @@ def test_autocast_linear(tmpdir, half_input, half_weight):
     with torch.cuda.amp.autocast():
         output = ds_linear(input)
         assert output.dtype == torch.half
-
-
