1430bd5bc2d90451fb2346c21fcbb2376b6fd546
hamlet <gvvvv@163.com>
2021-04-25 13:39:22 +0800

    Add cpu-adam compatible check

--- tests/unit/test_find_unused_parameters.py
+++ tests/unit/test_find_unused_parameters.py
@@ -3,22 +3,28 @@ import pytest
 import json
 import argparse
 import os
-
+import pytest
 from common import distributed_test
 from simple_model import UnusedParametersModel, random_dataloader, args_from_dict
+from deepspeed.ops.op_builder import CPUAdamBuilder
 
 import deepspeed
 
 
 @pytest.mark.parametrize('find_unused_parameters', [False, True])
 def test_stage2_find_unused_parameters(tmpdir, find_unused_parameters):
+    use_cpu_offload = True
+
+    if use_cpu_offload and not deepspeed.ops.__compatible_ops__[CPUAdamBuilder.NAME]:
+        pytest.skip("cpu-adam is not compatible")
+
     config_dict = {
         "train_micro_batch_size_per_gpu": 2,
         "gradient_accumulation_steps": 2,
         "steps_per_print": 1,
         "zero_optimization": {
             "stage": 2,
-            "cpu_offload": True,
+            "cpu_offload": use_cpu_offload,
             "find_unused_parameters": find_unused_parameters
         },
         "optimizer": {
