5e9b420e543e83d6fa1c64f86f9c44a708f37125
Reza Yazdani <reyazda@microsoft.com>
2021-06-08 16:57:30 +0000

    fix the inference for generation of large sequences (>1K & <32K)

--- csrc/transformer/inference/csrc/softmax.cu
+++ csrc/transformer/inference/csrc/softmax.cu
@@ -6,10 +6,8 @@
 #include <cstdlib>
 #include <ctime>
 
-#define Attn_Threads 128
-#define Reduce_Threads 32
-#define attn_warps 4
-#define MAX_ATTN_REG 4  // MAX Head Size 256
+#define ATTN_THREADS 1024
+#define MAX_REG_SIZE 8
 
 #define minus_infinity (-1 * std::numeric_limits<float>::infinity())
 
@@ -26,7 +24,6 @@ void CheckCudaErrorAux(const char* file, unsigned line)
 
 namespace cg = cooperative_groups;
 
-template <int tbSize, int tbSeq>
 __global__ void attn_softmax_v2(__half* vals,
                                 __half* mask,
                                 bool triangular,
@@ -37,22 +34,31 @@ __global__ void attn_softmax_v2(__half* vals,
                                 int heads,
                                 int sequence_length,
                                 int num_seq,
-                                float scale)
+                                float scale,
+                                int iterations,
+                                int reduceWidth)
 {
 #if __CUDA_ARCH__ >= 700
 
     cg::thread_block b = cg::this_thread_block();
-    cg::thread_block_tile<tbSize> g = cg::tiled_partition<tbSize>(b);
+    cg::thread_block_tile<WARP_SIZE> g = cg::tiled_partition<WARP_SIZE>(b);
 
-    float2 low_data[tbSeq];
-    float2 high_data[tbSeq];
+    float2 low_data[MAX_REG_SIZE];
+    float2 high_data[MAX_REG_SIZE];
 
     __half2 h_scale = __float2half2_rn(scale);
 
     int wid = threadIdx.x >> 5;
     int lane = threadIdx.x & 0x1f;
+    int warp_num = blockDim.x >> 5;
 
-    int iter_offset = blockIdx.x * (blockDim.x >> 5) + wid;
+    int reduce_blocks = reduceWidth >> 5;    
+    int seq_lane = threadIdx.x % reduceWidth;
+
+
+    __shared__ float partialSum[MAX_WARP_NUM];
+
+    int iter_offset = blockIdx.x * (warp_num / reduce_blocks) + (wid / reduce_blocks);
 
     if (iter_offset < total_count) {
         vals += (iter_offset * sequence_length);
@@ -70,8 +76,8 @@ __global__ void attn_softmax_v2(__half* vals,
 
         float max_val = minus_infinity;
 
-        for (int i = 0; i < tbSeq; i++) {
-            int data_id = i * (WARP_SIZE << 2) + (lane << 2);
+        for (int i = 0; i < iterations; i++) {
+            int data_id = i * (reduceWidth << 2) + (seq_lane << 2);
             if ((!triangular || ((data_id >> 2) <= seq_id4)) && (data_id >> 2) >= window_stride4 &&
                 data_id < sequence_length) {
                 if ((sequence_length - data_id) >= 4) {
@@ -130,27 +136,57 @@ __global__ void attn_softmax_v2(__half* vals,
             }
         }
 
-        for (int i = 1; i < tbSize; i *= 2) {
+        for (int i = 1; i < WARP_SIZE; i *= 2) {
             auto temp = g.shfl_xor(max_val, i);
             max_val = (temp > max_val ? temp : max_val);
         }
 
+        if (reduceWidth > WARP_SIZE) 
+        {
+            if (lane == 0) partialSum[wid] = max_val;
+            b.sync();
+    
+            if (lane < warp_num) max_val = partialSum[lane];
+    
+            b.sync();
+
+            for (int i = 1; i < reduce_blocks; i *= 2) {
+                auto temp = g.shfl_xor(max_val, i);
+                max_val = (temp > max_val ? temp : max_val);
+            }
+    
+            max_val = g.shfl(max_val, threadIdx.x / WARP_SIZE);
+        }
+
         float sum = 0;
-        for (int i = 0; i < tbSeq; i++) {
-            low_data[i].x = __expf(low_data[i].x - max_val);
-            low_data[i].y = __expf(low_data[i].y - max_val);
+        for (int i = 0; i < iterations; i++) {
+            low_data[i].x  = __expf(low_data[i].x  - max_val);
+            low_data[i].y  = __expf(low_data[i].y  - max_val);
             high_data[i].x = __expf(high_data[i].x - max_val);
             high_data[i].y = __expf(high_data[i].y - max_val);
 
             sum += (low_data[i].x + low_data[i].y + high_data[i].x + high_data[i].y);
         }
 
-        for (int i = 1; i < tbSize; i *= 2) sum += g.shfl_xor(sum, i);
+        for (int i = 1; i < WARP_SIZE; i *= 2) sum += g.shfl_xor(sum, i);
 
+        if (reduceWidth > WARP_SIZE) 
+        {
+            if (lane == 0) partialSum[wid] = sum;
+            b.sync();
+    
+            if (lane < warp_num) sum = partialSum[lane];
+    
+            b.sync();
+
+            for (int i = 1; i < reduce_blocks; i *= 2) { sum += g.shfl_xor(sum, i); }
+
+            sum = g.shfl(sum, threadIdx.x / WARP_SIZE);
+        }
         sum += 1e-6;
 
-        for (int i = 0; i < tbSeq; i++) {
-            int data_id = i * (WARP_SIZE << 2) + (lane << 2);
+        for (int i = 0; i < iterations; i++) {
+            int data_id = i * (reduceWidth << 2) + (seq_lane << 2);
 
             if (data_id < sequence_length) {
                 if ((sequence_length - data_id) >= 4) {
@@ -169,7 +205,6 @@ __global__ void attn_softmax_v2(__half* vals,
 #endif
 }
 
-template <int tbSize, int tbSeq>
 __global__ void attn_softmax_v2(float* vals,
                                 float* attn_mask,
                                 bool triangular,
@@ -180,18 +215,26 @@ __global__ void attn_softmax_v2(float* vals,
                                 int heads,
                                 int sequence_length,
                                 int num_seq,
-                                float scale)
+                                float scale,
+                                int iterations,
+                                int reduceWidth)
 {
     cg::thread_block b = cg::this_thread_block();
-    cg::thread_block_tile<tbSize> g = cg::tiled_partition<tbSize>(b);
+    cg::thread_block_tile<WARP_SIZE> g = cg::tiled_partition<WARP_SIZE>(b);
 
-    float4 data[tbSeq];
+    float4 data[MAX_REG_SIZE];
 
     int wid = threadIdx.x >> 5;
     int lane = threadIdx.x & 0x1f;
     int warp_num = blockDim.x >> 5;
 
-    int iter_offset = blockIdx.x * warp_num + wid;
+    int reduce_blocks = reduceWidth >> 5;    
+    int seq_lane = threadIdx.x % reduceWidth;
+
+
+    __shared__ float partialSum[MAX_WARP_NUM];
+
+    int iter_offset = blockIdx.x * (warp_num / reduce_blocks) + (wid / reduce_blocks);
     if (iter_offset < total_count) {
         vals += (iter_offset * sequence_length);
 
@@ -208,8 +251,8 @@ __global__ void attn_softmax_v2(float* vals,
 
         float max_val = minus_infinity;
 
-        for (int i = 0; i < tbSeq; i++) {
-            int data_id = i * (WARP_SIZE << 2) + (lane << 2);
+        for (int i = 0; i < iterations; i++) {
+            int data_id = i * (reduceWidth << 2) + (seq_lane << 2);
             if ((!triangular || ((data_id >> 2) <= seq_id4)) && (data_id >> 2) >= window_stride4 &&
                 data_id < sequence_length) {
                 if ((sequence_length - data_id) >= 4) {
@@ -263,13 +306,30 @@ __global__ void attn_softmax_v2(float* vals,
             }
         }
 
-        for (int i = 1; i < tbSize; i *= 2) {
+        for (int i = 1; i < WARP_SIZE; i *= 2) {
             auto temp = g.shfl_xor(max_val, i);
             max_val = (temp > max_val ? temp : max_val);
         }
 
+        if (reduceWidth > WARP_SIZE) 
+        {
+            if (lane == 0) partialSum[wid] = max_val;
+            b.sync();
+    
+            if (lane < warp_num) max_val = partialSum[lane];
+    
+            b.sync();
+
+            for (int i = 1; i < reduce_blocks; i *= 2) {
+                auto temp = g.shfl_xor(max_val, i);
+                max_val = (temp > max_val ? temp : max_val);
+            }
+    
+            max_val = g.shfl(max_val, threadIdx.x / WARP_SIZE);
+        }
+
         float sum = 0;
-        for (int i = 0; i < tbSeq; i++) {
+        for (int i = 0; i < iterations; i++) {
             data[i].x = __expf(data[i].x - max_val);
             data[i].y = __expf(data[i].y - max_val);
             data[i].z = __expf(data[i].z - max_val);
@@ -278,12 +338,25 @@ __global__ void attn_softmax_v2(float* vals,
             sum += (data[i].x + data[i].y + data[i].z + data[i].w);
         }
 
-        for (int i = 1; i < tbSize; i *= 2) sum += g.shfl_xor(sum, i);
+        for (int i = 1; i < WARP_SIZE; i *= 2) sum += g.shfl_xor(sum, i);
 
+        if (reduceWidth > WARP_SIZE) 
+        {
+            if (lane == 0) partialSum[wid] = sum;
+            b.sync();
+    
+            if (lane < warp_num) sum = partialSum[lane];
+    
+            b.sync();
+
+            for (int i = 1; i < reduce_blocks; i *= 2) { sum += g.shfl_xor(sum, i); }
+
+            sum = g.shfl(sum, threadIdx.x / WARP_SIZE);
+        }
         sum += 1e-6;
 
-        for (int i = 0; i < tbSeq; i++) {
-            int data_id = i * (WARP_SIZE << 2) + (lane << 2);
+        for (int i = 0; i < iterations; i++) {
+            int data_id = i * (reduceWidth << 2) + (seq_lane << 2);
 
             if (data_id < sequence_length) {
                 if ((sequence_length - data_id) >= 4) {
@@ -316,84 +389,28 @@ void launch_attn_softmax_v2(T* vals,
                             cudaStream_t stream)
 {
     int total_count = batch_size * heads * num_seq;
-    dim3 grid_dim((total_count - 1) / 32 + 1);
-    dim3 block_dim(1024);
-    if (sequence_length <= 128)
-        attn_softmax_v2<32, 1><<<grid_dim, block_dim, 0, stream>>>(vals,
-                                                                   mask,
-                                                                   triangular,
-                                                                   recompute,
-                                                                   local_attention,
-                                                                   window_size,
-                                                                   total_count,
-                                                                   heads,
-                                                                   sequence_length,
-                                                                   num_seq,
-                                                                   scale);
-    else if (sequence_length <= 256)
-        attn_softmax_v2<32, 2><<<grid_dim, block_dim, 0, stream>>>(vals,
-                                                                   mask,
-                                                                   triangular,
-                                                                   recompute,
-                                                                   local_attention,
-                                                                   window_size,
-                                                                   total_count,
-                                                                   heads,
-                                                                   sequence_length,
-                                                                   num_seq,
-                                                                   scale);
-    else if (sequence_length <= 512)
-        attn_softmax_v2<32, 4><<<grid_dim, block_dim, 0, stream>>>(vals,
-                                                                   mask,
-                                                                   triangular,
-                                                                   recompute,
-                                                                   local_attention,
-                                                                   window_size,
-                                                                   total_count,
-                                                                   heads,
-                                                                   sequence_length,
-                                                                   num_seq,
-                                                                   scale);
-    else if (sequence_length <= 1024)
-        attn_softmax_v2<32, 8><<<grid_dim, block_dim, 0, stream>>>(vals,
-                                                                   mask,
-                                                                   triangular,
-                                                                   recompute,
-                                                                   local_attention,
-                                                                   window_size,
-                                                                   total_count,
-                                                                   heads,
-                                                                   sequence_length,
-                                                                   num_seq,
-                                                                   scale);
-    else if (sequence_length <= 2048)
-        attn_softmax_v2<32, 16><<<grid_dim, block_dim, 0, stream>>>(vals,
-                                                                    mask,
-                                                                    triangular,
-                                                                    recompute,
-                                                                    local_attention,
-                                                                    window_size,
-                                                                    total_count,
-                                                                    heads,
-                                                                    sequence_length,
-                                                                    num_seq,
-                                                                    scale);
-    else if (sequence_length <= 4096)
-        attn_softmax_v2<32, 32><<<grid_dim, block_dim, 0, stream>>>(vals,
-                                                                    mask,
-                                                                    triangular,
-                                                                    recompute,
-                                                                    local_attention,
-                                                                    window_size,
-                                                                    total_count,
-                                                                    heads,
-                                                                    sequence_length,
-                                                                    num_seq,
-                                                                    scale);
+    dim3 grid_dim((total_count - 1) / (WARP_SIZE / ((sequence_length-1) / ATTN_THREADS + 1)) + 1);
+    dim3 block_dim(ATTN_THREADS);
+
+    const int reduce_width = ((sequence_length-1) / ATTN_THREADS + 1) * WARP_SIZE;
+    const int iterations = (sequence_length - 1) / (reduce_width << 2) + 1;
+
+    if (sequence_length <= 32768)
+        attn_softmax_v2<<<grid_dim, block_dim, 0, stream>>>(vals,
+                                                            mask,
+                                                            triangular,
+                                                            recompute,
+                                                            local_attention,
+                                                            window_size,
+                                                            total_count,
+                                                            heads,
+                                                            sequence_length,
+                                                            num_seq,
+                                                            scale, 
+                                                            iterations,
+                                                            reduce_width);
     else
-        throw std::runtime_error(
-            "Unsupport Seq_Length! Check the restriction of the max_threads and "
-            "max_thread_iterations!");
+        throw std::runtime_error("Unsupport Seq_Length!");
 }
 
 template void launch_attn_softmax_v2(float* vals,
