ee561862d59c1990aff26c7e7d88c4d1beac4540
Elton Zheng <eltonz@microsoft.com>
2021-06-09 00:35:06 +0000

    Add Windows support in README, use c++17 on Windows to support latest vc build tool

--- op_builder/builder.py
+++ op_builder/builder.py
@@ -364,6 +364,18 @@ class CUDAOpBuilder(OpBuilder):
         else:
             return ['-O3', '-std=c++14', '-g', '-Wno-reorder']
 
+    def nvcc_args(self):
+        args = [
+            '-O3',
+            '--use_fast_math',
+            '-std=c++17' if sys.platform == "win32" else '-std=c++14',
+            '-U__CUDA_NO_HALF_OPERATORS__',
+            '-U__CUDA_NO_HALF_CONVERSIONS__',
+            '-U__CUDA_NO_HALF2_OPERATORS__'
+        ]
+
+        return args + self.compute_capability_args()
+
     def libraries_args(self):
         if sys.platform == "win32":
             return ['cublas', 'curand']
--- op_builder/transformer_inference.py
+++ op_builder/transformer_inference.py
@@ -24,15 +24,3 @@ class InferenceBuilder(CUDAOpBuilder):
 
     def include_paths(self):
         return ['csrc/transformer/inference/includes']
-
-    def nvcc_args(self):
-        args = [
-            '-O3',
-            '--use_fast_math',
-            '-std=c++14',
-            '-U__CUDA_NO_HALF_OPERATORS__',
-            '-U__CUDA_NO_HALF_CONVERSIONS__',
-            '-U__CUDA_NO_HALF2_OPERATORS__',
-        ]
-
-        return args + self.compute_capability_args()
