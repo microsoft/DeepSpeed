ad578e1c392a5076c33b995c73d6012a25e5e521
Stas Bekman <stas@stason.org>
2021-06-23 14:23:35 -0700

    some more docs

--- deepspeed/utils/zero_to_fp32.py
+++ deepspeed/utils/zero_to_fp32.py
@@ -199,6 +199,19 @@ def get_fp32_state_dict_from_zero_chkpt(checkpoint_dir, tag=None):
     Returns:
         - pytorch ``state_dict``
 
+    Note: this approach may not work if your application doesn't have sufficient free CPU memory and you may need to use the offline approach using the ``zero_to_fp32.py`` script that is saved with the checkpoint.
+
+    A typical usage might be ::
+
+        from deepspeed.utils.zero_to_fp32 import get_fp32_state_dict_from_zero_chkpt
+        # do the training and checkpoint saving
+        state_dict = get_fp32_state_dict_from_zero_chkpt(checkpoint_dir) # already on cpu
+        model = model.cpu() # move to cpu
+        model.load_state_dict(state_dict)
+        #submit to model hub or save the model to share with others
+
+    In this example the ``model`` will no longer be useable in the deepspeed context of the same application. i.e. you will need to re-initialize the deepspeed engine, since ``model.load_state_dict(state_dict)`` will remove all the deepspeed magic from it.
+
     """
     if tag is None:
         latest_path = os.path.join(checkpoint_dir, 'latest')
