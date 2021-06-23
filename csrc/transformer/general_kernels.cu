0be96458a329b5df77d98e43d85f614b89fb388d
rraminen <rraminen@amd.com>
2021-06-23 19:51:43 +0000

    Revert "Cooperative Groups workaround for transformer kernels extension"
    This reverts commit fbddd9316c3686a15e3d805cbececf25feb4aa69.

--- csrc/transformer/general_kernels.cu
+++ csrc/transformer/general_kernels.cu
@@ -11,10 +11,7 @@ __global__ void column_sum_reduce(const T* __restrict__ inp,
     __shared__ float tile[TILE_DIM][TILE_DIM + 1];
 
     cg::thread_block b = cg::this_thread_block();
-    //cg::thread_block_tile<TILE_DIM> g = cg::tiled_partition<TILE_DIM>(b);
-    cg::thread_group g(cg::internal::cg_coalesced_tile, TILE_DIM);
-    g.tiled_partition(b, TILE_DIM);
-
+    cg::thread_block_tile<TILE_DIM> g = cg::tiled_partition<TILE_DIM>(b);
 
     int idx = blockDim.x * blockIdx.x + threadIdx.x;
 
