a1c4adfb439bb7108864bdc7f71375033faa2506
Stas Bekman <stas@stason.org>
2021-07-02 19:47:51 -0700

    wants half()

--- tests/unit/test_zero_context.py
+++ tests/unit/test_zero_context.py
@@ -355,7 +355,7 @@ def test_subclass_param_init():
     assert model.param.ds_status == ZeroParamStatus.NOT_AVAILABLE
 
     # test that the weights manipulation during each __init__ worked in all w/o needing gathering
-    ones = torch.ones(5).cuda()
+    ones = torch.ones(5).half().cuda()
     with deepspeed.zero.GatheredParameters(list(model.parameters(recurse=False))):
         assert torch.equal(model.param, ones + 1)
         assert torch.equal(model.param_pa, ones + 2)
