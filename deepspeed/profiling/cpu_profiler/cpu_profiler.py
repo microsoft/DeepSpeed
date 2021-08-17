84e563d0d571ff053c54177f6d14dce070af5f62
Harish S.G <harishsg99@gmail.com>
2021-08-17 23:05:10 +0530

    Added CPU profiler (!!working)

--- deepspeed/profiling/cpu_profiler/cpu_profiler.py
+++ deepspeed/profiling/cpu_profiler/cpu_profiler.py
@@ -2,8 +2,12 @@ import torch
 from torch.profiler import profile, record_function, ProfilerActivity
 
 
-def cpu_profiler(inputs):
-  with profile(activities=[ProfilerActivity.CPU], record_shapes=True) as prof:
-    with record_function("model_inference"):
-        model(inputs)
-  return (prof.key_averages().table(sort_by="cuda_time_total", row_limit=10))
+class CPUProfiler(object):
+    def __init__(self, model):
+        self.model = Model()
+
+    def start_gpu_profiler(inputs):
+        with profile(activities=[ProfilerActivity.CPU], record_shapes=True,profile_memory=True) as prof:
+                with record_function("model_inference"): 
+                        self.model(inputs)
+        return prof.key_averages().table(sort_by="cpu_time_total", row_limit=10)  
