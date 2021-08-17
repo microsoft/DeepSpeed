a2273408fcd279038b09a3c209f5215dd5d7aec5
Harish S.G <harishsg99@gmail.com>
2021-08-17 22:09:06 +0530

    Create gpu_profiler.py

---
+++ deepspeed/profiling/gpu_profiler/gpu_profiler.py
@@ -0,0 +1,9 @@
+import torch
+from torch.profiler import profile, record_function, ProfilerActivity
+
+with profile(activities=[
+        ProfilerActivity.CPU, ProfilerActivity.CUDA], record_shapes=True) as prof:
+    with record_function("model_inference"):
+        model(inputs)
+
+print(prof.key_averages().table(sort_by="cuda_time_total", row_limit=10))
