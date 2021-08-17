d4ef7eef17338c2d920f9d44d5631ee977240cbd
Harish S.G <harishsg99@gmail.com>
2021-08-17 22:23:26 +0530

    Added gpu_profiler class

--- deepspeed/profiling/gpu_profiler/gpu_profiler.py
+++ deepspeed/profiling/gpu_profiler/gpu_profiler.py
@@ -1,9 +1,18 @@
 import torch
 from torch.profiler import profile, record_function, ProfilerActivity
+class GPUProfiler(object):
+    def __init__(self, model):
+        self.model = model
 
+    def start_gpu_profiler():
+        with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], record_shapes=True) as prof:
+                with record_function("model_inference"): //need to implement
+                        model(inputs)
+        return prof.key_averages().table(sort_by="cuda_time_total", row_limit=10)                    
+'''
 with profile(activities=[
         ProfilerActivity.CPU, ProfilerActivity.CUDA], record_shapes=True) as prof:
     with record_function("model_inference"):
         model(inputs)
+'''
 
-print(prof.key_averages().table(sort_by="cuda_time_total", row_limit=10))
