f0147aa2ebff436af42bca73f1bed544d7947451
Pruthvi Madugundu <pruthvigithub@gmail.com>
2021-08-17 22:41:53 -0700

    Updating the torch version check to numeric

--- tests/unit/test_configurable_parallel.py
+++ tests/unit/test_configurable_parallel.py
@@ -12,8 +12,10 @@ from simple_model import args_from_dict, create_deepspeed_args
 from megatron_model import get_gpt2_model, get_megatron_version, GPT2ModelPipe
 from deepspeed.utils import RepeatingLoader
 
+TORCH_MAJOR = int(torch.__version__.split('.')[0])
+TORCH_MINOR = int(torch.__version__.split('.')[1])
 pytestmark = pytest.mark.skipif(
-    torch.__version__ < '1.5',
+    TORCH_MAJOR < 1 or (TORCH_MAJOR == 1 and TORCH_MINOR < 5),
     reason='Megatron-LM package requires Pytorch version 1.5 or above')
 
 
