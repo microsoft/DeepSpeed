895ecfb6a1c99343adb49c83133d2f968212fa01
Harish S.G <harishsg99@gmail.com>
2021-08-18 21:50:46 +0530

    Update gpu_profiler.py

--- deepspeed/profiling/gpu_profiler/gpu_profiler.py
+++ deepspeed/profiling/gpu_profiler/gpu_profiler.py
@@ -5,7 +5,7 @@ class GPUProfiler(object):
         self.model = Model()
 
     def start_gpu_profiler(inputs):
-        with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], record_shapes=True,profile_memory=True) as prof:
+        with profile(activities=[ProfilerActivity.CUDA], record_shapes=True,profile_memory=True) as prof:
                 with record_function("model_inference"): //need to implement
                         self.model(inputs)
         return prof.key_averages().table(sort_by="cuda_time_total", row_limit=10)                    
