7cbcc35b76073d1337aede7f179700c370b9af04
Hari Prasad <h4ri.prasad@gmail.com>
2021-08-29 12:28:06 +0530

    updated simple_model.py and test_data.py

--- tests/unit/test_data.py
+++ tests/unit/test_data.py
@@ -3,7 +3,7 @@ from deepspeed.runtime.dataloader import DeepSpeedDataLoader
 import torch
 import deepspeed
 from common import distributed_test
-from simple_model import SimpleModel, args_from_dict
+from simple_model import SimpleModel, args_from_dict, random_dataset
 
 
 def test_repeating_loader():
@@ -26,34 +26,20 @@ def test_dataloader_drop_last(tmpdir):
 
     model = SimpleModel(hidden_dim)
 
-    def random_dataloader(model,
-                          total_samples,
-                          hidden_dim,
-                          device,
-                          drop_last,
-                          dtype=torch.half):
-        batch_size = model.train_micro_batch_size_per_gpu()
-        train_data = torch.randn(total_samples, hidden_dim, device=device, dtype=dtype)
-        train_label = torch.empty(total_samples,
-                                  dtype=torch.long,
-                                  device=device).random_(hidden_dim)
-        train_dataset = torch.utils.data.TensorDataset(train_data, train_label)
-        train_loader = DeepSpeedDataLoader(train_dataset,
-                                           batch_size=batch_size,
-                                           dataloader_drop_last=drop_last)
-        return train_loader
-
     @distributed_test(world_size=[1])
     def _test_dataloader_drop_last(args, model, hidden_dim):
         optimizer = torch.optim.AdamW(params=model.parameters())
+        batch_size = model.train_micro_batch_size_per_gpu()
         model, _, _, _ = deepspeed.initialize(args=args,
                                               model=model,
                                               optimizer=optimizer)
-        data_loader = random_dataloader(model=model,
-                                        total_samples=50,
-                                        hidden_dim=hidden_dim,
-                                        device=model.device,
-                                        drop_last=True)
+        train_dataset = random_dataset(total_samples=50,
+                                       hidden_dim=hidden_dim,
+                                       device=model.device,
+                                       dtype=torch.half)
+        data_loader = DeepSpeedDataLoader(train_dataset,
+                                          batch_size=batch_size,
+                                          dataloader_drop_last=True)
         for n, batch in enumerate(data_loader):
             loss = model(batch[0], batch[1])
             model.backward(loss)
