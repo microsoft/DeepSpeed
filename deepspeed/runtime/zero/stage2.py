dab6dc866a0ab6e8d43cc00921748536f409d35e
Jeff Rasley <jerasley@microsoft.com>
2021-09-07 18:04:29 +0000

    remove bad import from cherry-pick

--- deepspeed/runtime/zero/stage2.py
+++ deepspeed/runtime/zero/stage2.py
@@ -13,7 +13,7 @@ from packaging import version as pkg_version
 import collections
 
 from deepspeed.runtime.fp16.loss_scaler import LossScaler, DynamicLossScaler
-from deepspeed.runtime.utils import bwc_tensor_model_parallel_rank, get_global_norm, see_memory_usage, is_model_parallel_parameter
+from deepspeed.runtime.utils import get_global_norm, see_memory_usage, is_model_parallel_parameter
 from deepspeed.runtime.zero.config import ZERO_OPTIMIZATION_GRADIENTS
 from deepspeed.ops.adam import DeepSpeedCPUAdam
 from deepspeed.ops.op_builder import UtilsBuilder
