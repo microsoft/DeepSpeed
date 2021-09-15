1a4899d25a1b3c166ae6d716599e68d662ba6a8e
Stas Bekman <stas@stason.org>
2021-09-15 15:44:03 -0700

    clean up the warning

--- deepspeed/runtime/zero/partition_parameters.py
+++ deepspeed/runtime/zero/partition_parameters.py
@@ -453,7 +453,7 @@ class Init(InsertPostInitMethodToModuleSubClasses):
         if config is not None:
             config_dict_or_path = config
             logger.warning(
-                f'DeepSpeedConfig: zero.Init: the `config` argument is deprecated. Please use `config_dict_or_path` instead.'
+                f'zero.Init: the `config` argument is deprecated. Please use `config_dict_or_path` instead.'
             )
 
         _ds_config = DeepSpeedConfig(config_dict_or_path,
