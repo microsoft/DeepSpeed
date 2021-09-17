8d59757d895576967023a15e34117e46b4db49d1
Jeff Rasley <jerasley@microsoft.com>
2021-09-17 15:11:09 -0700

    make sure test_path is not pytest triggered

--- tests/unit/common.py
+++ tests/unit/common.py
@@ -120,6 +120,6 @@ def distributed_test(world_size=2, backend='nccl'):
     return dist_wrap
 
 
-def test_path(filename):
+def get_test_path(filename):
     curr_path = Path(__file__).parent
     return str(curr_path.joinpath(filename))
