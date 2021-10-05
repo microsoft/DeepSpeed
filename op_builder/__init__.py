aeeb444ce61e7398b1401f09d7ba09cea45415d3
Jeff Rasley <jerasley@microsoft.com>
2021-10-05 10:22:58 -0700

    fix import error

--- op_builder/__init__.py
+++ op_builder/__init__.py
@@ -9,7 +9,6 @@ from .transformer import TransformerBuilder
 from .stochastic_transformer import StochasticTransformerBuilder
 from .utils import UtilsBuilder
 from .async_io import AsyncIOBuilder
-from .builder import get_default_compute_capatabilities, OpBuilder
 from .transformer_inference import InferenceBuilder
 from .quantizer import QuantizerBuilder
 
