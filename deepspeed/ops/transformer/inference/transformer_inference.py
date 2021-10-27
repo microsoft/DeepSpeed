fixing the softmax masking when using triangular masking

deepspeed/ops/transformer/inference/transformer_inference.py
@@ -159,36 +159,9 @@ class DeepSpeedSelfAttentionFunction(Function):
                                       (hidden_size_per_partition,)
             return x.view(*new_x_layer_shape)
 
-        def backup_attention(mixed_query, key_layer, value_layer, input_mask):
-            if layer_past is not None:
-                past_key, past_value = layer_past
-                key_layer = torch.cat((past_key.type_as(key_layer), key_layer), dim=-2)
-                value_layer = torch.cat((past_value.type_as(value_layer),
-                                         value_layer),
-                                        dim=-2)
-            query = _transpose_for_scores(mixed_query, False)
-            key = _transpose_for_scores(key_layer, True)
-            value = _transpose_for_scores(value_layer, False)
-            p = torch.matmul(query, key)
-
-            ds_softmax = inference_cuda_module.softmax_fp16 if config.fp16 else \
-                            inference_cuda_module.softmax_fp32
-            p = ds_softmax(p / (float(key.size(-2))**0.5),
-                           input_mask,
-                           True,
-                           False,
-                           False,
-                           256)
-            p = p.to(value.dtype)
-            context_layer = torch.matmul(p, value)
-            context_layer = _transpose_for_context(context_layer)
-            return context_layer, key_layer, value_layer
-
         def compute_attention(qkv_out, input_mask):
             score_context_func = inference_cuda_module.softmax_context_fp32 if (not config.fp16) else \
                                     inference_cuda_module.softmax_context_fp16
-            #if not config.triangular_masking:
-            #    qkv_out = qkv_out.float()
 
             if merge_count > 0 and config.q_int8:
                 split_dim = (qkv_out.dim() - 1)
@@ -216,10 +189,7 @@ class DeepSpeedSelfAttentionFunction(Function):
             if no_masking:
                 input_mask = torch.empty(1)
             head_size = (mixed_query.shape[-1] // num_attention_heads_per_partition)
-            #return backup_attention(mixed_query,
-            #     key_layer,
-            #     value_layer,
-            #     input_mask)
+
             unfused_mode = not config.specialized_mode or \
                                 mixed_query.shape[1] >= 32 or head_size > 128
 
@@ -270,7 +240,6 @@ class DeepSpeedSelfAttentionFunction(Function):
                     config.local_attention,
                     config.window_size,
                     no_masking)
-            #import pdb;pdb.set_trace()
             if unfused_mode:
                 context_layer, _, _ = attn_key_value
             else:
@@ -278,8 +247,6 @@ class DeepSpeedSelfAttentionFunction(Function):
 
             # Transpose Context
             context_layer = _transpose_for_context(context_layer)
-            #if (config.fp16 or config.q_int8) and not config.triangular_masking:
-            #    context_layer = context_layer.half()
 
             return context_layer, key_layer, value_layer
 