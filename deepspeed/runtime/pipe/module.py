661d90943f05b5b72f2b2b1dfcd9faa0ae588bc3
hyunwoongko <gusdnd852@naver.com>
2021-11-06 14:41:25 +0900

    remove property

--- deepspeed/runtime/pipe/module.py
+++ deepspeed/runtime/pipe/module.py
@@ -512,7 +512,6 @@ class PipelineModule(nn.Module):
         """ ProcessTopology object to query process mappings. """
         return self._topo
 
-    @property
     def mpu(self):
         return self._grid
 
