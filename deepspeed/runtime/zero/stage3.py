ddb77d92e9f475db6abad6eb86a5844e04d53cbc
Olatunji Ruwase <olruwase@microsoft.com>
2021-11-11 14:40:31 +0000

    Update parameter names

--- deepspeed/runtime/zero/stage3.py
+++ deepspeed/runtime/zero/stage3.py
@@ -3411,9 +3411,17 @@ def estimate_zero3_model_states_mem_needs_all_cold(total_params,
 
     """
     def format_options(cpu_offload, cpu_offload_params, zero_init):
+        offload_dict = {
+            OFFLOAD_PARAM: {
+                OFFLOAD_PARAM_DEVICE:
+                OFFLOAD_CPU_DEVICE if cpu_offload_params else "none"
+            },
+            OFFLOAD_OPTIMIZER: {
+                OFFLOAD_OPTIMIZER_DEVICE: OFFLOAD_CPU_DEVICE if cpu_offload else "none"
+            }
+        }
         enabled = []
-        enabled.append(f"cpu_offload={1 if cpu_offload else 0}")
-        enabled.append(f"cpu_offload_params={1 if cpu_offload_params else 0}")
+        enabled.append(f"{offload_dict}")
         enabled.append(f"zero_init={1 if zero_init else 0}")
         return ", ".join(enabled)
 
