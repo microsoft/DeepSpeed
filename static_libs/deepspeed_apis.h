13efb6df85473fcdbe71df284b0505515b4f44b5
Reza Yazdani <reyazda@microsoft.com>
2021-11-16 13:58:44 +0500

    adding the sample gelu static shared library

---
+++ static_libs/deepspeed_apis.h
@@ -0,0 +1,16 @@
+
+
+#pragma once
+
+namespace DeepSpeedAPI {
+
+void bias_gelu(void* inout, void* bias, unsigned bsz, unsigned intermediate_size, bool is_float, cudaStream_t stream);
+
+void bias_residual(void* inout,
+                   void* residual,
+                   void* bias,
+                   unsigned bsz,
+                   unsigned hidden_dim,
+                   bool is_float);
+
+}  // namespace DeepSpeedAPI
---
+++ static_libs/test/pt_binding_test.cpp
@@ -0,0 +1,26 @@
+
+#include <torch/extension.h>
+#include <vector>
+#include <ATen/cuda/CUDAContext.h>
+#include <cuda_runtime_api.h>
+#include "deepspeed_apis.h"
+
+void bias_gelu(torch::Tensor& x, torch::Tensor& bias)
+{
+    bool is_float = (x.scalar_type() == at::kFloat);
+    auto sizes = x.sizes();
+    unsigned bsz = 1;
+    for (auto size : sizes) bsz *= size;
+    DeepSpeedAPI::bias_gelu(x.data_ptr(),
+                            bias.data_ptr(),
+                            bsz / x.size(sizes.size() - 1),
+                            x.size(sizes.size() - 1),
+                            is_float,
+                            at::cuda::getStreamFromPool());
+                            //at::cuda::getCurrentCUDAStream());
+}
+
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
+{
+    m.def("bias_gelu", &bias_gelu, "DeepSpeed bias_gelu forward(CUDA)");
+}
