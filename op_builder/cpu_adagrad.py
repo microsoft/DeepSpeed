1359e6da1c648b61b51705e9a8c0346b9bf9b254
Jeff Rasley <jerasley@microsoft.com>
2021-11-30 14:30:48 -0800

    use rocm compatible cuda_home for cpu_adagrad

--- op_builder/cpu_adagrad.py
+++ op_builder/cpu_adagrad.py
@@ -26,8 +26,20 @@ class CPUAdagradBuilder(CUDAOpBuilder):
 
     def include_paths(self):
         import torch
-        CUDA_INCLUDE = os.path.join(torch.utils.cpp_extension.CUDA_HOME, "include")
-        return ['csrc/includes', CUDA_INCLUDE]
+        if not self.is_rocm_pytorch():
+            CUDA_INCLUDE = [os.path.join(torch.utils.cpp_extension.CUDA_HOME, "include")]
+        else:
+            CUDA_INCLUDE = [
+                os.path.join(torch.utils.cpp_extension.ROCM_HOME,
+                             "include"),
+                os.path.join(torch.utils.cpp_extension.ROCM_HOME,
+                             "include",
+                             "rocrand"),
+                os.path.join(torch.utils.cpp_extension.ROCM_HOME,
+                             "include",
+                             "hiprand"),
+            ]
+        return ['csrc/includes'] + CUDA_INCLUDE
 
     def cxx_args(self):
         import torch
