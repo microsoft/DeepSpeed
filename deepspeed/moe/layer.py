bd74c0b3eb80d04ea6c4d86c302726a8c659b753
Ammar Ahmad Awan <ammar.awan@microsoft.com>
2022-02-16 11:18:56 -0800

    Zheweiyao/moe post release residual moe (#289) (#290)
    * add residual support
    
    * fix the unit-test example
    
    Co-authored-by: yaozhewei <zheweiy@berkeley.edu>
    
    Co-authored-by: yaozhewei <zheweiy@berkeley.edu>

--- deepspeed/moe/layer.py
+++ deepspeed/moe/layer.py
@@ -25,6 +25,7 @@ class MoE(torch.nn.Module):
                  capacity_factor=1.,
                  eval_capacity_factor=1.,
                  min_capacity=4,
+                 use_residual=False,
                  noisy_gate_policy: typing.Optional[str] = None,
                  drop_tokens: bool = True,
                  use_rts=True,
@@ -48,6 +49,7 @@ class MoE(torch.nn.Module):
 
         super(MoE, self).__init__()
 
+        self.use_residual = use_residual
         self.ep_size = ep_size
         self.expert_group_name = f"ep_size_{self.ep_size}"
         self.num_experts = num_experts
@@ -74,6 +76,10 @@ class MoE(torch.nn.Module):
                                       self.expert_group_name,
                                       self.num_local_experts,
                                       use_tutel=use_tutel)
+        if self.use_residual:
+            self.mlp = expert
+            # coefficient is used for weighted sum of the output of expert and mlp
+            self.coefficient = torch.nn.Linear(hidden_size, 2)
 
     def forward(self, hidden_states, used_token=None):
         """ MoE forward
@@ -92,4 +98,12 @@ class MoE(torch.nn.Module):
             * exp_counts (int): expert count
         """
         output = self.deepspeed_moe(hidden_states, used_token)
+        if self.use_residual:
+            # Residual MoE
+            output_mlp = self.mlp(hidden_states)
+            if type(output_mlp) is tuple:
+                output_mlp = output_mlp[0]  # Ignore the bias term for now
+            coef = self.coefficient(hidden_states)
+            coef = torch.nn.functional.softmax(coef, dim=1)
+            output = output * coef[..., 0:1] + output_mlp * coef[..., 1:]
         return output, self.deepspeed_moe.l_aux, self.deepspeed_moe.exp_counts
