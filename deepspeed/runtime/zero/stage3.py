2e868184429e757cbc98a77564ee8672629f4b45
Jeff Rasley <jerasley@microsoft.com>
2022-03-23 16:37:59 -0700

    address comment

--- deepspeed/runtime/zero/stage3.py
+++ deepspeed/runtime/zero/stage3.py
@@ -117,7 +117,7 @@ def _apply_to_tensors_only(module, functional, backward_function, outputs):
     else:
         if not is_builtin_type(outputs):
             logger.warning(
-                f"A module is returning an unknown type ({type(outputs)}) at forward, backward will be skipped for any tensors embedded in this structure."
+                f"A module has unknown inputs or outputs type ({type(output)}) and the tensors embedded in it cannot be detected. The ZeRO-3 hooksdesigned to trigger before or after backward pass of the module relies on knowing the input and output tensors and therefore may not get triggered properly."
             )
         return outputs
 
