0a2ceb607072d5182c9f7c774e64f50cd7b62513
lkm2835 <lkm2835@gmail.com>
2022-04-22 02:33:47 +0000

    Fix dataloader len

--- deepspeed/runtime/dataloader.py
+++ deepspeed/runtime/dataloader.py
@@ -68,7 +68,7 @@ class DeepSpeedDataLoader(object):
         self.device_count = device_count
         self.batch_size = batch_size
         self.pin_memory = pin_memory
-        self.len = int(len(self.data_sampler) / batch_size)
+        self.len = len(self.data_sampler)
         self.data = None
         self.dataloader_drop_last = dataloader_drop_last
 
@@ -100,6 +100,8 @@ class DeepSpeedDataLoader(object):
                                          collate_fn=self.collate_fn,
                                          num_workers=self.num_local_io_workers,
                                          drop_last=self.dataloader_drop_last)
+        self.len = len(self.dataloader)
+
         self.data = (x for x in self.dataloader)
 
         return self.dataloader
