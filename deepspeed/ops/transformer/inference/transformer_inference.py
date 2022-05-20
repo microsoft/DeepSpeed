c53d2a3235743bf028db15449dfb53555c8a8e0b
Jeff Rasley <jerasley@microsoft.com>
2022-05-20 16:22:17 -0700

    fixes for neox

--- deepspeed/ops/transformer/inference/transformer_inference.py
+++ deepspeed/ops/transformer/inference/transformer_inference.py
@@ -267,11 +267,15 @@ class DeepSpeedSelfAttentionFunction(Function):
             else:
                 qkv_func = inference_cuda_module.qkv_gemm_fp16 if config.fp16 else \
                                     inference_cuda_module.qkv_gemm_fp32
-                if isinstance(attn_qkvb, torch.Tensor) and attn_qkvb.numel() == 0:
-                    attn_qkvb = None
+                if attn_qkvb is None:
+                    qkv_bias = norm_b
+                elif isinstance(attn_qkvb, torch.Tensor) and attn_qkvb.numel() == 0:
+                    qkv_bias = norm_b
+                else:
+                    qkv_bias = attn_qkvb
                 qkv_out = qkv_func(input,
                                    attn_qkvw,
-                                   (attn_qkvb if attn_qkvb is not None else norm_b),
+                                   qkv_bias,
                                    norm_w,
                                    norm_b,
                                    config.epsilon,
