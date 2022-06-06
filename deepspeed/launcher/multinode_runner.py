3678ee177886bd815c6f77e02416abaac629f76a
Michael Wyatt <michaelwyatt@microsoft.com>
2022-06-06 16:42:21 -0700

    [bug] Add user-defined launcher args for MPI launcher (#1933)
    Co-authored-by: Jeff Rasley <jerasley@microsoft.com>

--- deepspeed/launcher/multinode_runner.py
+++ deepspeed/launcher/multinode_runner.py
@@ -3,7 +3,7 @@ import sys
 import shutil
 import subprocess
 import warnings
-from shlex import quote
+from shlex import quote, split
 from abc import ABC, abstractmethod
 
 from ..utils import logger
@@ -137,7 +137,7 @@ class OpenMPIRunner(MultiNodeRunner):
             '--mca',
             'btl_tcp_if_include',
             'eth0',
-        ]
+        ] + split(self.args.launcher_args)
 
         export_cmd = []
         for k, v in self.exports.items():
@@ -227,7 +227,7 @@ class MVAPICHRunner(MultiNodeRunner):
             f'{process_per_node}',
             '--hostfile',
             f'{MVAPICH_TMP_HOSTFILE}',
-        ]
+        ] + split(self.args.launcher_args)
 
         export_cmd = []
         for k, v in self.exports.items():
