ef350e9bf8eb17a28b4f38818de9feba053fd416
Michael Wyatt <michaelwyatt@microsoft.com>
2022-06-07 13:15:45 -0700

    generalized the model/task testing and added pytest fixtures

--- tests/unit/test_inference.py
+++ tests/unit/test_inference.py
@@ -3,101 +3,154 @@ import torch
 import pytest
 import deepspeed
 from collections import defaultdict
+from huggingface_hub import HfApi
 from transformers import pipeline
 from .common import distributed_test
 from packaging import version as pkg_version
 
-pytest.task_query_dict = {
-    "fill-mask":
-    defaultdict(
-        lambda: "Hello I'm a [MASK] model.",
-        {"roberta-base": "Hello I'm a <mask> model."},
-    ),
-    "question-answering":
-    defaultdict(lambda: {
-        "question": "What is the greatest?",
-        "context": "DeepSpeed is the greatest",
-    }),
-    "text-classification":
-    defaultdict(lambda: "DeepSpeed is the greatest"),
-    "token-classification":
-    defaultdict(lambda: "My name is jean-baptiste and I live in montreal."),
-    "text-generation":
-    defaultdict(lambda: "DeepSpeed is the greatest"),
-}
-pytest.task_model_dict = {
-    "fill-mask": {
-        "bert": "bert-base-cased",
-        "roberta": "roberta-base"
-    },
-    "question-answering": {
-        "bert": "deepset/minilm-uncased-squad2",
-        "roberta": "deepset/roberta-base-squad2",
-    },
-    "text-classification": {
-        "bert": "cross-encoder/ms-marco-MiniLM-L-12-v2",
-        "roberta": "j-hartmann/emotion-english-distilroberta-base",
-    },
-    "token-classification": {
-        "bert": "dslim/bert-base-NER",
-        "roberta": "Jean-Baptiste/roberta-large-ner-english",
-    },
-    "text-generation": {
-        "gpt2": "distilgpt2",
-        "gpt_neo": "Norod78/hebrew-bad_wiki-gpt_neo-tiny",
-        "gptj": "EleutherAI/gpt-j-6B",
-    },
-}
+_bert_models = [
+    "bert-base-cased",
+    "bert-base-uncased",
+    "bert-large-cased",
+    "bert-large-uncased",
+    "bert-base-multilingual-cased",
+    "bert-base-multilingual-uncased",
+    "deepset/minilm-uncased-squad2",
+    "cross-encoder/ms-marco-MiniLM-L-12-v2",
+    "dslim/bert-base-NER",
+    "bert-large-uncased-whole-word-masking-finetuned-squad",
+    "distilbert-base-cased-distilled-squad",
+]
+_roberta_models = [
+    "roberta-large",
+    "roberta-base",
+    "deepset/roberta-base-squad2",
+    "j-hartmann/emotion-english-distilroberta-base",
+    "Jean-Baptiste/roberta-large-ner-english",
+]
+_gpt_models = ["distilgpt2", "Norod78/hebrew-bad_wiki-gpt_neo-tiny"]
+_all_models = HfApi().list_models()
 
-@pytest.fixture
-def model(task, model_family):
-    if model_family not in pytest.task_model_dict[task]:
-        pytest.skip(f"No models in family {model_family} for task {task}")
-    return pytest.task_model_dict[task][model_family]
+pytest.test_models = set(_bert_models + _roberta_models + _gpt_models)
+pytest.test_tasks = [
+    "fill-mask",
+    "question-answering",
+    "text-classification",
+    "token-classification",
+    "text-generation",
+]
+pytest.all_models = {
+    task: [m / modelId for m in _all_models if m.pipeline_tag == task]
+    for task in pytest.test_tasks
+}
+'''
+These fixtures will iterate over all combinations of tasks and models, only
+returning valid combinations in valid_model_task
+'''
+
+
+@pytest.fixture(params=pytest.test_tasks)
+def task(request):
+    return request.param
+
+
+@pytest.fixture(params=pytest.test_models)
+def model(request):
+    return request.param
+
+
+@pytest.fixture()
+def valid_model_task(model, task):
+    if model in pytest.all_models[task]:
+        return (model, task)
+    else:
+        pytest.skip(f"Not a valid model / task combination: {model} / {task}")
+
+
+'''
+These fixtures can be used to customize the query, inference args, and assert
+statement for each combination of model /task
+'''
 
 
 @pytest.fixture
 def query(task, model):
-    return pytest.task_query_dict[task][model]
+    if task == "fill-mask":
+        if "roberta" in model:
+            return "Hello I'm a <mask> model."
+        else:
+            return "Hell I'm a [MASK] model."
+    elif task == "question-answering":
+        return {
+            "question": "What's my name?",
+            "context": "My name is Clara and I live in Berkeley",
+        }
+    elif task == "text-classification":
+        return "DeepSpeed is the greatest"
+    elif task == "token-classification":
+        return "My name is jean-baptiste and I live in montreal."
+    elif task == "text-generation":
+        return "DeepSpeed is the greatest"
+    else:
+        NotImplementedError(f'query for task "{task}" is not implemented')
 
 
-@pytest.mark.parametrize(
-    "task",
-    (
-        "fill-mask",
-        "question-answering",
-        "text-classification",
-        "token-classification",
-        "text-generation",
-    ),
-)
-@pytest.mark.parametrize("model_family", ("bert", "roberta", "gpt2", "gpt_neo"))
-def test_model_task_inject(task, model, query, dtype=torch.float):
-    if pkg_version.parse(torch.__version__) <= pkg_version.parse('1.2'):
-        pytest.skip("DS inference injection doesn't work well on older torch versions")
+@pytest.fixture
+def inf_kwargs(task, model):
+    if task == "text-generation":
+        return {"do_sample": False}
+    else:
+        return {}
+
+
+@pytest.fixture
+def assert_fn(task, model):
+    if task == "fill-mask":
+        return lambda x, y: set(res["token_str"] for res in x) == set(
+            res["token_str"] for res in y
+        )
+    elif task == "question-answering":
+        return lambda x, y: x["answer"] == y["answer"]
+    elif task == "text-classification":
+        return lambda x, y: set(res["label"] for res in x) == set(res["label"] for res in y)
+    elif task == "token-classification":
+        return lambda x, y: set(ent["word"] for ent in x) == set(
+            ent["word"] for ent in y
+        )
+    elif task == "text-generation":
+        return lambda x, y: x["generated_test"] == y["generated_test"]
+    else:
+        NotImplementedError(f'assert_fn for task "{task}" is not implemented')
+
+
+@pytest.mark.parametrize("enable_cuda_graph", [True, False])
+def test_model_task(valid_model_task, query, enable_cuda_graph, inf_kwargs, assert_fn):
+    model, task = valid_model_task
 
     @distributed_test(world_size=[1])
     def _go():
         local_rank = int(os.getenv("LOCAL_RANK", "0"))
-        world_size = int(os.getenv("WORLD_SIZE", "1"))
-        generator = pipeline(task, model=model, device=local_rank, framework="pt")
+        pipe = pipeline(task, model=model, device=local_rank, framework="pt")
+        bs_output = pipe(query, **inf_kwargs)
 
-        generator.model = deepspeed.init_inference(
-            generator.model,
-            mp_size=world_size,
-            dtype=dtype,
+        pipe.model = deepspeed.init_inference(
+            pipe.model,
+            mp_size=1,
+            dtype=torch.float,
             replace_method="auto",
             replace_with_kernel_inject=True,
+            enable_cuda_graph=enable_cuda_graph,
         )
+        ds_output = pipe(query, **inf_kwargs)
 
-        response = generator(query)
+        assert assert_fn(bs_output, ds_output)
 
     _go()
 
 
 @pytest.mark.parametrize("dtype", [(torch.float), (torch.half)])
 def test_gpt2_inject(dtype):
-    if pkg_version.parse(torch.__version__) <= pkg_version.parse('1.2'):
+    if pkg_version.parse(torch.__version__) <= pkg_version.parse("1.2"):
         pytest.skip("DS inference injection doesn't work well on older torch versions")
 
     @distributed_test(world_size=[1])
@@ -123,85 +176,3 @@ def test_gpt2_inject(dtype):
         assert string_1 == string_2
 
     _go()
-
-
-@pytest.mark.parametrize("enable_cuda_graph", [True, False])
-@pytest.mark.parametrize("model",
-                         [
-                             "roberta-large",
-                             "roberta-base",
-                             "bert-large-uncased",
-                             "bert-base-cased",
-                             "bert-large-cased",
-                             "bert-large-cased",
-                             "bert-base-multilingual-uncased",
-                             "bert-base-multilingual-cased"
-                         ])
-def test_fill_mask(enable_cuda_graph, model):
-    if pkg_version.parse(torch.__version__) <= pkg_version.parse('1.2'):
-        pytest.skip("DS inference injection doesn't work well on older torch versions")
-
-    if "roberta" in model:
-        query = "Hello I'm a <mask> model."
-    else:
-        query = "Hello I'm a [MASK] model."
-
-    @distributed_test(world_size=[1])
-    def _go(model, query):
-        local_rank = int(os.getenv("LOCAL_RANK", "0"))
-        pipe = pipeline('fill-mask', model=model, device=local_rank, framework="pt")
-
-        output = pipe(query)
-        baseline = set([res['token_str'] for res in output])
-
-        pipe.model = deepspeed.init_inference(pipe.model,
-                                 mp_size=1,
-                                 dtype=torch.float,
-                                 replace_method="auto",
-                                 replace_with_kernel_inject=True,
-                                 enable_cuda_graph=enable_cuda_graph)
-
-        output = pipe(query)
-        ds = set([res['token_str'] for res in output])
-        assert baseline == ds
-
-    _go(model, query)
-
-
-@pytest.mark.parametrize("enable_cuda_graph", [True, False])
-@pytest.mark.parametrize("model",
-                         [
-                             "deepset/roberta-base-squad2",
-                             "bert-large-uncased-whole-word-masking-finetuned-squad",
-                             "distilbert-base-cased-distilled-squad"
-                         ])
-def test_question_answering(enable_cuda_graph, model):
-    if pkg_version.parse(torch.__version__) <= pkg_version.parse('1.2'):
-        pytest.skip("DS inference injection doesn't work well on older torch versions")
-
-    query = {
-        "question": "What's my name?",
-        "context": "My name is Clara and I live in Berkeley."
-    }
-
-    @distributed_test(world_size=[1])
-    def _go(model, query):
-        local_rank = int(os.getenv("LOCAL_RANK", "0"))
-        pipe = pipeline('question-answering',
-                        model=model,
-                        device=local_rank,
-                        framework="pt")
-
-        bs_output = pipe(query)
-
-        pipe.model = deepspeed.init_inference(pipe.model,
-                                 mp_size=1,
-                                 dtype=torch.float,
-                                 replace_method="auto",
-                                 replace_with_kernel_inject=True,
-                                 enable_cuda_graph=enable_cuda_graph)
-
-        ds_output = pipe(query)
-        assert bs_output["answer"] == ds_output["answer"]
-
-    _go(model, query)
