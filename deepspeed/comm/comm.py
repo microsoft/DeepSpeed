3c5e065ee003248076f9cc5d17aa05405bfdb629
Quentin Anthony <qganthony@yahoo.com>
2022-06-10 02:01:46 +0500

    Clean up comm.py logic

--- deepspeed/comm/comm.py
+++ deepspeed/comm/comm.py
@@ -119,10 +119,12 @@ def is_available() -> bool:
 
 def set_backend(backend):
     if not use_ds_backend:
-        utils.logger.warn(
-            "DeepSpeed communication backend is required. Please use deepspeed.comm.init_process_group(backend, use_deepspeed=True) to use this functionality"
+        utils.logger.error(
+            "DeepSpeed communication backend is required. Please use deepspeed.comm.init_distributed(backend, use_deepspeed=True) to use this functionality"
         )
-        return
+        raise RuntimeError(
+            'Error: Custom DeepSpeed backend called without initializing DeepSpeed distributed.'
+        )
 
     global cdb
     global nccl_backend
@@ -411,21 +413,17 @@ def init_distributed(dist_backend="nccl",
             else:
                 mpi_discovery(distributed_port=distributed_port, verbose=verbose)
 
-        if cdb is None or not cdb.is_initialized():
-            if verbose and int(os.getenv('RANK', '0')) == 0:
-                utils.logger.info(
-                    "Initializing torch distributed with backend: {}".format(
-                        dist_backend))
+        if cdb is not None and cdb.is_initialized():
+            if int(os.getenv('RANK', '0')) == 0:
+                utils.logger.info('Distributed backend already initialized')
+        else:
             assert isinstance(timeout, timedelta)
-
-            if cdb is not None and cdb.is_initialized():
-                if int(os.getenv('RANK', '0')) == 0:
-                    utils.logger.info('Distributed backend already initialized')
-            else:
-                if int(os.getenv('RANK', '0')) == 0:
-                    utils.logger.info('Initializing TorchBackend in DeepSpeed')
-                # Create a torch backend object, initialize torch distributed, and assign to cdb
-                cdb = TorchBackend(dist_backend, timeout, init_method)
+            if int(os.getenv('RANK', '0')) == 0:
+                utils.logger.info(
+                    'Initializing TorchBackend in DeepSpeed with backend {}'.format(
+                        dist_backend))
+            # Create a torch backend object, initialize torch distributed, and assign to cdb
+            cdb = TorchBackend(dist_backend, timeout, init_method)
 
 
 def mpi_discovery(distributed_port=TORCH_DISTRIBUTED_DEFAULT_PORT, verbose=True):
