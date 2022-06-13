f99fc0d78f70b64735423b3ae05245bd7ea2b850
Jeff Rasley <jerasley@microsoft.com>
2022-06-13 12:53:51 -0700

    during injection reshape data on cpu to avoid extra GPU memory

--- deepspeed/module_inject/replace_module.py
+++ deepspeed/module_inject/replace_module.py
@@ -351,8 +351,11 @@ def replace_transformer_layer(orig_layer_impl,
             # linear layer is created with [input, output] shape
             # transpose it here to reduce inference cost!
             def transpose(data):
+                # temp move to cpu to avoid requiring extra GPU memory during the reshape
+                data = data.to('cpu')
                 data.reshape(-1).copy_(data.transpose(-1, -2).contiguous().reshape(-1))
                 data = data.reshape(data.shape[-1], data.shape[-2])
+                data.to(torch.cuda.current_device())
                 return data
 
             if attn_linear_layer:
