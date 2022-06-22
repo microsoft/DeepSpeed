669d4750058cb75e61a404d8326f88925725856f
root <root@MININT-MQV725J.fareast.corp.microsoft.com>
2022-06-22 10:34:56 +0800

    ignore post-operation for zero ckpt name

--- deepspeed/runtime/engine.py
+++ deepspeed/runtime/engine.py
@@ -2774,23 +2774,25 @@ class DeepSpeedEngine(Module):
             mp_rank=mp_rank,
             dp_world_size=self.loaded_checkpoint_dp_world_size,
             bf16_mode=bf16_mode)
-        invalid_zero_ckpt_paths = []
-        for i, ckpt_name in enumerate(zero_ckpt_names):
-            if not os.path.exists(ckpt_name):
-                # transparently handle the old file pattern for optim_states
-                if "optim_states.pt" in ckpt_name:
-                    ckpt_name_try = ckpt_name.replace("_optim_states.pt",
-                                                      "optim_states.pt")
-                    if os.path.exists(ckpt_name_try):
-                        zero_ckpt_names[i] = ckpt_name_try
-                        continue
-                invalid_zero_ckpt_paths.append(ckpt_name)
+        
+        if not self.enable_nebula:
+            invalid_zero_ckpt_paths = []
+            for i, ckpt_name in enumerate(zero_ckpt_names):
+                if not os.path.exists(ckpt_name):
+                    # transparently handle the old file pattern for optim_states
+                    if "optim_states.pt" in ckpt_name:
+                        ckpt_name_try = ckpt_name.replace("_optim_states.pt",
+                                                        "optim_states.pt")
+                        if os.path.exists(ckpt_name_try):
+                            zero_ckpt_names[i] = ckpt_name_try
+                            continue
+                    invalid_zero_ckpt_paths.append(ckpt_name)
 
-        if len(invalid_zero_ckpt_paths) > 0:
-            logger.warn(
-                f"The following zero checkpoints paths are missing: {invalid_zero_ckpt_paths}"
-            )
-            return None
+            if len(invalid_zero_ckpt_paths) > 0:
+                logger.warn(
+                    f"The following zero checkpoints paths are missing: {invalid_zero_ckpt_paths}"
+                )
+                return None
 
         return zero_ckpt_names
 
