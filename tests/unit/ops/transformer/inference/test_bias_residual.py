508dd96a6a04b79d55ad1341e9cf467b46a0d4c6
GuanhuaWang <guanhua@cs.berkeley.edu>
2022-09-06 20:01:26 +0000

    format fix

--- tests/unit/ops/transformer/inference/test_bias_residual.py
+++ tests/unit/ops/transformer/inference/test_bias_residual.py
@@ -10,11 +10,13 @@ if not deepspeed.ops.__compatible_ops__[InferenceBuilder.NAME]:
 inference_module = None
 torch_minor_version = None
 
-def allclose(x,y):
+
+def allclose(x, y):
     assert x.dtype == y.dtype
     assert x.shape == y.shape
     rtol, atol = {torch.float32: (5e-4, 5e-5), torch.float16: (3e-2, 2e-3)}[x.dtype]
-    return torch.allclose(x,y, rtol=rtol, atol=atol)
+    return torch.allclose(x, y, rtol=rtol, atol=atol)
+
 
 def run_bias_residual_reference(inputs, residuals, bias):
     return inputs
@@ -28,6 +30,7 @@ def run_bias_residual_ds(inputs, residuals, bias):
         return inference_module.bias_residual_fp16(inputs, residuals, bias)
     else:
         return inference_module.bias_residual_fp32(inputs, residuals, bias)
+
 
 @pytest.mark.inference
 @pytest.mark.parametrize("batch", [1, 2])
@@ -45,4 +48,4 @@ def test_bias_residual(batch, sequence, channels, dtype):
 
     ds_out = run_bias_residual_ds(inputs_ds, residuals_ds, bias_ds)
     ref_out = run_bias_residual_reference(inputs_ref, residuals_ref, bias_ref)
-    assert(allclose(ds_out, ref_out))
+    assert (allclose(ds_out, ref_out))
