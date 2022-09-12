1ebd819cdd0c18ca671b0bb74e8d84a96be9f3a5
GuanhuaWang <guanhua@cs.berkeley.edu>
2022-09-12 22:23:48 +0000

    single param

--- tests/unit/ops/quantizer/test_quant.py
+++ tests/unit/ops/quantizer/test_quant.py
@@ -34,23 +34,20 @@ def run_quant_dequant(inputs, groups, bits):
 
 
 @pytest.mark.inference
-@pytest.mark.parametrize("small_tensor_shape", [8, 8])
-@pytest.mark.parametrize("big_tensor_shape", [128, 256])
-def test_quant_dequant(small_tensor_shape, big_tensor_shape):
+@pytest.mark.parametrize("tensor_shape", [(8, 8), (128, 256)])
+def test_quant_dequant(tensor_shape):
+    input_tensor = torch.rand((tensor_shape), dtype=torch.float16).cuda()
 
-    input_small_tensor = torch.rand((small_tensor_shape), dtype=torch.float16).cuda()
-    input_big_tensor = torch.rand((big_tensor_shape), dtype=torch.float16).cuda()
-
-    # test 8bit quant/dequant on 8x8 small tensor partitioned in 1 group.
-    ref_input_small_8bit_1group = input_small_tensor.clone().detach()
-    ref_out_small_8bit_1group = quantize_dequantize_ref(ref_input_small_8bit_1group, 8)
+    # test 8bit quant/dequant on tensor partitioned in 1 group.
+    ref_input_8bit_1group = input_tensor.clone().detach()
+    ref_out_8bit_1group = quantize_dequantize_ref(ref_input_8bit_1group, 8)
     # run_quant_dequant will do quantize then dequantize and return the dequantized value.
-    ds_out_small_8bit_1group = run_quant_dequant(input_small_tensor, 1, 8)
-    assert (allclose(ds_out_small_8bit_1group, ref_out_small_8bit_1group))
+    ds_out_8bit_1group = run_quant_dequant(input_tensor, 1, 8)
+    assert (allclose(ds_out_8bit_1group, ref_out_8bit_1group))
 
-    # test 4bit quant/dequant on 128x256 big tensor partitioned into 16 groups.
+    # test 4bit quant/dequant on tensor partitioned into 16 groups.
     # Note that we have an explicit boundary for groups as ((size / groups) - 1) / 4096 + 1) <= MAX_REG.
-    ref_input_big_4bit_16group = input_big_tensor.clone().detach()
-    ref_out_big_4bit_16group = quantize_dequantize_ref(ref_input_big_4bit_16group, 4, 16)
-    ds_out_big_4bit_16group = run_quant_dequant(input_big_tensor, 16, 4)
-    assert (allclose(ds_out_big_4bit_16group, ref_out_big_4bit_16group))
+    ref_input_4bit_16group = input_tensor.clone().detach()
+    ref_out_4bit_16group = quantize_dequantize_ref(ref_input_4bit_16group, 4, 16)
+    ds_out_4bit_16group = run_quant_dequant(input_tensor, 16, 4)
+    assert (allclose(ds_out_4bit_16group, ref_out_4bit_16group))
