40b4b816459774a3392e4de3b9b232c3ce97ace1
Tunji Ruwase <olruwase@microsoft.com>
2022-09-19 22:23:29 +0500

    Permit only import exceptions; Check is_available()

--- deepspeed/accelerator/real_accelerator.py
+++ deepspeed/accelerator/real_accelerator.py
@@ -18,18 +18,24 @@ def init_accelerator():
     try:
         # TODO: This import should reference an external module to DeepSpeed
         from deepspeed.accelerator.cuda_accelerator import CUDA_Accelerator
-        ds_accelerator = CUDA_Accelerator()
-        return
-    except:
+    except ImportError as e:
         pass
+    else:
+        ds_accelerator = CUDA_Accelerator()
+        assert ds_accelerator.is_available(), \
+            f'CUDA_Accelerator fails is_available() test (import was successful)'
+        return
 
     try:
         # TODO: This import should reference an external module to DeepSpeed
         from deepspeed.accelerator.xpu_accelerator import XPU_Accelerator
-        ds_accelerator = XPU_Accelerator()
-        return
-    except:
+    except ImportError as e:
         pass
+    else:
+        ds_accelerator = XPU_Accelerator()
+        assert ds_accelerator.is_available(), \
+            f'XPU_Accelerator fails is_available() test (import was successful)'
+        return
 
     assert ds_accelerator is not None, f'failed to instantiate a DeepSpeed accelerator'
 
