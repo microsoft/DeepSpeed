dca97c25dcb0a001d9077f55a7134dfb6f8853a0
molly-smith <mosm@microsoft.com>
2022-09-23 18:29:40 +0000

    Keep running tests even if errors

--- benchmarks/inference/sweep.sh
+++ benchmarks/inference/sweep.sh
@@ -1,4 +1,4 @@
-set -ex
+set -x
 
 export TRANSFORMERS_CACHE=/tmp/hf-cache
 
@@ -8,10 +8,10 @@ branch2=$2
 gptneo_models="EleutherAI/gpt-neo-2.7B EleutherAI/gpt-neo-1.3B EleutherAI/gpt-neo-125M"
 gpt2_models="gpt2 gpt2-large gpt2-xl"
 gptj_models="EleutherAI/gpt-j-6B"
-opt_models="facebook/opt-125m facebook/opt-1.3b facebook/opt-2.7b facebook/opt-6.7b facebook/opt-13b"
-bloom_models="bigscience/bloom-560m bigscience/bloom-1b7 bigscience/bloom-3b bigscience/bloom-7b1"
+opt_models=""
+bloom_models=""
 
-for gpus in `echo "1 2 4 8"`; do
+for gpus in `echo "1"`; do
     for dtype in `echo "fp16 fp32"`; do
         for graphs in `echo "true false"`; do
             for kernel in `echo "true false"`; do
