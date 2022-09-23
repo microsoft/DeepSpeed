64c27122211d4bdca659cca48e41ee091d581e88
Jeff Rasley <jerasley@microsoft.com>
2022-09-22 17:51:01 -0700

    bug fix for running bert w/o kernels

--- deepspeed/module_inject/replace_module.py
+++ deepspeed/module_inject/replace_module.py
@@ -687,7 +687,7 @@ def replace_transformer_layer(orig_layer_impl,
                 weight_shape = child.weight.ds_shape
             else:
                 weight_shape = child.weight.shape
-            if name in all_reduce_linears:
+            if isinstance(all_reduce_linears, dict) and name in all_reduce_linears:
                 new_weight = torch.empty((
                     weight_shape[1] if conv_linear_layer else weight_shape[0],
                     (weight_shape[0] if conv_linear_layer else weight_shape[1]) //
