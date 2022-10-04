2dbf0a4ff3f97db382bb14016543b50943d4759d
Adam Moody <moody20@llnl.gov>
2022-10-03 18:43:08 -0700

    enable parallel layer write according to config param

--- deepspeed/runtime/pipe/module.py
+++ deepspeed/runtime/pipe/module.py
@@ -563,14 +563,23 @@ class PipelineModule(nn.Module):
 
     def save_state_dict(self, save_dir, checkpoint_engine):
         # Processes having the same model parallel rank on different data parallel instances
-        # have identical layer weights.  We distribute the task of saving the layer weights
+        # have identical layer weights.  We can distribute the task of saving the layer weights
         # among the data parallel ranks.  For example, if a pipeline stage has 9 layers and
         # if there are 2 data parallel instances, rank 0 will save the first 5 layers and
         # rank 1 will save the last 4.
         dp_rank = self._grid.data_parallel_id
         dp_size = self._grid.data_parallel_size
-        offsets = ds_utils.partition_uniform(len(self.forward_funcs), dp_size)
-        start, end = offsets[dp_rank], offsets[dp_rank + 1]
+        num_layers = len(self.forward_funcs)
+        if self.checkpoint_parallel_write_pipeline:
+            # spread layers evenly across data parallel ranks
+            offsets = ds_utils.partition_uniform(num_layers, dp_size)
+            start, end = offsets[dp_rank], offsets[dp_rank + 1]
+        else:
+            # assign all layers to rank 0
+            if dp_rank == 0:
+                start, end = 0, num_layers
+            else:
+                start, end = 0, 0
         layer_list = self.forward_funcs[start:end]
 
         os.makedirs(save_dir, exist_ok=True)
