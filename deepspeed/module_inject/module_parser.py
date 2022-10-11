6ab4e095b8997ea43f529d10dc315627de764df3
molly-smith <mosm@microsoft.com>
2022-10-11 21:17:23 +0000

    Cleanup

--- deepspeed/module_inject/module_parser.py
+++ deepspeed/module_inject/module_parser.py
@@ -23,10 +23,9 @@ args = parser.parse_args()
 
 def get_class_source(node, class_name):
     classes = [n for n in node.body if isinstance(n, ast.ClassDef)]
-
     for class_ in classes:
         if class_.name == class_name:
-            print("Class name:", class_.name)
+            #print("Class name:", class_.name)
             methods = [n for n in class_.body if isinstance(n, ast.FunctionDef)]
             for method in methods:
                 if method.name == "__init__":
@@ -37,17 +36,11 @@ def get_class_source(node, class_name):
 def get_linear_layers(method_source):
     matches = re.findall(r"self.(.*?) = nn.Linear", method_source)
     if not matches:
-        #check for attributes at layer class level
-        #print("No linear layers found, checking attributes...")
-        #matches = re.findall(r"self.(.*?) = (.*?)\(", method_source)
+        #check for attributes at block class level     
         matches = re.findall(r"self.(.*?).append\((.*?)\(", method_source)
         if not matches:
-            #check for attributes at block class level
-            #print("Checking layer class level...")
-            #matches = re.findall(r"self.(.*?).append\((.*?)\(", method_source)
+            #check for attributes at layer class level
             matches = re.findall(r"self.(.*?) = (\w*?)\(", method_source)
-            #if not matches:
-                #print("Warning: No matches found")
         return False, matches
     else:
         return True, matches        
@@ -59,12 +52,13 @@ def update_name_list(name, matches):
         new_list = new_list + [name + "." + match] 
     return new_list
 
+
 def update_name_list_2(name, matches):
     new_list = []
     for match_name, match_attribute in matches:
         new_list.append(tuple([name + "." + match_name, match_attribute]))
-        print("HERE", new_list)
     return new_list
+
 
 def check_matches(matches):
     new_matches = []
@@ -73,20 +67,17 @@ def check_matches(matches):
         if source is not None:
             result, i_matches = get_linear_layers(source)
             if result:
-                #add linear layers to list
-                name_list.append(name)
+                #add linear layers to list   
                 i_matches = update_name_list(name, i_matches)
                 linear_layer_list.append(i_matches)
             if not result:
                 #add next level of class methods to check
-                name_list.append(name)
                 i_matches = update_name_list_2(name, i_matches) 
                 new_matches = new_matches + i_matches
     return new_matches
 
 
 if __name__ == "__main__":
-    name_list = []
     linear_layer_list = []
 
     #parse file for specified module
@@ -99,53 +90,20 @@ if __name__ == "__main__":
 
     #check for linear layers in source code        
     result, matches = get_linear_layers(source)
-    
 
     #if no linear layers found, check attribute source code
     if not result:
         while len(matches):
             print("checking matches...", matches)
             matches = check_matches(matches)
-
     else:
         #add linear layers to list
         update_name_list(name, matches)
         linear_layer_list.append(matches)
 
     #generate injection policy gems from name and linear layer lists
-    print(name_list)
     print(linear_layer_list)
 
     for group in linear_layer_list:
         print(group[-1])
-            
-    #functions = [n for n in node.body if isinstance(n, ast.FunctionDef)]
-    #classes = [n for n in node.body if isinstance(n, ast.ClassDef)]
-
-    #for function in functions:
-    #    show_info(function)
-
-    #for class_ in classes:
-    #    if class_.name == args.module:
-    #        print("Class name:", class_.name)
-    #        methods = [n for n in class_.body if isinstance(n, ast.FunctionDef)]
-    #        for method in methods:
-    #            if method.name == "__init__":
-                    #show_info(method)
-    #                method_source = ast.get_source_segment(file_content, method)
-    #                print(method_source)
-    #                get_linear_layers(class_.name, method_source)
-
-
-
-    #inside module, look for "self.<name1> = <submodle>(...)"
-        #in submodule, parse for "self.<name2> = nn.Linear(...)"
-        #the last match is used to generate injection policy name1.name2
-
-    #else look for "self.layer.append(<layermodule>(...))"
-        #In layermodule, look for "self.<name1> = submodule(...)"
-            #in submodule, parse for "self.<name2> = nn.Linear(...)"
-            #the last match is used to generate injection policy name1.name2
-
-    #more generic approach:
-    #always look for linear layer first, if none look for submodules and go level deeper
+    
