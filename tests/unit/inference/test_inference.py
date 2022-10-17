48b6f0887ea849652a07f8ee827077f550b4a179
Lev Kurilenko <lekurile@microsoft.com>
2022-10-17 21:40:10 +0000

    world_size [1,2]

--- tests/unit/inference/test_inference.py
+++ tests/unit/inference/test_inference.py
@@ -359,17 +359,15 @@ class TestMPSize(DistributedTest):
     ids=["t5",
          "roberta"],
 )
-#@pytest.mark.parametrize("world_size", [1, 4])
+@pytest.mark.parametrize("world_size", [1, 2])
 @pytest.mark.parametrize("dtype", [torch.float], ids=["fp32"])
 @pytest.mark.parametrize("enable_cuda_graph", [False], ids=["noCG"])
 class TestInjectionPolicy(DistributedTest):
-    world_size = [1, 4]
-
     def test(
         self,
         model_w_task,
         injection_policy,
-        #world_size,
+        world_size,
         query,
         inf_kwargs,
         assert_fn,
@@ -382,7 +380,6 @@ class TestInjectionPolicy(DistributedTest):
 
         model, task = model_w_task
         local_rank = int(os.getenv("LOCAL_RANK", "0"))
-        world_size = int(os.getenv('WORLD_SIZE', '4'))
 
         # We have to load these large models on CPU with pipeline because not
         # enough GPU memory
