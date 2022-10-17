1d35ee84d90273aa0e430d7a709927ae3a1cbe1d
Lev Kurilenko <lekurile@microsoft.com>
2022-10-17 17:07:12 +0000

    Add comment describing PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION environment variable

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,7 @@ from os.path import abspath, dirname, join
 import torch
 import warnings
 
+# Set this environment variable for the T5 inference unittest(s) (e.g. google/t5-v1_1-small)
 os.environ['PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION'] = 'python'
 
 # allow having multiple repository checkouts and not needing to remember to rerun
