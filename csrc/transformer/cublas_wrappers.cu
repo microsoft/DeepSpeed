764cec6b5f1407843795a3ac19481f98e8e9cc3c
Michael Wyatt <michaelwyatt@microsoft.com>
2022-10-20 15:06:32 -0700

    blas_gemm_ex strided function

--- csrc/transformer/cublas_wrappers.cu
+++ csrc/transformer/cublas_wrappers.cu
@@ -89,8 +89,7 @@ int cublas_gemm_ex(gpu_lib::blasHandle_t handle,
     return 0;
 }
 
-#ifdef __HIP_PLATFORM_HCC__
-int cublas_strided_batched_gemm(rocblas_handle handle,
+int cublas_strided_batched_gemm(gpu_lib::blasHandle_t handle,
                                 int m,
                                 int n,
                                 int k,
@@ -99,108 +98,51 @@ int cublas_strided_batched_gemm(rocblas_handle handle,
                                 const float* A,
                                 const float* B,
                                 float* C,
-                                rocblas_operation op_A,
-                                rocblas_operation op_B,
+                                gpu_lib::blasOperation_t op_A,
+                                gpu_lib::blasOperation_t op_B,
                                 int stride_A,
                                 int stride_B,
                                 int stride_C,
                                 int batch,
-                                rocblas_gemm_algo algo)
-#else
-int cublas_strided_batched_gemm(cublasHandle_t handle,
-                                int m,
-                                int n,
-                                int k,
-                                const float* alpha,
-                                const float* beta,
-                                const float* A,
-                                const float* B,
-                                float* C,
-                                cublasOperation_t op_A,
-                                cublasOperation_t op_B,
-                                int stride_A,
-                                int stride_B,
-                                int stride_C,
-                                int batch,
-                                cublasGemmAlgo_t algo)
-#endif
-{
-#ifdef __HIP_PLATFORM_HCC__
-    rocblas_status status =
-        rocblas_gemm_strided_batched_ex(handle,
-                                        op_A,
-                                        op_B,
-                                        m,
-                                        n,
-                                        k,
-                                        alpha,
-                                        A,
-                                        rocblas_datatype_f32_r,
-                                        (op_A == rocblas_operation_none) ? m : k,
-                                        stride_A,
-                                        B,
-                                        rocblas_datatype_f32_r,
-                                        (op_B == rocblas_operation_none) ? k : n,
-                                        stride_B,
-                                        beta,
-                                        C,
-                                        rocblas_datatype_f32_r,
-                                        m,
-                                        stride_C,
-                                        C,
-                                        rocblas_datatype_f32_r,
-                                        m,
-                                        stride_C,
-                                        batch,
-                                        rocblas_datatype_f32_r,
-                                        algo,
-                                        0,
-                                        0);
-#else
-    cublasStatus_t status = cublasGemmStridedBatchedEx(handle,
-                                                       op_A,
-                                                       op_B,
-                                                       m,
-                                                       n,
-                                                       k,
-                                                       alpha,
-                                                       A,
-                                                       CUDA_R_32F,
-                                                       (op_A == CUBLAS_OP_N) ? m : k,
-                                                       stride_A,
-                                                       B,
-                                                       CUDA_R_32F,
-                                                       (op_B == CUBLAS_OP_N) ? k : n,
-                                                       stride_B,
-                                                       beta,
-                                                       C,
-                                                       CUDA_R_32F,
-                                                       m,
-                                                       stride_C,
-                                                       batch,
-                                                       CUDA_R_32F,
-                                                       algo);
-#endif
+                                gpu_lib::blasGemmAlgo_t algo) gpu_lib::blasStatus_t status =
+    gpu_lib::blasGemmStridedBatchedEx(handle,
+                                      op_A,
+                                      op_B,
+                                      m,
+                                      n,
+                                      k,
+                                      alpha,
+                                      A,
+                                      gpu_lib::DT_R_32F,
+                                      (op_A == gpu_lib::BLAS_OP_N) ? m : k,
+                                      stride_A,
+                                      B,
+                                      gpu_lib::DT_R_32F,
+                                      (op_B == gpu_lib::BLAS_OP_N) ? k : n,
+                                      stride_B,
+                                      beta,
+                                      C,
+                                      gpu_lib::DT_R_32F,
+                                      m,
+                                      stride_C,
+                                      batch,
+                                      gpu_lib::DT_R_32F,
+                                      algo);
 
-#ifdef __HIP_PLATFORM_HCC__
-    if (status != rocblas_status_success) {
-#else
-    if (status != CUBLAS_STATUS_SUCCESS) {
-#endif
-        fprintf(stderr,
-                "!!!! kernel execution error. (batch: %d, m: %d, n: %d, k: %d, error: %d) \n",
-                batch,
-                m,
-                n,
-                k,
-                (int)status);
-        return EXIT_FAILURE;
-    }
-    return 0;
+if (status != gpu_lib::BLAS_STATUS_SUCCESS) {
+    fprintf(stderr,
+            "!!!! kernel execution error. (batch: %d, m: %d, n: %d, k: %d, error: %d) \n",
+            batch,
+            m,
+            n,
+            k,
+            (int)status);
+    return EXIT_FAILURE;
+}
+return 0;
 }
 
-#ifdef __HIP_PLATFORM_HCC__
-int cublas_strided_batched_gemm(rocblas_handle handle,
+int cublas_strided_batched_gemm(gpu_lib::blasHandle_t handle,
                                 int m,
                                 int n,
                                 int k,
@@ -209,94 +151,39 @@ int cublas_strided_batched_gemm(rocblas_handle handle,
                                 const __half* A,
                                 const __half* B,
                                 __half* C,
-                                rocblas_operation op_A,
-                                rocblas_operation op_B,
+                                gpu_lib::blasOperation_t op_A,
+                                gpu_lib::blasOperation_t op_B,
                                 int stride_A,
                                 int stride_B,
                                 int stride_C,
                                 int batch,
-                                rocblas_gemm_algo algo)
-#else
-int cublas_strided_batched_gemm(cublasHandle_t handle,
-                                int m,
-                                int n,
-                                int k,
-                                const float* alpha,
-                                const float* beta,
-                                const __half* A,
-                                const __half* B,
-                                __half* C,
-                                cublasOperation_t op_A,
-                                cublasOperation_t op_B,
-                                int stride_A,
-                                int stride_B,
-                                int stride_C,
-                                int batch,
-                                cublasGemmAlgo_t algo)
-#endif
+                                gpu_lib::blasGemmAlgo_t algo)
 {
-#ifdef __HIP_PLATFORM_HCC__
-    rocblas_status status =
-        rocblas_gemm_strided_batched_ex(handle,
-                                        op_A,
-                                        op_B,
-                                        m,
-                                        n,
-                                        k,
-                                        alpha,
-                                        A,
-                                        rocblas_datatype_f16_r,
-                                        (op_A == rocblas_operation_none) ? m : k,
-                                        stride_A,
-                                        B,
-                                        rocblas_datatype_f16_r,
-                                        (op_B == rocblas_operation_none) ? k : n,
-                                        stride_B,
-                                        beta,
-                                        C,
-                                        rocblas_datatype_f16_r,
-                                        m,
-                                        stride_C,
-                                        C,
-                                        rocblas_datatype_f16_r,
-                                        m,
-                                        stride_C,
-                                        batch,
-                                        rocblas_datatype_f32_r,
-                                        algo,
-                                        0,
-                                        0);
-#else
-    cublasStatus_t status = cublasGemmStridedBatchedEx(handle,
-                                                       op_A,
-                                                       op_B,
-                                                       m,
-                                                       n,
-                                                       k,
-                                                       alpha,
-                                                       A,
-                                                       CUDA_R_16F,
-                                                       (op_A == CUBLAS_OP_N) ? m : k,
-                                                       stride_A,
-                                                       B,
-                                                       CUDA_R_16F,
-                                                       (op_B == CUBLAS_OP_N) ? k : n,
-                                                       stride_B,
-                                                       beta,
-                                                       C,
-                                                       CUDA_R_16F,
-                                                       m,
-                                                       stride_C,
-                                                       batch,
-                                                       CUDA_R_32F,
-                                                       algo);
-#endif
-
-#ifdef __HIP_PLATFORM_HCC__
-    if (status != rocblas_status_success) {
-#else
-    if (status != CUBLAS_STATUS_SUCCESS) {
-#endif
+    gpu_lib::blasStatus_t status =
+        gpu_lib::blasGemmStridedBatchedEx(handle,
+                                          op_A,
+                                          op_B,
+                                          m,
+                                          n,
+                                          k,
+                                          alpha,
+                                          A,
+                                          gpu_lib::DT_R_16F,
+                                          (op_A == gpu_lib::BLAS_OP_N) ? m : k,
+                                          stride_A,
+                                          B,
+                                          gpu_lib::DT_R_16F,
+                                          (op_B == gpu_lib::BLAS_OP_N) ? k : n,
+                                          stride_B,
+                                          beta,
+                                          C,
+                                          gpu_lib::DT_R_16F,
+                                          m,
+                                          stride_C,
+                                          batch,
+                                          gpu_lib::DT_R_32F,
+                                          algo);
+    if (status != gpu_lib::BLAS_STATUS_SUCCESS) {
         fprintf(stderr,
                 "!!!! kernel execution error. (m: %d, n: %d, k: %d, error: %d) \n",
                 m,
