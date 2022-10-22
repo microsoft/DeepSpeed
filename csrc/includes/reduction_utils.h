d5ca7969310c988526517c1a53002ee818e3888d
cmikeh2 <connorholmes@microsoft.com>
2022-10-22 03:43:47 +0000

    Add element reduction, partioned_block reduction

--- csrc/includes/reduction_utils.h
+++ csrc/includes/reduction_utils.h
@@ -32,6 +32,13 @@ and performs that reduction operation on that variable. The reductions
 of each of the arguments are completely independent of each other (
 i.e., the val1-op1 combination has no impact on val2-op2).
 
+Example usage:
+``` cpp
+float max_val;
+float min_val;
+reduce::block<rop::Max, rop::Min>(tb, warp, max_val, min_val);
+```
+
 TODO(cmikeh2): In theory, we might be able to do this sequentially with
 device functions and rely on the assembler correctly behaving. My initial
 instinct is this won't work, but if it does it would reduce implementation
@@ -67,6 +74,62 @@ DS_D_INLINE void block(cg::thread_block& tb,
                        float& val3,
                        float& val4);
 
+/*
+The partitioned block is a special case of the above where in the warps of a threadblock are
+partitioned into separate independent reductions. For example, I might have an 8 warp thread block
+in which each pair of warps is processing an independent piece of data. I would then reduce that
+data with the something like the following:
+``` cpp
+float max_val;
+reduce::partitioned_block<rop::Max, 2>(tb, warp, max_val);
+```
+After which, each pair of warps would have coherent data with each other. Note, this API will not
+provide correct results if the number of warps per partition is not a power of 2.
+*/
+template <ROpType Op, int num_warps>
+DS_D_INLINE void partitioned_block(cg::thread_block& tb,
+                                   cg::thread_block_tile<hw_warp_size>& warp,
+                                   float& val);
+
+template <ROpType Op1, ROpType Op2, int num_warps>
+DS_D_INLINE void partitioned_block(cg::thread_block& tb,
+                                   cg::thread_block_tile<hw_warp_size>& warp,
+                                   float& val1,
+                                   float& val2);
+
+template <ROpType Op1, ROpType Op2, ROpType Op3, int num_warps>
+DS_D_INLINE void partitioned_block(cg::thread_block& tb,
+                                   cg::thread_block_tile<hw_warp_size>& warp,
+                                   float& val1,
+                                   float& val2,
+                                   float& val3);
+
+template <ROpType Op1, ROpType Op2, ROpType Op3, ROpType Op4, int num_warps>
+DS_D_INLINE void partitioned_block(cg::thread_block& tb,
+                                   cg::thread_block_tile<hw_warp_size>& warp,
+                                   float& val1,
+                                   float& val2,
+                                   float& val3,
+                                   float& val4);
+
+/*
+Single element reduction primitives. Used inside serial collection
+loops.
+
+Example usage:
+using rop = reduce::OpType;
+float min = init<rop::Min>();
+for (int i = 0; i < 4; i++) {
+    min = reduce::element<rop::Min>(min, data[i]);
+}
+*/
+
+template <ROpType Op, typename T>
+DS_D_INLINE T element(const T lhs, const T rhs);
+
+template <ROpType OType, typename T = float>
+DS_D_INLINE T init();
+
 /********************** Internal reduction APIs **********************/
 
 /*
@@ -80,34 +143,91 @@ many values were already reduced into each scalar. Implementing this on top
 of reduce should be straightforward (can just wrap the sum reduction) and
 would be a good extension of the header.
 */
-template <ROpType op_type>
-DS_D_INLINE float elem_reduce(const float lhs, const float rhs);
 
-/* Element reduce implementations */
+/* Float element reduce implementations */
 template <>
-DS_D_INLINE float elem_reduce<ROpType::Add>(const float lhs, const float rhs)
+DS_D_INLINE float element<ROpType::Add>(const float lhs, const float rhs)
 {
     return lhs + rhs;
 }
 
 template <>
-DS_D_INLINE float elem_reduce<ROpType::Max>(const float lhs, const float rhs)
+DS_D_INLINE float element<ROpType::Max>(const float lhs, const float rhs)
 {
     return fmaxf(lhs, rhs);
 }
 
 template <>
-DS_D_INLINE float elem_reduce<ROpType::Min>(const float lhs, const float rhs)
+DS_D_INLINE float element<ROpType::Min>(const float lhs, const float rhs)
 {
     return fminf(lhs, rhs);
+}
+
+/* __half element reduce implementation */
+template <>
+DS_D_INLINE __half element<ROpType::Add>(const __half lhs, const __half rhs)
+{
+    return lhs + rhs;
+}
+
+template <>
+DS_D_INLINE __half element<ROpType::Max>(const __half lhs, const __half rhs)
+{
+#if __CUDA_ARCH__ >= 800
+    // Intrinsic limited to Ampere + newer
+    return __hmax(lhs, rhs);
+#else
+    return (lhs > rhs) ? lhs : rhs;
+#endif
+}
+
+template <>
+DS_D_INLINE __half element<ROpType::Min>(const __half lhs, const __half rhs)
+{
+#if __CUDA_ARCH__ >= 800
+    // Intrinsic limited to Ampere + newer
+    return __hmin(lhs, rhs);
+#else
+    return (lhs < rhs) ? lhs : rhs;
+#endif
+}
+
+/* __half2 element reduce implementation */
+template <>
+DS_D_INLINE __half2 element<ROpType::Add>(const __half2 lhs, const __half2 rhs)
+{
+    return lhs + rhs;
+}
+
+template <>
+DS_D_INLINE __half2 element<ROpType::Max>(const __half2 lhs, const __half2 rhs)
+{
+#if __CUDA_ARCH__ >= 800
+    return __hmax2(lhs, rhs);
+#else
+    __half2 ret_val;
+    ret_val.x = (lhs.x > rhs.x) ? lhs.x : rhs.x;
+    ret_val.y = (lhs.y > rhs.y) ? lhs.y : rhs.y;
+    return ret_val;
+#endif
+}
+
+template <>
+DS_D_INLINE __half2 element<ROpType::Min>(const __half2 lhs, const __half2 rhs)
+{
+#if __CUDA_ARCH__ >= 800
+    return __hmin2(lhs, rhs);
+#else
+    __half2 ret_val;
+    ret_val.x = (lhs.x < rhs.x) ? lhs.x : rhs.x;
+    ret_val.y = (lhs.y < rhs.y) ? lhs.y : rhs.y;
+    return ret_val;
+#endif
 }
 
 /*
 Reduction initialization primitives
 */
-template <ROpType OType>
-DS_D_INLINE float init();
-
 template <>
 DS_D_INLINE float init<ROpType::Add>()
 {
@@ -128,34 +248,76 @@ DS_D_INLINE float init<ROpType::Max>()
     return -INFINITY;
 }
 
-template <ROpType Op>
-DS_D_INLINE void init(float* data)
+template <>
+DS_D_INLINE __half init<ROpType::Add>()
 {
-    data[0] = init<Op>();
+    constexpr __half_raw zero = {0x0000};
+    return __half(zero);
 }
 
-template <ROpType Op1, ROpType Op2>
-DS_D_INLINE void init(float* data)
+template <>
+DS_D_INLINE __half init<ROpType::Min>()
 {
-    data[0] = init<Op1>();
-    data[1] = init<Op2>();
+    constexpr __half_raw inf = {0x7C00};
+    return __half(inf);
 }
 
-template <ROpType Op1, ROpType Op2, ROpType Op3>
-DS_D_INLINE void init(float* data)
+template <>
+__half init<ROpType::Max>()
 {
-    data[0] = init<Op1>();
-    data[1] = init<Op2>();
-    data[2] = init<Op3>();
+    constexpr __half_raw neg_inf = {0xFC00};
+    return __half(neg_inf);
 }
 
-template <ROpType Op1, ROpType Op2, ROpType Op3, ROpType Op4>
-DS_D_INLINE void init(float* data)
+template <>
+DS_D_INLINE __half2 init<ROpType::Add>()
 {
-    data[0] = init<Op1>();
-    data[1] = init<Op2>();
-    data[2] = init<Op3>();
-    data[3] = init<Op4>();
+    constexpr __half2_raw zero = {0x0000, 0x0000};
+    return __half2(zero);
+}
+
+template <>
+DS_D_INLINE __half2 init<ROpType::Min>()
+{
+    constexpr __half2_raw inf = {0x7C00, 0x7C00};
+    return __half2(inf);
+}
+
+template <>
+DS_D_INLINE __half2 init<ROpType::Max>()
+{
+    constexpr __half2_raw neg_inf = {0xFC00, 0xFC00};
+    return __half2(neg_inf);
+}
+
+template <ROpType Op, typename T>
+DS_D_INLINE void init(T* data)
+{
+    data[0] = init<Op, T>();
+}
+
+template <ROpType Op1, ROpType Op2, typename T>
+DS_D_INLINE void init(T* data)
+{
+    data[0] = init<Op1, T>();
+    data[1] = init<Op2, T>();
+}
+
+template <ROpType Op1, ROpType Op2, ROpType Op3, typename T>
+DS_D_INLINE void init(T* data)
+{
+    data[0] = init<Op1, T>();
+    data[1] = init<Op2, T>();
+    data[2] = init<Op3, T>();
+}
+
+template <ROpType Op1, ROpType Op2, ROpType Op3, ROpType Op4, typename T>
+DS_D_INLINE void init(T* data)
+{
+    data[0] = init<Op1, T>();
+    data[1] = init<Op2, T>();
+    data[2] = init<Op3, T>();
+    data[3] = init<Op4, T>();
 }
 
 /*
@@ -175,7 +337,7 @@ DS_D_INLINE void _warp(cg::thread_block_tile<hw_warp_size>& warp, float* data)
 {
 #pragma unroll
     for (int i = 1; i < reduce_width; i *= 2) {
-        data[0] = elem_reduce<Op>(data[0], warp.shfl_xor(data[0], i));
+        data[0] = element<Op>(data[0], warp.shfl_xor(data[0], i));
     }
 }
 
@@ -184,8 +346,8 @@ DS_D_INLINE void _warp(cg::thread_block_tile<hw_warp_size>& warp, float* data)
 {
 #pragma unroll
     for (int i = 1; i < reduce_width; i *= 2) {
-        data[0] = elem_reduce<Op1>(data[0], warp.shfl_xor(data[0], i));
-        data[1] = elem_reduce<Op2>(data[1], warp.shfl_xor(data[1], i));
+        data[0] = element<Op1>(data[0], warp.shfl_xor(data[0], i));
+        data[1] = element<Op2>(data[1], warp.shfl_xor(data[1], i));
     }
 }
 
@@ -194,9 +356,9 @@ DS_D_INLINE void _warp(cg::thread_block_tile<hw_warp_size>& warp, float* data)
 {
 #pragma unroll
     for (int i = 1; i < reduce_width; i *= 2) {
-        data[0] = elem_reduce<Op1>(data[0], warp.shfl_xor(data[0], i));
-        data[1] = elem_reduce<Op2>(data[1], warp.shfl_xor(data[1], i));
-        data[2] = elem_reduce<Op3>(data[2], warp.shfl_xor(data[2], i));
+        data[0] = element<Op1>(data[0], warp.shfl_xor(data[0], i));
+        data[1] = element<Op2>(data[1], warp.shfl_xor(data[1], i));
+        data[2] = element<Op3>(data[2], warp.shfl_xor(data[2], i));
     }
 }
 
@@ -205,49 +367,66 @@ DS_D_INLINE void _warp(cg::thread_block_tile<hw_warp_size>& warp, float* data)
 {
 #pragma unroll
     for (int i = 1; i < reduce_width; i *= 2) {
-        data[0] = elem_reduce<Op1>(data[0], warp.shfl_xor(data[0], i));
-        data[1] = elem_reduce<Op2>(data[1], warp.shfl_xor(data[1], i));
-        data[2] = elem_reduce<Op3>(data[2], warp.shfl_xor(data[2], i));
-        data[3] = elem_reduce<Op4>(data[3], warp.shfl_xor(data[3], i));
+        data[0] = element<Op1>(data[0], warp.shfl_xor(data[0], i));
+        data[1] = element<Op2>(data[1], warp.shfl_xor(data[1], i));
+        data[2] = element<Op3>(data[2], warp.shfl_xor(data[2], i));
+        data[3] = element<Op4>(data[3], warp.shfl_xor(data[3], i));
     }
 }
 
 /*
-Implementation for primary block reduction
+Implementation for primary block reduction that serves both `block` and
+`partitioned_block`.
+
+`local_warp_rank` refers to the warp's location within the partition, so
+for an unpartitioned threadblock this will be equivalent to
+`warp_arg.meta_group_rank()`.
+
+Similarly, the warp offset is the `local_warp_rank` of the warp with the
+lowest rank in the partition. In the case of an 8 warp block with a
+4 warp reduction, this would map to [0, 0, 0, 0, 4, 4, 4, 4].
+
+Partition size is the number of warps per partition (equal to the thread
+block in the default case). This enables us to only perform the warp reduction
+when able to.
 */
 template <int total_warps, ROpType... Ops>
 DS_D_INLINE void _block(cg::thread_block& tb,
                         cg::thread_block_tile<hw_warp_size>& warp_arg,
-                        float* data)
+                        float* data,
+                        int local_warp_rank,
+                        int warp_offset,
+                        int partition_size)
 {
     constexpr int elems = sizeof...(Ops);
     // Separated for now in case this no longer is true
     constexpr int bytes = sizeof(float);
-    // Unused when `warp_arg.meta_group_size() == 1` or total_warps == 1
-    __shared__ float reduce_buffer[total_warps * elems];
+    // Unused when `partition_size == 1` or total_warps == 1
+    __shared__ float reduce_buffer[max_warps * elems];
 
     // Always perform warp-scope reduction
     _warp<Ops...>(warp_arg, data);
 
     // If max_warps == 1 let's skip the runtime check
-    if (warp_arg.meta_group_size() > 1 && total_warps != 1) {
+    if (partition_size > 1 && total_warps != 1) {
         if (warp_arg.thread_rank() == 0) {
 #pragma unroll
             for (int i = 0; i < elems; i++) {
                 mem_access::store_shared<bytes>(
-                    reduce_buffer + i * total_warps + warp_arg.meta_group_rank(), data + i);
+                    reduce_buffer + i * max_warps + warp_offset + local_warp_rank, data + i);
             }
         }
 
         // Synchronization inside block-uniform conditional is safe
         tb.sync();
 
-        if (warp_arg.meta_group_rank() == 0) {
-            if (warp_arg.thread_rank() < warp_arg.meta_group_size()) {
+        if (local_warp_rank == 0) {
+            if (warp_arg.thread_rank() < partition_size) {
 #pragma unroll
                 for (int i = 0; i < elems; i++) {
                     mem_access::load_shared<bytes>(
-                        data + i, reduce_buffer + i * max_warps + warp_arg.thread_rank());
+                        data + i,
+                        reduce_buffer + i * max_warps + warp_offset + warp_arg.thread_rank());
                 }
             } else {
                 init<Ops...>(data);
@@ -258,7 +437,8 @@ DS_D_INLINE void _block(cg::thread_block& tb,
             if (warp_arg.thread_rank() == 0) {
 #pragma unroll
                 for (int i = 0; i < elems; i++) {
-                    mem_access::store_shared<bytes>(reduce_buffer + i, data + i);
+                    mem_access::store_shared<bytes>(reduce_buffer + i * max_warps + warp_offset,
+                                                    data + i);
                 }
             }
         }
@@ -268,7 +448,7 @@ DS_D_INLINE void _block(cg::thread_block& tb,
 
 #pragma unroll
         for (int i = 0; i < elems; i++) {
-            mem_access::load_shared<bytes>(data + i, reduce_buffer + i);
+            mem_access::load_shared<bytes>(data + i, reduce_buffer + i * max_warps + warp_offset);
         }
     }
 }
@@ -277,12 +457,13 @@ DS_D_INLINE void _block(cg::thread_block& tb,
 Main API implementations. For the most part, they just convert the individual
 variables into arrays, which makes working with them easier with a single
 implementation. In theory, we could use the `_block` implementation as another
-option, but the nature of using a pointer is a little less safe.
+option, but the nature of using a pointer is a little less safe and this allows
+us to obfuscate the details of the partitioned implementation.
 */
 template <ROpType Op, int warp_bound>
 DS_D_INLINE void block(cg::thread_block& tb, cg::thread_block_tile<hw_warp_size>& warp, float& val)
 {
-    _block<warp_bound, Op>(tb, warp, val);
+    _block<warp_bound, Op>(tb, warp, val, warp.meta_group_rank(), 0, warp.meta_group_size());
 }
 
 template <ROpType Op1, ROpType Op2, int warp_bound>
@@ -292,7 +473,7 @@ DS_D_INLINE void block(cg::thread_block& tb,
                        float& val2)
 {
     float data[2] = {val1, val2};
-    _block<warp_bound, Op1, Op2>(tb, warp, data);
+    _block<warp_bound, Op1, Op2>(tb, warp, data, warp.meta_group_rank(), 0, warp.meta_group_size());
     val1 = data[0];
     val2 = data[1];
 }
@@ -305,7 +486,8 @@ DS_D_INLINE void block(cg::thread_block& tb,
                        float& val3)
 {
     float data[3] = {val1, val2, val3};
-    _block<warp_bound, Op1, Op2, Op3>(tb, warp, data);
+    _block<warp_bound, Op1, Op2, Op3>(
+        tb, warp, data, warp.meta_group_rank(), 0, warp.meta_group_size());
     val1 = data[0];
     val2 = data[1];
     val3 = data[2];
@@ -320,7 +502,76 @@ DS_D_INLINE void block(cg::thread_block& tb,
                        float& val4)
 {
     float data[4] = {val1, val2, val3, val4};
-    _block<warp_bound, Op1, Op2, Op3, Op4>(tb, warp, data);
+    _block<warp_bound, Op1, Op2, Op3, Op4>(
+        tb, warp, data, warp.meta_group_rank(), 0, warp.meta_group_size());
+    val1 = data[0];
+    val2 = data[1];
+    val3 = data[2];
+    val4 = data[3];
+}
+
+/*
+Note: for the partitioned blocks, the implementation does not support non-power of 2 blocks in order
+to shorten block scale reduction length.
+*/
+template <ROpType Op, int num_warps>
+DS_D_INLINE void partitioned_block(cg::thread_block& tb,
+                                   cg::thread_block_tile<hw_warp_size>& warp,
+                                   float& val)
+{
+    const int warp_rank = warp.meta_group_rank() % num_warps;
+    const int warp_offset = warp.meta_group_rank() & ~(num_warps - 1);
+    _block<num_warps, Op>(tb, warp, val, warp_rank, warp_offset, num_warps);
+}
+
+template <ROpType Op1, ROpType Op2, int num_warps>
+DS_D_INLINE void partitioned_block(cg::thread_block& tb,
+                                   cg::thread_block_tile<hw_warp_size>& warp,
+                                   float& val1,
+                                   float& val2)
+{
+    float data[2] = {val1, val2};
+
+    const int warp_rank = warp.meta_group_rank() % num_warps;
+    const int warp_offset = warp.meta_group_rank() & ~(num_warps - 1);
+    _block<num_warps, Op1, Op2>(tb, warp, data, warp_rank, warp_offset, num_warps);
+
+    val1 = data[0];
+    val2 = data[1];
+}
+
+template <ROpType Op1, ROpType Op2, ROpType Op3, int num_warps>
+DS_D_INLINE void partitioned_block(cg::thread_block& tb,
+                                   cg::thread_block_tile<hw_warp_size>& warp,
+                                   float& val1,
+                                   float& val2,
+                                   float& val3)
+{
+    float data[3] = {val1, val2, val3};
+
+    const int warp_rank = warp.meta_group_rank() % num_warps;
+    const int warp_offset = warp.meta_group_rank() & ~(num_warps - 1);
+    _block<num_warps, Op1, Op2, Op3>(tb, warp, data, warp_rank, warp_offset, num_warps);
+
+    val1 = data[0];
+    val2 = data[1];
+    val3 = data[2];
+}
+
+template <ROpType Op1, ROpType Op2, ROpType Op3, ROpType Op4, int num_warps>
+DS_D_INLINE void partitioned_block(cg::thread_block& tb,
+                                   cg::thread_block_tile<hw_warp_size>& warp,
+                                   float& val1,
+                                   float& val2,
+                                   float& val3,
+                                   float& val4)
+{
+    float data[4] = {val1, val2, val3, val4};
+
+    const int warp_rank = warp.meta_group_rank() % num_warps;
+    const int warp_offset = warp.meta_group_rank() & ~(num_warps - 1);
+    _block<num_warps, Op1, Op2, Op3, Op4>(tb, warp, data, warp_rank, warp_offset, num_warps);
+
     val1 = data[0];
     val2 = data[1];
     val3 = data[2];
