20ae6244eaf787e051a6da992dcc665c541351a3
Michael Wyatt <michaelwyatt@microsoft.com>
2022-11-08 04:55:54 -0800

    update how version is read in docs

--- docs/code-docs/source/conf.py
+++ docs/code-docs/source/conf.py
@@ -20,7 +20,8 @@ copyright = '2020, Microsoft'
 author = 'Microsoft'
 
 # The full version, including alpha/beta/rc tags
-release = '0.6'
+with open("../../../version.txt", "r") as f:
+    release = f.readline().rstrip()
 
 master_doc = 'index'
 
