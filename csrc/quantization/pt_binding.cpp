78d4ca1f4bb2f0e8a7f2cfd052b0e7733ab41898
lokoppakmsft <112720551+lokoppakmsft@users.noreply.github.com>
2022-11-17 11:09:45 -0800

    Deepspeed quantization library v0.1 (#2450)
    * Initial commit Deepspeed quantization library
    
    * Match function signatures
    
    * Add Quantization Kernel
    
    * adding offset comparision and precommit changes
    
    * format fixes
    
    * FIle name changes
    
    * pt_binding_changes
    
    * test name change
    
    * Integer quantization, minor refactors
    
    * Add directed test_case
    
    * format fixes
    
    * Move param calculation to constructor of params class
    
    * Use local function and add elemsPerBlock
    
    * change function to be specalized
    
    * sub block reduce
    
    * add new schedule
    
    * Add new schedule test case
    
    * fix illegal writes in sch1
    
    * Style fixes in comments
    
    Co-authored-by: Connor Holmes <connorholmes@microsoft.com>

--- csrc/quantization/pt_binding.cpp
+++ csrc/quantization/pt_binding.cpp
@@ -1,7 +1,7 @@
 #include <ATen/cuda/CUDAContext.h>
 #include <torch/extension.h>
 #include <vector>
-#include "custom_cuda_layers.h"
+#include "quantization.h"
 
 template <typename T>
 at::Tensor ds_quantize(at::Tensor& vals, int groups, int bits)
@@ -10,8 +10,8 @@ at::Tensor ds_quantize(at::Tensor& vals, int groups, int bits)
     int size = 1;
     for (auto dim : t_size) size *= dim;
 
-    if ((((size / groups) - 1) / 4096 + 1) <= MAX_REG) {
-        launch_quantize_kernel(
+    if ((((size / groups) - 1) / 4096 + 1) <= 256) {
+        launch_fake_quantize_kernel(
             (T*)vals.data_ptr(), size, groups, bits, at::cuda::getCurrentCUDAStream());
     }
     return vals;
@@ -25,7 +25,7 @@ at::Tensor ds_sr_quantize(at::Tensor& vals, int groups, int bits)
     for (auto dim : t_size) size *= dim;
 
     if (((size / groups) / 4 / 1024) <= 256) {
-        launch_sr_quantize_kernel(
+        launch_sr_fake_quantize_kernel(
             (T*)vals.data_ptr(), size, groups, bits, at::cuda::getCurrentCUDAStream());
     }
     return vals;
@@ -38,8 +38,8 @@ at::Tensor ds_quantize_asym(at::Tensor& vals, int groups, int bits)
     int size = 1;
     for (auto dim : t_size) size *= dim;
 
-    if ((((size / groups) - 1) / 4096 + 1) <= MAX_REG) {
-        launch_quantize_kernel_asym(
+    if ((((size / groups) - 1) / 4096 + 1) <= 256) {
+        launch_fake_quantize_kernel_asym(
             (T*)vals.data_ptr(), size, groups, bits, at::cuda::getCurrentCUDAStream());
     }
     return vals;
@@ -53,10 +53,63 @@ at::Tensor ds_sr_quantize_asym(at::Tensor& vals, int groups, int bits)
     for (auto dim : t_size) size *= dim;
 
     if (((size / groups) / 4 / 1024) <= 256) {
-        launch_sr_quantize_kernel_asym(
+        launch_sr_fake_quantize_kernel_asym(
             (T*)vals.data_ptr(), size, groups, bits, at::cuda::getCurrentCUDAStream());
     }
     return vals;
+}
+
+#define QUANTIZATION_CASE(TYPE, BITS)                               \
+    case TYPE:                                                      \
+        launch_quant<BITS, TYPE>((int8_t*)output.data_ptr(),        \
+                                 (float*)params.data_ptr(),         \
+                                 (__half*)input_vals.data_ptr(),    \
+                                 groups,                            \
+                                 elems_per_group,                   \
+                                 at::cuda::getCurrentCUDAStream()); \
+        break;
+
+std::vector<at::Tensor> quantize_kernel(at::Tensor& input_vals,
+                                        int groups,
+                                        int numBits,
+                                        quantize::Type quantType)
+{
+    auto dtype = (quantType == quantize::Type::IntegerSymmetric) ? torch::kInt32 : at::kFloat;
+    auto params_options = at::TensorOptions()
+                              .dtype(dtype)
+                              .layout(at::kStrided)
+                              .device(at::kCUDA)
+                              .requires_grad(false);
+    const int param_elems = (quantize::requires_offset(quantType)) ? 2 : 1;
+    auto params = torch::empty({groups, param_elems}, params_options);
+
+    auto output_options = at::TensorOptions()
+                              .dtype(at::kChar)
+                              .layout(at::kStrided)
+                              .device(at::kCUDA)
+                              .requires_grad(false);
+    int K = input_vals.size(1) / (numBits == 8 ? 1 : 2);
+    int M = input_vals.size(0);
+
+    auto output = torch::empty({M, K}, output_options);
+
+    const int elems_per_group = at::numel(input_vals) / groups;
+
+    if (numBits == 4) {
+        switch (quantType) {
+            QUANTIZATION_CASE(quantize::Type::Symmetric, 4)
+            QUANTIZATION_CASE(quantize::Type::Asymmetric, 4)
+            QUANTIZATION_CASE(quantize::Type::IntegerSymmetric, 4)
+        }
+    } else {
+        switch (quantType) {
+            QUANTIZATION_CASE(quantize::Type::Symmetric, 8)
+            QUANTIZATION_CASE(quantize::Type::Asymmetric, 8)
+            QUANTIZATION_CASE(quantize::Type::IntegerSymmetric, 8)
+        }
+    }
+
+    return {output, params};
 }
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
@@ -74,4 +127,10 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
     m.def("ds_sr_quantize_asym_fp16",
           &ds_sr_quantize_asym<__half>,
           "DeepSpeed Quantize with fp16 (CUDA)");
+    pybind11::enum_<quantize::Type>(m, "QuantizationType")
+        .value("Symmetric", quantize::Type::Symmetric)
+        .value("Asymmetric", quantize::Type::Asymmetric)
+        .value("IntegerSymmetric", quantize::Type::IntegerSymmetric)
+        .export_values();
+    m.def("quantize", &quantize_kernel);
 }
