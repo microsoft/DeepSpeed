e4de217862c1c1ad26f94e6addbcb7b844466e09
Ammar Ahmad Awan <ammar.awan@microsoft.com>
2022-11-23 11:23:46 -0800

    Update constants.py

--- deepspeed/launcher/constants.py
+++ deepspeed/launcher/constants.py
@@ -6,6 +6,6 @@ PDSH_MAX_FAN_OUT = 1024
 OPENMPI_LAUNCHER = 'openmpi'
 SLURM_LAUNCHER = 'slurm'
 MVAPICH_LAUNCHER = 'mvapich'
-MVAPICH_TMP_HOSTFILE = '/tmp/deepspeed_mvapich_hostfile'
+MVAPICH_TMP_HOSTFILE = './deepspeed_mvapich_hostfile'
 
 ELASTIC_TRAINING_ID_DEFAULT = "123456789"
