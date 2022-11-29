fbdcab7f37c16188790d4925ee005b9ea8ef18f8
Michael Wyatt <mrwyattii@gmail.com>
2022-11-29 10:03:56 -0800

    Update gpu_lib.h

--- csrc/includes/gpu_lib/gpu_lib.h
+++ csrc/includes/gpu_lib/gpu_lib.h
@@ -2,12 +2,12 @@
 #define DS_GPU_LIB_HPP
 
 #ifdef __HIP_PLATFORM_HCC__
-#include "gpu_lib/rocm.hpp"
+#include "rocm.hpp"
 namespace gpu_lib {
 using namespace rocm;
 }
 #else
-#include "gpu_lib/cuda.hpp"
+#include "cuda.hpp"
 namespace gpu_lib {
 using namespace cuda;
 }
