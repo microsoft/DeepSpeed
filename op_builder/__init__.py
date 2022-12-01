5a1ae0e3dfb26b3916a009c3b63149ce18a540ce
Ma, Guokai <guokai.ma@intel.com>
2022-12-01 12:53:27 +0800

    remove unneeded get_accelerator import in op_builder/__init__.py

--- op_builder/__init__.py
+++ op_builder/__init__.py
@@ -1,7 +1,6 @@
 """
 Copyright 2020 The Microsoft DeepSpeed Team
 """
-from deepspeed.accelerator import get_accelerator
 from .cpu_adam import CPUAdamBuilder
 from .cpu_adagrad import CPUAdagradBuilder
 from .fused_adam import FusedAdamBuilder
