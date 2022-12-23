bb68c526ad2c267dfb235db9c0d0fb1413d19a34
Jeff Rasley <jerasley@microsoft.com>
2022-12-22 17:19:49 -0800

    [inference] ds-attention refactor w.r.t. ops (#2623)

---
+++ deepspeed/ops/transformer/inference/op_binding/__init__.py
@@ -0,0 +1,5 @@
+from .linear import LinearOp
+from .vector_matmul import VectorMatMulOp
+from .softmax_context import SoftmaxContextOp
+from .qkv_gemm import QKVGemmOp
+from .softmax import SoftmaxOp
