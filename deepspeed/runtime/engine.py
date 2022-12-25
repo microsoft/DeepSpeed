74216878146efa1ba5ee8082225e08f94f6a0267
Qinghuan Rao <qinghuanrao@microsoft.com>
2022-12-25 23:02:22 +0800

    test

--- deepspeed/runtime/engine.py
+++ deepspeed/runtime/engine.py
@@ -925,6 +925,7 @@ class DeepSpeedEngine(Module):
                     NebulaCheckpointEngine
                 self.checkpoint_engine = NebulaCheckpointEngine(
                     config_params=self._config.nebula_config)
+                print("Use NebulaCheckpointEngine")
             except ImportError as err:
                 logger.error(
                     f"No torch_nebula was found! Will fall back to torch.save. Details: {err}"
