fbe04d41db6c0d3975f49f5335fa900b9a8e5c99
Qinghuan Rao <qinghuanrao@microsoft.com>
2022-12-26 15:56:26 +0800

    refine

--- tests/unit/checkpoint/test_nebula_checkpoint.py
+++ tests/unit/checkpoint/test_nebula_checkpoint.py
@@ -5,38 +5,14 @@ import pytest
 class TestNebulaCheckpoint(DistributedTest):
     world_size = 4
 
-    @pytest.mark.parametrize("zero_stage", [0, 1])
-    def test_checkpoint_nebula_engine(self, zero_stage, tmpdir):
+    def test_checkpoint_nebula_engine(self, tmpdir):
         config_dict = {
             "train_batch_size": 2,
-            "train_micro_batch_size_per_gpu": 1,
             "steps_per_print": 1,
             "optimizer": {
                 "type": "Adam",
                 "params": {
-                    "lr": 1e-5
-                }
-            },
-            "zero_optimization": {
-                "stage": zero_stage
-            },
-            "fp16": {
-                "enabled": zero_stage > 0
-            },
-            "scheduler": {
-                "type": "OneCycle",
-                "params": {
-                    "cycle_first_step_size": 1000,
-                    "cycle_first_stair_count": 500,
-                    "cycle_second_step_size": 1000,
-                    "cycle_second_stair_count": 500,
-                    "decay_step_size": 1000,
-                    "cycle_min_lr": 0.0001,
-                    "cycle_max_lr": 0.0010,
-                    "decay_lr_rate": 0.001,
-                    "cycle_min_mom": 0.85,
-                    "cycle_max_mom": 0.99,
-                    "decay_mom_rate": 0.0
+                    "lr": 0.00015
                 }
             },
             "nebula": {
@@ -47,12 +23,14 @@ class TestNebulaCheckpoint(DistributedTest):
                 "enable_nebula_load": "true"
             }
         }
-        models = [LinearStackPipe(num_stages=2) for _ in range(2)]
+        hidden_dim = 10
+        models = [SimpleModel(hidden_dim=hidden_dim) for _ in range(2)]
         checkpoint_correctness_verification(config_dict=config_dict,
                                             models=models,
-                                            hidden_dim=models[0].hidden_dim,
+                                            hidden_dim=hidden_dim,
                                             tmpdir=tmpdir,
-                                            fp16=config_dict['fp16']['enabled'],
                                             load_optimizer_states=True,
-                                            load_lr_scheduler_states=True,
-                                            train_batch=True)
+                                            load_lr_scheduler_states=False,
+                                            fp16=False,
+                                            empty_tag=True)
+
