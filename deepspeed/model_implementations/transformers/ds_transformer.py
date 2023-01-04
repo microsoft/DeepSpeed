8e91537e588c3b14a7e50f78e72df93a87e9ac77
Reza Yazdani <reyazda@microsoft.com>
2023-01-05 02:29:39 +0500

    Reset KV-cache at the beginning of text-generation

--- deepspeed/model_implementations/transformers/ds_transformer.py
+++ deepspeed/model_implementations/transformers/ds_transformer.py
@@ -89,6 +89,10 @@ class DeepSpeedTransformerInference(nn.Module):
         self.allocate_workspace = inference_cuda_module.allocate_workspace_fp32 if (not config.fp16) else \
                                 inference_cuda_module.allocate_workspace_fp16
 
+    @classmethod
+    def reset_cache(cls):
+        inference_cuda_module.reset_cache()
+
     def forward(
             self,
             input=None,
