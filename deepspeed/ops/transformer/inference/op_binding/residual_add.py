53a70fa630915cb3c9a8b0cd229ca44aa7557569
Jeff Rasley <jerasley@microsoft.com>
2023-01-06 11:52:33 -0800

    fix bad indentation

--- deepspeed/ops/transformer/inference/op_binding/residual_add.py
+++ deepspeed/ops/transformer/inference/op_binding/residual_add.py
@@ -11,27 +11,26 @@ class ResidualAddOp(BaseOp):
         else:
             self.residual_add_func = self.inference_cuda_module.residual_add_bias_fp32
 
+    def forward(self,
+                hidden_state: torch.Tensor,
+                residual: torch.Tensor,
+                attention_output: torch.Tensor,
+                attention_bias: torch.Tensor,
+                final_bias: torch.Tensor,
+                add_bias: bool,
+                residual_add: torch.Tensor):
 
-def forward(self,
-            hidden_state: torch.Tensor,
-            residual: torch.Tensor,
-            attention_output: torch.Tensor,
-            attention_bias: torch.Tensor,
-            final_bias: torch.Tensor,
-            add_bias: bool,
-            residual_add: torch.Tensor):
+        if not self.config.pre_layer_norm and residual_add is not None:
+            # only use residual add if its set and we are not pre layer norm
+            residual = residual_add
 
-    if not self.config.pre_layer_norm and residual_add is not None:
-        # only use residual add if its set and we are not pre layer norm
-        residual = residual_add
-
-    output = self.residual_add_func(hidden_state,
-                                    residual,
-                                    attention_output,
-                                    attention_bias,
-                                    final_bias,
-                                    self.config.mp_size,
-                                    self.config.mlp_after_attn,
-                                    add_bias,
-                                    self.config.pre_layer_norm)
-    return output
+        output = self.residual_add_func(hidden_state,
+                                        residual,
+                                        attention_output,
+                                        attention_bias,
+                                        final_bias,
+                                        self.config.mp_size,
+                                        self.config.mlp_after_attn,
+                                        add_bias,
+                                        self.config.pre_layer_norm)
+        return output
