d9ab307b8c23a4d33097de2c9a3fde1bc1c2e611
Ma, Guokai <guokai.ma@intel.com>
2023-01-08 23:16:28 +0800

    no need to check cuda and whether nvtx supported

--- deepspeed/utils/nvtx.py
+++ deepspeed/utils/nvtx.py
@@ -1,18 +1,13 @@
-import torch
 from deepspeed.accelerator import get_accelerator
 
 
 def instrument_w_nvtx(func):
     """decorator that causes an NVTX range to be recorded for the duration of the
     function call."""
-    if get_accelerator().device_name() == 'cuda' and hasattr(torch.cuda.nvtx, "range"):
+    def wrapped_fn(*args, **kwargs):
+        get_accelerator().range_push(func.__qualname__)
+        ret_val = func(*args, **kwargs)
+        get_accelerator().range_pop()
+        return ret_val
 
-        def wrapped_fn(*args, **kwargs):
-            get_accelerator().range_push(func.__qualname__)
-            ret_val = func(*args, **kwargs)
-            get_accelerator().range_pop()
-            return ret_val
-
-        return wrapped_fn
-    else:
-        return func
+    return wrapped_fn
