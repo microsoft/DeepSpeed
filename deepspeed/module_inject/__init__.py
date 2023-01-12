a1122d611b7fe50fb5bf3912ae1322424c637c3a
Lev Kurilenko <lekurile@microsoft.com>
2023-01-12 23:29:33 +0000

    Make replace_layer -> replace_module, update to match replace_module in master

--- deepspeed/module_inject/__init__.py
+++ deepspeed/module_inject/__init__.py
@@ -1,8 +1,5 @@
 from .replace_module import revert_transformer_layer, ReplaceWithTensorSlicing, GroupQuantizer, generic_injection
-# if we want to use the refactored/new code, we need to import the new code
-from .replace_layer import replace_transformer_layer
-# else: we need to import the old code
-#from .replace_module import replace_transformer_layer
+from .replace_module import replace_transformer_layer
 from .module_quantize import quantize_transformer_layer
 from .replace_policy import HFBertLayerPolicy
 from .layers import LinearAllreduce, LinearLayer, EmbeddingLayer, Normalize
