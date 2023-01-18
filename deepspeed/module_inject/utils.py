3a2fa502802464a24bdce92dfbca7b74a5ec5315
Michael Wyatt <michaelwyatt@microsoft.com>
2023-01-18 15:08:24 -0800

    refactor policy to container mapping function

--- deepspeed/module_inject/utils.py
+++ deepspeed/module_inject/utils.py
@@ -1,49 +1,38 @@
+from deepspeed.utils import log_dist
+
+
 # helper function to map between DS policies and DS containers
 def policy_to_ds_container(**kwargs):
+    from .containers import HFGPT2LayerPolicy, DS_GPT2Container
+    from .containers import HFBertLayerPolicy, DS_BERTContainer
+    from .containers import BLOOMLayerPolicy, DS_BloomContainer
+    from .containers import HFGPTJLayerPolicy, DS_GPTJContainer
+    from .containers import HFGPTNEOLayerPolicy, DS_GPTNEOContainer
+    from .containers import GPTNEOXLayerPolicy, DS_GPTNEOXContainer
+    from .containers import HFOPTLayerPolicy, DS_OPTContainer
+    from .containers import MegatronLayerPolicy, DS_MegatronGPTContainer
+    from .containers import HFDistilBertLayerPolicy, DS_DistilBERTContainer
+
+    policy_to_container = {
+        HFGPT2LayerPolicy: DS_GPT2Container,
+        HFBertLayerPolicy: DS_BERTContainer,
+        BLOOMLayerPolicy: DS_BloomContainer,
+        HFGPTJLayerPolicy: DS_GPTJContainer,
+        HFGPTNEOLayerPolicy: DS_GPTNEOContainer,
+        GPTNEOXLayerPolicy: DS_GPTNEOXContainer,
+        HFOPTLayerPolicy: DS_OPTContainer,
+        MegatronLayerPolicy: DS_MegatronGPTContainer,
+        HFDistilBertLayerPolicy: DS_DistilBERTContainer,
+    }
+
     container = None
     policy = kwargs['policy']
+    assert policy is not None, "Policy cannot be None"
+    policy_type = type(policy)
 
-    if policy is None:
-        print("Policy is None")
+    if policy_type not in policy_to_container:
+        log_dist(f"Policy type {policy_type} not supported", [0])
     else:
-        from .containers import HFGPT2LayerPolicy
-        from .containers import HFBertLayerPolicy
-        from .containers import BLOOMLayerPolicy
-        from .containers import HFGPTJLayerPolicy
-        from .containers import HFGPTNEOLayerPolicy
-        from .containers import GPTNEOXLayerPolicy
-        from .containers import HFOPTLayerPolicy
-        from .containers import MegatronLayerPolicy
-        from .containers import HFDistilBertLayerPolicy
-
-        if isinstance(policy, HFGPT2LayerPolicy):
-            from .containers import DS_GPT2Container
-            container = DS_GPT2Container(**kwargs)
-        elif isinstance(policy, HFBertLayerPolicy):
-            from .containers import DS_BERTContainer
-            container = DS_BERTContainer(**kwargs)
-        elif isinstance(policy, BLOOMLayerPolicy):
-            from .containers import DS_BloomContainer
-            container = DS_BloomContainer(**kwargs)
-        elif isinstance(policy, HFGPTJLayerPolicy):
-            from .containers import DS_GPTJContainer
-            container = DS_GPTJContainer(**kwargs)
-        elif isinstance(policy, HFGPTNEOLayerPolicy):
-            from .containers import DS_GPTNEOContainer
-            container = DS_GPTNEOContainer(**kwargs)
-        elif isinstance(policy, GPTNEOXLayerPolicy):
-            from .containers import DS_GPTNEOXContainer
-            container = DS_GPTNEOXContainer(**kwargs)
-        elif isinstance(policy, HFOPTLayerPolicy):
-            from .containers import DS_OPTContainer
-            container = DS_OPTContainer(**kwargs)
-        elif isinstance(policy, MegatronLayerPolicy):
-            from .containers import DS_MegatronGPTContainer
-            container = DS_MegatronGPTContainer(**kwargs)
-        elif isinstance(policy, HFDistilBertLayerPolicy):
-            from .containers import DS_DistilBERTContainer
-            container = DS_DistilBERTContainer(**kwargs)
-        else:
-            print("policy file is not recognized")
+        container = policy_to_container[policy_type](**kwargs)
 
     return container
