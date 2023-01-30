841ee593b943de4fd14db7d352d5bcb6b25f2ff9
Michael Wyatt <michaelwyatt@microsoft.com>
2023-01-30 15:02:02 -0800

    additionaly cleanup and added auto-gen docs

--- deepspeed/comm/config.py
+++ deepspeed/comm/config.py
@@ -12,7 +12,16 @@ def get_comms_config(param_dict):
 
 class DeepSpeedCommsConfig(DeepSpeedConfigModel):
     enabled: bool = False
+    """ Whether communication logging is enabled. """
+
     prof_all: bool = True
+    """ Whether to profile all operations. """
+
     prof_ops: list = []
+    """ A list of communication operations to log (only the specified ops will be profiled). """
+
     verbose: bool = False
+    """ Whether to immediately print every communication operation. """
+
     debug: bool = False
+    """ Appends the caller function to each communication operation's `log_name`. """
--- deepspeed/utils/comms_logging.py
+++ deepspeed/utils/comms_logging.py
@@ -54,15 +54,6 @@ def calc_bw_log(comm_op, size, duration):
 
 
 class CommsLogger:
-    def __init__(self):
-        from deepspeed.comm.constants import COMMS_LOGGER_VERBOSE_DEFAULT, COMMS_LOGGER_DEBUG_DEFAULT, COMMS_LOGGER_PROF_OPS_DEFAULT, COMMS_LOGGER_PROF_ALL_DEFAULT, COMMS_LOGGER_ENABLED_DEFAULT
-        self.comms_dict = {}
-        self.verbose = COMMS_LOGGER_VERBOSE_DEFAULT
-        self.debug = COMMS_LOGGER_DEBUG_DEFAULT
-        self.prof_ops = COMMS_LOGGER_PROF_OPS_DEFAULT
-        self.prof_all = COMMS_LOGGER_PROF_ALL_DEFAULT
-        self.enabled = COMMS_LOGGER_ENABLED_DEFAULT
-
     def configure(self, comms_config):
         self.enabled = comms_config.enabled
         if self.enabled:
