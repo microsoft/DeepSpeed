b4bb1b1cafa820b8db46c3c801b5ca93d3d85034
Tunji Ruwase <olruwase@microsoft.com>
2023-02-01 20:49:17 +0000

    Formatting

--- csrc/aio/py_lib/deepspeed_pin_tensor.cpp
+++ csrc/aio/py_lib/deepspeed_pin_tensor.cpp
@@ -9,32 +9,19 @@ Functionality for managing pinned CPU memory.
 
 using namespace std;
 
-deepspeed_pin_tensor_t::deepspeed_pin_tensor_t()
-{
-}
+deepspeed_pin_tensor_t::deepspeed_pin_tensor_t() {}
 
-deepspeed_pin_tensor_t::~deepspeed_pin_tensor_t()
-{
-}
+deepspeed_pin_tensor_t::~deepspeed_pin_tensor_t() {}
 
 torch::Tensor deepspeed_pin_tensor_t::alloc(const size_t num_elem, const at::ScalarType& elem_type)
 {
     const auto num_bytes = num_elem * elementSize(elem_type);
     auto pinned_buffer = ds_page_aligned_alloc(num_bytes, true);
-    assert (nullptr != pinned_buffer);
-    
-     auto options = torch::TensorOptions()
-                       .dtype(elem_type)
-                       .device(torch::kCPU);
+    assert(nullptr != pinned_buffer);
 
-    return at::from_blob(
-        pinned_buffer, 
-        static_cast<long int>(num_bytes),
-        options
-        );
+    auto options = torch::TensorOptions().dtype(elem_type).device(torch::kCPU);
+
+    return at::from_blob(pinned_buffer, static_cast<long int>(num_bytes), options);
 }
 
-bool deepspeed_pin_tensor_t::free(torch::Tensor& locked_tensor)
-{
-    return true;
-}
+bool deepspeed_pin_tensor_t::free(torch::Tensor& locked_tensor) { return true; }
--- csrc/aio/py_lib/deepspeed_pin_tensor.h
+++ csrc/aio/py_lib/deepspeed_pin_tensor.h
@@ -9,12 +9,11 @@ Functionality for managing pinned CPU memory.
 #include "deepspeed_py_aio.h"
 
 struct deepspeed_pin_tensor_t {
-
     deepspeed_pin_tensor_t();
 
     ~deepspeed_pin_tensor_t();
 
     torch::Tensor alloc(const size_t num_elem, const at::ScalarType& elem_type);
-    
-    bool free(torch::Tensor& locked_tensor);       
+
+    bool free(torch::Tensor& locked_tensor);
 };
