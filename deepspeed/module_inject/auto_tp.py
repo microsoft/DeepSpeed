01a8635bbc354387217c7d3c50a2171a4278a90b
Molly Smith <112220543+molly-smith@users.noreply.github.com>
2023-02-16 13:18:32 -0800

    add xlnet to unsupported models

--- deepspeed/module_inject/auto_tp.py
+++ deepspeed/module_inject/auto_tp.py
@@ -25,7 +25,7 @@ class AutoTP():
         return mlist
 
     def supported(model):
-        unsupported = ['bloom', 'codegen', 'deberta', 'flaubert', 'fsmt', 'led', 'longformer', 'xlm']
+        unsupported = ['bloom', 'codegen', 'deberta', 'flaubert', 'fsmt', 'led', 'longformer', 'xlm', 'xlnet']
         model = str(model)
         key = re.search(r": (.*?)Model", model)
         if key is None:
