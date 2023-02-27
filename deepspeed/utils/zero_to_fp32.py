271365933cad6416c069efc25272da494d84250e
Stas Bekman <stas00@users.noreply.github.com>
2023-02-27 15:53:14 -0800

    fix zero_to_fp32.py

--- deepspeed/utils/zero_to_fp32.py
+++ deepspeed/utils/zero_to_fp32.py
@@ -1,6 +1,6 @@
-'''Copyright The Microsoft DeepSpeed Team'''
-
 #!/usr/bin/env python
+
+''Copyright The Microsoft DeepSpeed Team'''
 
 # This script extracts fp32 consolidated weights from a zero 2 and 3 DeepSpeed checkpoints. It gets
 # copied into the top level checkpoint dir, so the user can easily do the conversion at any point in
