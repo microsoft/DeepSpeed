a363f0198ee39e1d8cafe4a6ed3ab72cb3f76424
Cao, Zhong Z <zhong.z.cao@intel.com>
2023-03-02 01:42:07 -0500

    Remove unecessary directory
    Signed-off-by: Cao, Zhong Z <zhong.z.cao@intel.com>

--- deepspeed/module_inject/replace_module.py
+++ deepspeed/module_inject/replace_module.py
@@ -556,9 +556,7 @@ def replace_transformer_layer(orig_layer_impl,
 
             for i in range(len(checkpoint)):
                 sd = [
-                    torch.load(os.path.join(base_dir1,
-                                            checkpoint[i]),
-                               map_location='cpu')
+                    torch.load(checkpoint[i], map_location='cpu')
                 ]
                 load_model_with_checkpoint(replaced_module,
                                            sd,
