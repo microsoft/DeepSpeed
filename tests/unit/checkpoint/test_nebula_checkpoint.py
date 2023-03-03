97c1ebad093e672e200584ed0beaf7cca2b493fa
Qinghuan Rao <qinghuanrao@microsoft.com>
2023-03-03 14:47:41 +0800

    refine nebula save test

--- tests/unit/checkpoint/test_nebula_checkpoint.py
+++ tests/unit/checkpoint/test_nebula_checkpoint.py
@@ -1,63 +1,88 @@
 from unit.common import DistributedTest
-from unit.checkpoint.common import checkpoint_correctness_verification
+from unit.checkpoint.common import *
 from unit.simple_model import *
 import pytest
 
 
 class TestNebulaCheckpoint(DistributedTest):
-  world_size = 4
+    world_size = 2
 
-  @pytest.mark.parametrize("zero_stage", [0, 1, 2, 3])
-  @pytest.mark.parametrize("stage3_gather_16bit_weights_on_model_save", [True, False])
-  def test_checkpoint_nebula_engine(self, zero_stage, stage3_gather_16bit_weights_on_model_save, tmpdir):
-    config_dict = {
-        "train_batch_size": 2,
-        "train_micro_batch_size_per_gpu": 1,
-        "steps_per_print": 1,
-        "optimizer": {
-            "type": "Adam",
-            "params": {
-                "lr": 1e-5
+    @pytest.mark.parametrize('zero_stage', [3])
+    def test_save_16bit_model(self, tmpdir, zero_stage):
+        config_dict = {
+            "optimizer": {
+                "type": 'Adam'
+            },
+            "fp16": {
+                "enabled": True,
+                "initial_scale_power": 8
+            },
+            "zero_optimization": {
+                "stage": zero_stage,
+                "stage3_gather_fp16_weights_on_model_save": True,
+            },
+            "gradient_accumulation_steps": 1,
+            "train_micro_batch_size_per_gpu": 1,
+            "train_batch_size": 4,
+            "nebula": {
+                "enabled": True,
+                "persistent_storage_path": "/tmp/nebula_checkpoint/",
+                "persistent_time_interval": 10,
+                "num_of_version_in_retention": 2,
+                "enable_nebula_load": True
             }
-        },
-        "zero_optimization": {
-            "stage": zero_stage,
-            "stage3_gather_16bit_weights_on_model_save": stage3_gather_16bit_weights_on_model_save
-        },
-        "fp16": {
-            "enabled": zero_stage > 0
-        },
-        "scheduler": {
-            "type": "OneCycle",
-            "params": {
-                "cycle_first_step_size": 1000,
-                "cycle_first_stair_count": 500,
-                "cycle_second_step_size": 1000,
-                "cycle_second_stair_count": 500,
-                "decay_step_size": 1000,
-                "cycle_min_lr": 0.0001,
-                "cycle_max_lr": 0.0010,
-                "decay_lr_rate": 0.001,
-                "cycle_min_mom": 0.85,
-                "cycle_max_mom": 0.99,
-                "decay_mom_rate": 0.0
-            }
-        },
-        "nebula": {
-            "enabled": "true",
-            "persistent_storage_path": "/tmp/data_storage",
-            "persistent_time_interval": 100,
-            "num_of_version_in_retention": 2,
-            "enable_nebula_load": "true"
         }
-    }
-    models = [LinearStackPipe(num_stages=2) for _ in range(2)]
-    checkpoint_correctness_verification(
-        config_dict=config_dict,
-        models=models,
-        hidden_dim=models[0].hidden_dim,
-        tmpdir=tmpdir,
-        fp16=config_dict['fp16']['enabled'],
-        load_optimizer_states=True,
-        load_lr_scheduler_states=True,
-        train_batch=True)
+        hidden_dim = 10
+        models = [SimpleModel(hidden_dim=hidden_dim) for _ in range(2)]
+
+        ds_model = create_deepspeed_model(config_dict=config_dict,
+                                            model=models[0],
+                                            base_optimizer=None)
+
+        data_loader = random_dataloader(model=ds_model,
+                                        total_samples=2,
+                                        hidden_dim=hidden_dim,
+                                        device=ds_model.device,
+                                        dtype=torch.half)
+
+        batch = next(iter(data_loader))
+        loss = ds_model(batch[0], batch[1])
+        ds_model.backward(loss)
+        ds_model.step()
+
+        ds_model.save_16bit_model(tmpdir, "model.pt")
+
+    @pytest.mark.parametrize('zero_stage', [0,1])
+    def test_save_checkpoint(self, tmpdir, zero_stage):
+        config_dict = {
+            "optimizer": {
+                "type": 'Adam'
+            },
+            "fp16": {
+                "enabled": True,
+                "initial_scale_power": 8
+            },
+            "zero_optimization": {
+                "stage": zero_stage
+            },
+            "gradient_accumulation_steps": 1,
+            "train_micro_batch_size_per_gpu": 1,
+            "train_batch_size": 4,
+            "nebula": {
+                "enabled": True,
+                "persistent_storage_path": "/tmp/nebula_checkpoint/",
+                "persistent_time_interval": 10,
+                "num_of_version_in_retention": 2,
+                "enable_nebula_load": True
+            }
+        }
+        hidden_dim = 10
+        models = [SimpleModel(hidden_dim=hidden_dim) for _ in range(2)]
+        checkpoint_correctness_verification(config_dict=config_dict,
+                                            models=models,
+                                            hidden_dim=hidden_dim,
+                                            tmpdir=tmpdir,
+                                            load_optimizer_states=True,
+                                            load_lr_scheduler_states=False,
+                                            fp16=False,
+                                            empty_tag=True)
