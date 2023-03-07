5e12a14ac826edb0398709f497c2699f0397dd53
Qinghuan Rao <qinghuanrao@microsoft.com>
2023-03-07 16:05:28 +0800

    format

--- tests/unit/checkpoint/test_nebula_checkpoint.py
+++ tests/unit/checkpoint/test_nebula_checkpoint.py
@@ -11,65 +11,6 @@ import json
 class TestNebulaCheckpoint(DistributedTest):
     world_size = 1
 
-    # @pytest.mark.parametrize('zero_stage', [3])
-    # def test_save_16bit_model(self, tmpdir, zero_stage):
-    #     config_dict = {
-    #         "optimizer": {
-    #             "type": 'Adam'
-    #         },
-    #         "fp16": {
-    #             "enabled": True,
-    #             "initial_scale_power": 8
-    #         },
-    #         "zero_optimization": {
-    #             "stage": zero_stage,
-    #             "stage3_gather_fp16_weights_on_model_save": True,
-    #         },
-    #         "gradient_accumulation_steps": 1,
-    #         "train_micro_batch_size_per_gpu": 1,
-    #         "train_batch_size": 4,
-    #         "nebula": {
-    #             "enabled": True,
-    #             "persistent_storage_path": "/tmp/nebula_checkpoint/",
-    #             "persistent_time_interval": 10,
-    #             "num_of_version_in_retention": 2,
-    #             "enable_nebula_load": True
-    #         }
-    #     }
-    #     hidden_dim = 10
-    #     models = [SimpleModel(hidden_dim=hidden_dim) for _ in range(2)]
-
-    #     ds_model = create_deepspeed_model(config_dict=config_dict,
-    #                                       model=models[0],
-    #                                       base_optimizer=None)
-
-    #     data_loader = random_dataloader(model=ds_model,
-    #                                     total_samples=2,
-    #                                     hidden_dim=hidden_dim,
-    #                                     device=ds_model.device,
-    #                                     dtype=torch.half)
-
-    #     batch = next(iter(data_loader))
-    #     loss = ds_model(batch[0], batch[1])
-    #     ds_model.backward(loss)
-    #     ds_model.step()
-
-    #     save_folder = os.path.join(tmpdir, 'save_16bit_model')
-    #     save_tag = None
-    #     ds_model.save_16bit_model(save_folder)
-
-    #     loaded_model = create_deepspeed_model(config_dict=config_dict,
-    #                                         model=models[1],
-    #                                         base_optimizer=None)
-
-    #     assert list(ds_model.parameters())[0].dtype == list(loaded_model.parameters())[0].dtype
-
-    #     loaded_model.load_checkpoint(tmpdir,
-    #                                  tag=save_tag)
-
-    #     compare_model_states(ds_model,
-    #                          loaded_model)
-
     def test_save_checkpoint(self, tmpdir):
         print("list /dev/shm origin status: ", os.listdir("/dev/shm/"))
         for filename in glob.glob("/dev/shm/shm_name_partition_*"):
@@ -86,6 +27,7 @@ class TestNebulaCheckpoint(DistributedTest):
                     "n2e0b2u2la_mars") or is_service_launched(
                         "n2e0b2u2la_replica_server"):
             shut_down_nebula_service()
+
         config_dict = {
             "train_batch_size": 2,
             "steps_per_print": 1,
@@ -138,13 +80,8 @@ class TestNebulaCheckpoint(DistributedTest):
             loaded_model.parameters())[0].dtype
 
         import torch_nebula as tn
-
         tn.flush_persistence()
-        for root, dirs, files in os.walk("/tmp/nebula_checkpoint/", topdown=False):
-            for name in files:
-                print(os.path.join(root, name))
-            for name in dirs:
-                print(os.path.join(root, name))
+
         global_tag_ckpt = tn.list_checkpoints()
         js = json.dumps(global_tag_ckpt, sort_keys=True, indent=4, separators=(",", ":"))
         print(js)
@@ -152,6 +89,7 @@ class TestNebulaCheckpoint(DistributedTest):
         # get latest checkpoints by name
         latest_ckpt = tn.get_latest_checkpoint()
         print("latest_ckpt: ", latest_ckpt.tag)
+
         loaded_model.load_checkpoint(save_folder, tag=save_tag)
 
         compare_model_states(trained_model,
