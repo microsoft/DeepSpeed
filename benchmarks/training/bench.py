1ffba860d83e2b9aa200cc19a00e85376835a693
Michael Wyatt <michaelwyatt@microsoft.com>
2023-03-08 10:02:43 -0800

    refactoring code, improvements

--- benchmarks/training/bench.py
+++ benchmarks/training/bench.py
@@ -4,23 +4,47 @@ import transformers
 import datasets
 import os
 import time
+import argparse
 import numpy as np
 
 from torch.utils.data import Dataset, DataLoader
+from abc import ABC, abstractmethod
+
 
 def get_parameters_in_billions(model):
     gpus_per_model = int(os.getenv("WORLD_SIZE", "1"))
-    approx_parameters_in_billions = sum([sum([p.ds_numel if hasattr(p,'ds_id') else  p.nelement() for p in model_module.parameters()])
-                                        for model_module in model])
-    return approx_parameters_in_billions*gpus_per_model/(1e9)
+    approx_parameters_in_billions = sum(
+        [
+            sum(
+                [
+                    p.ds_numel if hasattr(p, "ds_id") else p.nelement()
+                    for p in model_module.parameters()
+                ]
+            )
+            for model_module in model
+        ]
+    )
+    return approx_parameters_in_billions * gpus_per_model / (1e9)
 
-def throughput_calculator(model, micro_batch_size, iteration_time, total_iterations, seq_length, hidden_size, num_layers, vocab_size):
+
+def throughput_calculator(
+    model,
+    micro_batch_size,
+    iteration_time,
+    total_iterations,
+    seq_length,
+    hidden_size,
+    num_layers,
+    vocab_size,
+):
     gpus_per_model = int(os.getenv("WORLD_SIZE", "1"))
     batch_size = micro_batch_size * gpus_per_model
     samples_per_model = batch_size * seq_length
     model_replica_count = 1
-    approx_parameters_in_billions = None if (model is None) else get_parameters_in_billions(model)
-    elapsed_time_per_iter = iteration_time/total_iterations
+    approx_parameters_in_billions = (
+        None if (model is None) else get_parameters_in_billions(model)
+    )
+    elapsed_time_per_iter = iteration_time / total_iterations
     samples_per_second = batch_size / elapsed_time_per_iter
 
     # General TFLOPs formula (borrowed from Equation 3 in Section 5.1 of
@@ -28,9 +52,21 @@ def throughput_calculator(model, micro_batch_size, iteration_time, total_iterati
     # The factor of 4 is when used with activation check-pointing,
     # otherwise it will be 3.
     checkpoint_activations_factor = 3
-    flops_per_iteration = (24 * checkpoint_activations_factor * batch_size * seq_lenth * num_layers * (hidden_size**2)) * (1. + (seq_len / (6. * hidden_size)) + (vocab_size / (16. * num_layers * hidden_size)))
+    flops_per_iteration = (
+        24
+        * checkpoint_activations_factor
+        * batch_size
+        * seq_lenth
+        * num_layers
+        * (hidden_size**2)
+    ) * (
+        1.0
+        + (seq_len / (6.0 * hidden_size))
+        + (vocab_size / (16.0 * num_layers * hidden_size))
+    )
     tflops = flops_per_iteration / (elapsed_time_per_iter * gpus_per_model * (10**12))
     return samples_per_second, tflops, approx_parameters_in_billions
+
 
 class GPTLMLoss(torch.nn.Module):
     def __init__(self):
@@ -41,16 +77,12 @@ class GPTLMLoss(torch.nn.Module):
         shift_logits = logits[..., :-1, :].contiguous()
         shift_labels = labels[..., 1:].contiguous()
         # Flatten the tokens
-        return self.loss(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1))
-
-model_classes = {"gpt2": transformers.GPT2LMHeadModel}
-
-model_configs = {"gpt2": transformers.GPT2Config}
-
-model_criterion = {"gpt2": GPTLMLoss}
+        return self.loss(
+            shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1)
+        )
 
 
-class SyntheticDataset(Dataset):
+class SyntheticTokenDataset(Dataset):
     def __init__(self, size, vocab_size, seq_length) -> None:
         super().__init__()
         self.size = size
@@ -70,46 +102,109 @@ class SyntheticDataset(Dataset):
         }
 
 
-class ModelFromHF(torch.nn.Module):
-    def __init__(self, config, model_cls):
-        super().__init__()
-        self.module = model_cls(config)
-        # if CONFIG['model'].get('checkpoint'):
-        #    self.module.apply(self.set_checkpointing)
+class BenchModelSetup(ABC):
+    def __init__(self, args, model_config, ds_config):
+        self.args = args
+        self.model_config = model_config
+        self.ds_config = ds_config
 
-    def set_checkpointing(self, module):
-        if hasattr(module, "gradient_checkpointing"):
-            module.gradient_checkpointing = True
+    @property
+    @abstractmethod
+    def model(self):
+        ...
 
-    def forward(self, *args, **kwargs):
-        output = self.module(*args, **kwargs)
-        return output.logits
+    @property
+    @abstractmethod
+    def dataset(self):
+        ...
+
+    @property
+    @abstractmethod
+    def criterion(self):
+        ...
 
 
-class DeepSpeedInit(object):
-    def __init__(self, model_name):
-        self.model_name = model_name
+class GPT2ModelSetup(BenchModelSetup):
+    @property
+    def model(self):
+        hf_config = transformers.GPT2Config(**self.model_config)
+        hf_model = transformers.GPT2LMHeadModel(hf_config)
+        return hf_model
 
-    def get_data(self, batch_size, steps, vocab_size, seq_length):
+    @property
+    def dataset(self):
+        batch_size = self.ds_config.get("train_micro_batch_size_per_gpu")
+        steps = self.args.steps_per_epoch
         world_size = int(os.getenv("WORLD_SIZE", "1"))
         size = batch_size * world_size * steps
-        return SyntheticDataset(size, vocab_size, seq_length)
+        vocab_size = self.model_config.get("vocab_size")
+        seq_length = self.model_config.get("seq_length")
+        return SyntheticTokenDataset(size, vocab_size, seq_length)
 
-    def get_model(self, config_kwargs, ds_config):
-        model_config = model_configs[self.model_name](**config_kwargs)
-        with deepspeed.zero.Init(config_dict_or_path=ds_config):
-            hf_model = ModelFromHF(model_config, model_classes[self.model_name])
-        model, _, _, _ = deepspeed.initialize(
-            model=hf_model, model_parameters=hf_model.parameters(), config=ds_config
-        )
-        return model
+    @property
+    def criterion(self):
+        return GPTLMLoss()
 
-    def get_criterion(self):
-        return model_criterion[self.model_name]()
+
+model_classes = {"gpt2": GPT2ModelSetup}
+
+
+def deepspeed_bench(model_setup):
+    hf_model = model_setup.model
+    dataset = model_setup.dataset
+    criterion = model_setup.criterion
+
+    model, _, _, _ = deepspeed.initialize(
+        model=hf_model,
+        model_parameters=hf_model.parameters(),
+        config=model_setup.ds_config,
+    )
+    model.train()
+
+    data = DataLoader(
+        dataset, batch_size=model_setup.ds_config["train_micro_batch_size_per_gpu"]
+    )
+
+    local_rank = int(os.getenv("LOCAL_RANK", "0"))
+    times = []
+    for step, batch in enumerate(data):
+        time_start = time.time_ns()
+        for key, val in batch.items():
+            batch[key] = val.to(f"cuda:{local_rank}")
+        outputs = model(**batch)
+
+        loss = criterion(outputs.logits, batch["labels"])
+        model.backward(loss)
+        model.step()
+        time_end = time.time_ns()
+
+        if step >= model_setup.args.warmup:
+            times.append((time_end - time_start) / (10**9))
+
+    return times
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", "-m", type=str, help="hf model name")
+    parser.add_argument(
+        "--steps-per-epoch", "-s", type=int, default=50, help="steps per epoch"
+    )
+    parser.add_argument("--epochs", "-e", type=int, default=10, help="number of epochs")
+    parser.add_argument(
+        "--warmup",
+        "-w",
+        type=int,
+        default=5,
+        help="number of steps before measuring performance",
+    )
+    parser.add_argument("--local_rank", type=int, default=0, help="local rank")
+    args = parser.parse_args()
+    return args
 
 
 def main():
-    gpt_config = {
+    gpt2_config = {
         "seq_length": 1024,
         "vocab_size": 50257,
         "hidden_size": 768,
@@ -120,7 +215,6 @@ def main():
         "evaluation": "ppl",
         "use_cache": False,
     }
-
     ds_config = {
         "train_micro_batch_size_per_gpu": 16,
         "optimizer": {"type": "Adam", "params": {"lr": 1e-4}},
@@ -135,40 +229,14 @@ def main():
             "contiguous_gradients": True,
         },
     }
+    args = parse_args()
 
-    ds = DeepSpeedInit(model_name="gpt2")
-
-    data_set = ds.get_data(batch_size=16, steps=100,
-        vocab_size=gpt_config["vocab_size"],
-        seq_length=gpt_config["seq_length"],
+    model_setup = model_classes[args.model](
+        model_config=gpt2_config, ds_config=ds_config, args=args
     )
-    data = DataLoader(data_set, batch_size=ds_config["train_micro_batch_size_per_gpu"])
-
-    model = ds.get_model(gpt_config, ds_config)
-    model.train()
-
-    criterion = ds.get_criterion()
-
-    times = []
-    iters = 100
-    warmup = 5
-    for step, batch in enumerate(data):
-        if step >= iters:
-            break
-        time_start = time.time_ns()
-        for key, val in batch.items():
-            batch[key] = val.to("cuda:0")
-        outputs = model(**batch)
-
-        loss = criterion(outputs, batch["labels"])
-        model.backward(loss)
-        model.step()
-        time_end = time.time_ns()
-
-        if step >= warmup:
-            times.append((time_end - time_start) / (10 ** 9))
-
+    times = deepspeed_bench(model_setup)
     print(np.mean(times))
+
 
 if __name__ == "__main__":
     main()
