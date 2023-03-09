0083a5ae66e771cb410e80e0f72199240bc4f9e0
Ma, Guokai <guokai.ma@intel.com>
2023-03-09 17:54:44 +0800

    add pre-commit check for torch.cuda() conversion

--- scripts/check-torchcuda.py
+++ scripts/check-torchcuda.py
@@ -38,3 +38,22 @@ elif res.returncode == 2:
     err(f"Error invoking grep on {', '.join(sys.argv[1:])}:")
     err(res.stderr.decode("utf-8"))
     sys.exit(2)
+
+res = subprocess.run(
+    ["git",
+     "grep",
+     "-Hn",
+     "--no-index",
+     r"\.cuda()",
+     *sys.argv[1:]],
+    capture_output=True,
+)
+if res.returncode == 0:
+    err('Error: The string ".cuda()" was found. This implies convert a tensor to cuda tensor.  Please replace all calls to tensor.cuda() with "tensor.to(get_accelerator().device_name())" and add the following import line:\nfrom deepspeed.accelerator import get_accelerator'
+        )
+    err(res.stdout.decode("utf-8"))
+    sys.exit(1)
+elif res.returncode == 2:
+    err(f"Error invoking grep on {', '.join(sys.argv[1:])}:")
+    err(res.stderr.decode("utf-8"))
+    sys.exit(2)
