704936796d6320f4e265a95ec128b0695a9061e7
Ma, Guokai <guokai.ma@intel.com>
2023-03-11 10:30:22 +0800

    add #ignore-cuda check to ignore code that mean to be cuda specific

--- scripts/check-torchcuda.py
+++ scripts/check-torchcuda.py
@@ -21,16 +21,23 @@ def err(s: str) -> None:
 #  - unlike plain grep, which is slower and has different flags on MacOS versus
 #    Linux, git grep is always the same.
 res = subprocess.run(
-    ["git",
-     "grep",
-     "-Hn",
-     "--no-index",
-     r"torch\.cuda",
-     *sys.argv[1:]],
+    [
+        "git",
+        "grep",
+        "-Hn",
+        "--no-index",
+        "-e",
+        r"torch\.cuda",
+        "--and",
+        "--not",
+        "-e",
+        "#ignore-cuda",
+        *sys.argv[1:]
+    ],
     capture_output=True,
 )
 if res.returncode == 0:
-    err('Error: The string "torch.cuda" was found. Please replace all calls to torch.cuda with "get_accelerator()" and add the following import line:\nfrom deepspeed.accelerator import get_accelerator'
+    err('Error: The string "torch.cuda" was found.\nPlease replace all calls to torch.cuda with "get_accelerator()" and add the following import line:\n\n    from deepspeed.accelerator import get_accelerator\n\nIf your code is mean to be cuda specific, please add the following comment in the line with torch.cuda:\n\n    #ignore-cuda\n'
         )
     err(res.stdout.decode("utf-8"))
     sys.exit(1)
