34bc4b1dbe22a5b7fb1e2ca8e6921a39da833b85
Logan Adams <loadams@microsoft.com>
2023-03-17 13:42:06 -0700

    Switch name from GPT2Model to GPTModel

--- tests/unit/megatron_model.py
+++ tests/unit/megatron_model.py
@@ -19,7 +19,7 @@ def get_megatron_version():
 
 
 def get_gpt2_model(args_others, mp_size=1):
-    from megatron.model import GPT2Model
+    from megatron.model import GPTModel
     from megatron.initialize import initialize_megatron
 
     args_defaults = {
