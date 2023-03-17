adb1adfc5d74a632365bff553712cc9d63562dcd
Guokai Ma <guokai.ma@gmail.com>
2023-03-17 15:21:45 +0800

    fix format issue

--- deepspeed/inference/engine.py
+++ deepspeed/inference/engine.py
@@ -31,7 +31,10 @@ from torch import nn
 
 INFERENCE_MODEL_TIMER = "model-forward-inference"
 
-def build_bloom_alibi_tensor(attention_mask: torch.Tensor, num_heads: int, dtype: torch.dtype) -> torch.Tensor:
+
+def build_bloom_alibi_tensor(attention_mask: torch.Tensor,
+                             num_heads: int,
+                             dtype: torch.dtype) -> torch.Tensor:
     """
     Link to paper: https://arxiv.org/abs/2108.12409 Alibi tensor is not causal as the original paper mentions, it
     relies on a translation invariance of softmax for quick implementation: with l being a tensor, and a fixed value
@@ -50,19 +53,26 @@ def build_bloom_alibi_tensor(attention_mask: torch.Tensor, num_heads: int, dtype
     """
     import math
     batch_size, seq_length = attention_mask.shape
-    closest_power_of_2 = 2 ** math.floor(math.log2(num_heads))
-    base = torch.tensor(
-        2 ** (-(2 ** -(math.log2(closest_power_of_2) - 3))), device=attention_mask.device, dtype=torch.float32
-    )
-    powers = torch.arange(1, 1 + closest_power_of_2, device=attention_mask.device, dtype=torch.int32)
+    closest_power_of_2 = 2**math.floor(math.log2(num_heads))
+    base = torch.tensor(2**(-(2**-(math.log2(closest_power_of_2) - 3))),
+                        device=attention_mask.device,
+                        dtype=torch.float32)
+    powers = torch.arange(1,
+                          1 + closest_power_of_2,
+                          device=attention_mask.device,
+                          dtype=torch.int32)
     slopes = torch.pow(base, powers)
 
     if closest_power_of_2 != num_heads:
-        extra_base = torch.tensor(
-            2 ** (-(2 ** -(math.log2(2 * closest_power_of_2) - 3))), device=attention_mask.device, dtype=torch.float32
-        )
+        extra_base = torch.tensor(2**(-(2**-(math.log2(2 * closest_power_of_2) - 3))),
+                                  device=attention_mask.device,
+                                  dtype=torch.float32)
         num_remaining_heads = min(closest_power_of_2, num_heads - closest_power_of_2)
-        extra_powers = torch.arange(1, 1 + 2 * num_remaining_heads, 2, device=attention_mask.device, dtype=torch.int32)
+        extra_powers = torch.arange(1,
+                                    1 + 2 * num_remaining_heads,
+                                    2,
+                                    device=attention_mask.device,
+                                    dtype=torch.int32)
         slopes = torch.cat([slopes, torch.pow(extra_base, extra_powers)], dim=0)
 
     # Note: alibi will added to the attention bias that will be applied to the query, key product of attention
@@ -78,10 +88,11 @@ def build_bloom_alibi_tensor(attention_mask: torch.Tensor, num_heads: int, dtype
         num_heads_per_rank = int(num_heads / dist.get_world_size())
         offset = dist.get_rank() * num_heads_per_rank
         alibi = alibi.view(batch_size, num_heads, 1, seq_length)
-        alibi = alibi[:, offset : num_heads_per_rank + offset, :, :]
+        alibi = alibi[:, offset:num_heads_per_rank + offset, :, :]
         return alibi.reshape(batch_size * num_heads_per_rank, 1, seq_length)
     else:
         return alibi.reshape(batch_size * num_heads, 1, seq_length).to(dtype)
+
 
 class InferenceEngine(Module):
     inference_mp_group = None
@@ -142,7 +153,6 @@ class InferenceEngine(Module):
         else:
             # This is a hack to redefine the alibi func due to TP
             self.build_alibi_tensor()
-
 
         if get_accelerator().device_name() == 'cuda' and config.enable_cuda_graph:
             assert pkg_version.parse(torch.__version__) >= pkg_version.parse("1.10"), \
