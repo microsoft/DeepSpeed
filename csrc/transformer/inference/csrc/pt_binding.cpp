251e82eca931b4916f305a751942e1cb98de874d
Jeff Rasley <jerasley@microsoft.com>
2023-03-21 15:02:22 -0700

    formatting fixes

--- csrc/transformer/inference/csrc/pt_binding.cpp
+++ csrc/transformer/inference/csrc/pt_binding.cpp
@@ -525,14 +525,21 @@ std::vector<at::Tensor> ds_softmax_context(at::Tensor& query_key_value,
                                1);
 
     if (layer_id == num_layers - 1) Context::Instance().advance_tokens();
-    auto prev_key = torch::from_blob(workspace + offset, {bsz, heads, all_tokens, k},
-                                     {hidden_dim * Context::Instance().GetMaxTokenLenght(), k * Context::Instance().GetMaxTokenLenght(), k, 1},
+    auto prev_key = torch::from_blob(workspace + offset,
+                                     {bsz, heads, all_tokens, k},
+                                     {hidden_dim * Context::Instance().GetMaxTokenLenght(),
+                                      k * Context::Instance().GetMaxTokenLenght(),
+                                      k,
+                                      1},
                                      options);
 
-    auto prev_value =
-        torch::from_blob(workspace + offset + value_offset, {bsz, heads, all_tokens, k},
-                         {hidden_dim * Context::Instance().GetMaxTokenLenght(), k * Context::Instance().GetMaxTokenLenght(), k, 1},
-                         options);
+    auto prev_value = torch::from_blob(workspace + offset + value_offset,
+                                       {bsz, heads, all_tokens, k},
+                                       {hidden_dim * Context::Instance().GetMaxTokenLenght(),
+                                        k * Context::Instance().GetMaxTokenLenght(),
+                                        k,
+                                        1},
+                                       options);
 
     return {output, prev_key, prev_value};
 }
