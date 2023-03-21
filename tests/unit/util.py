9374d8a491102b6017d93720195b8f9470dd5724
Logan Adams <loadams@microsoft.com>
2023-03-21 16:58:50 -0700

    Move cuda check into utils too

--- tests/unit/util.py
+++ tests/unit/util.py
@@ -15,6 +15,17 @@ def skip_on_arch(min_arch=7):
         return
 
 
+def skip_on_cuda(valid_cuda=[101, 102, 110, 111, 112, 116, 117]):
+    if deepspeed.accelerator.get_accelerator().device_name() == 'cuda':
+        CUDA_MAJOR, CUDA_MINOR = split_version(torch_info['cuda_version'])
+        CUDA_VERSION = (CUDA_MAJOR * 10) + CUDA_MINOR
+        if valid_cuda.count(CUDA_VERSION) = 0:
+           pytest.skip("requires cuda versions {valid_cuda}")
+    else:
+        assert deepspeed.accelerator.get_accelerator().device_name() == 'xpu'
+        return
+
+
 def required_torch_version():
     TORCH_MAJOR = int(torch.__version__.split('.')[0])
     TORCH_MINOR = int(torch.__version__.split('.')[1])
