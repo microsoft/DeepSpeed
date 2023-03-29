74e2c800f11b6c73fd736c3e657ac6935fb5a1f7
mayank31398 <mayank31398@gmail.com>
2023-03-30 01:52:40 +0530

    incorporate suggestion

--- benchmarks/communication/all_gather.py
+++ benchmarks/communication/all_gather.py
@@ -1,16 +1,13 @@
 '''Copyright The Microsoft DeepSpeed Team'''
 
 from benchmarks.communication.utils import *
-from benchmarks.communication.constants import *
 from deepspeed.accelerator import get_accelerator
-from deepspeed.comm import TorchBackend
+import time
 
 COMMS_BENCH_DIR = os.path.join(os.path.dirname(__file__), "../")
 sys.path.append(COMMS_BENCH_DIR)
 
-from communication.utils import *
-from communication.constants import *
-from deepspeed.accelerator import get_accelerator
+from deepspeed.comm import TorchBackend
 
 
 # Run all_gather and print metrics
