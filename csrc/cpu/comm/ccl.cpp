71ad82e51283c532f4324e4d279a56a95da111e9
Ma, Guokai <guokai.ma@intel.com>
2023-04-06 09:16:55 +0800

    clean up ccl.cpp

--- csrc/cpu/comm/ccl.cpp
+++ csrc/cpu/comm/ccl.cpp
@@ -4,9 +4,7 @@
 
 std::set<int> _comm_ids;
 std::set<int> _colors;
-// std::unordered_map<int, ccl::communicator> _ccl_comms;
 ccl::vector_class<ccl::communicator> _ccl_comms;
-// void create_comm_group(std::vector<int> comm_ranks, int rank, int comm_id, int color);
 
 #define CCLCHECK(cmd) \
     do {              \
@@ -88,10 +86,6 @@ py::object new_group(std::vector<int> ranks)
     int color = next_unique_val(_colors);
     std::cout << "RANK: " << get_rank() << " COMM_ID: " << comm_id << " COLOR: " << color
               << std::endl;
-    // create_comm_group(ranks, get_rank(), comm_id, color);
-    // py::object ProcessGroup = py::module_::import("deepspeed.comm").attr("ProcessGroup");
-    // py::object newPG = ProcessGroup(comm_id, ranks);
-    // return newPG;
 }
 
 ccl::datatype get_ccl_datatype(c10::ScalarType type)
@@ -107,111 +101,6 @@ ccl::datatype get_ccl_datatype(c10::ScalarType type)
     }
     return ccl_type;
 }
-
-#if 0
-cclComm_t _get_comm_from_group(py::object group);
-
-cudaStream_t s;
-cclComm_t _world_ccl_comm;
-
-//py::module_ dist = py::module_::import("deepspeed.comm");
-
-std::vector<cclComm_t> global_ccl_comms;
-std::vector<cudaStream_t> global_streams;
-
-
-//REZA+AMMAR CODE
-//curandGenerator_t _gen;
-//cublasHandle_t _cublasHandle;
-cudaEvent_t _comp_event;
-cudaEvent_t _comm_event;
-void* _workspace;
-uint64_t _seed;
-uint64_t _curr_offset;
-size_t _workSpaceSize;
-unsigned _token_length;
-unsigned _num_tokens;
-std::vector<std::array<int, 3>> _gemm_algos;
-cudaStream_t _comp_stream;
-cudaStream_t _comm_stream;
-MPI_Group _group;
-std::unordered_map<int, int> _world_sizes;
-std::unordered_map<int, int> _color_map;
-MPI_Comm _comm;
-bool _comm_created;
-//py::object ProcessGroup = py::module_::import("deepspeed.comm").attr("ProcessGroup");
-//py::object world_group;
-
-int get_rank(int group = 0)
-{
-    int world_rank;
-    MPICHECK(MPI_Comm_rank(MPI_COMM_WORLD, &world_rank));
-    return world_rank;
-}
-
-
-// Given a cclUniqueId, convert it to a string representation that can be put
-// in the store.
-std::string buildCclUniqueIdStr(const cclUniqueId& cclID)
-{
-    const uint8_t* bytes = reinterpret_cast<const uint8_t*>(&cclID);
-    std::ostringstream oss;
-    for (const auto i : c10::irange(CCL_UNIQUE_ID_BYTES)) {
-        oss << std::hex << static_cast<int>(bytes[i]);
-    }
-    return oss.str();
-}
-
-std::string getCclId()
-{
-    cclUniqueId cclID;
-    CCLCHECK(cclGetUniqueId(&cclID));
-    return buildCclUniqueIdStr(cclID);
-
-    // std::string id = "hello";
-    // for (int i=0; i<128; i++)
-    //     std::cout << "cclID =" << cclID[i];
-    // std::cout<< std::endl;
-    // return id;
-}
-
-
-/*
-py::object get_world_group() {
-    int world_size = get_world_size(0);
-    std::vector<int> ranks(world_size);
-    std::iota(ranks.begin(), ranks.end(), 0);
-    py::object ProcessGroup = py::module_::import("deepspeed.comm").attr("ProcessGroup");
-    return ProcessGroup(0, ranks);
-}
-*/
-
-void _print_comm_number() { std::cout << "Number of Sub-Comms:" << _ccl_comms.size() + 1 << "\n"; }
-
-cudaStream_t GetCommStream(bool async_op = false)
-{
-    if (!_comm_stream)
-        _comm_stream = async_op ? at::cuda::getStreamFromPool(true)
-                                : at::cuda::getCurrentCUDAStream();
-    return _comm_stream;
-}
-
-void finalize()
-{
-    CCLCHECK(cclCommDestroy(_world_ccl_comm));
-}
-
-void test_set() {
-    std::set<int> val1 = {6, 5, 10, 1};
-    std::set<int> val2 = {};
-    std::set<int> val3 = {0};
-    std::set<int> val4 = {0,1,2,3,6,4};
-    if (get_rank() == 0) {
-        std::cout << next_unique_val(val4) << std::endl;
-    }
-}
-
-#endif
 
 ccl::reduction get_ccl_reduce_op(py::object op, at::Tensor& input)
 {
@@ -250,21 +139,7 @@ ccl::reduction get_ccl_reduce_op(py::object op, at::Tensor& input)
 
 ccl::communicator& _get_comm_from_group() { return _ccl_comms[0]; }
 
-ccl::communicator& _get_comm_from_group(py::object group)
-{
-    return _ccl_comms[0];
-#if 0
-    if (group == Py_None) {
-        return _ccl_comms[0];
-    } else {
-        py::object ProcessGroup = py::module_::import("deepspeed.comm").attr("ProcessGroup");
-        if (!py::isinstance(group, ProcessGroup)) {
-            throw std::runtime_error ("Error: group must be of type ProcessGroup");
-        }
-        return _ccl_comms[py::int_(group.attr("comm_id"))];
-    }
-#endif
-}
+ccl::communicator& _get_comm_from_group(py::object group) { return _ccl_comms[0]; }
 
 void broadcast(torch::Tensor& data, int src, py::object group, bool async_op)
 {
@@ -328,18 +203,4 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
     m.def("all_reduce", &all_reduce, "ccl all_reduce");
     m.def("all_reduce_caching", &all_reduce_caching, "ccl all_reduce with caching");
     m.def("barrier", &barrier, "barrier");
-    // m.def("new_group", &new_group, "automatically create comm group");
-    // m.def("getCclId", &getCclId, "Get Unique CCL ID");
-    // m.def("finalize", &finalize, "ccl finalize");
-    // m.def("send", &send, "ccl send");
-    // m.def("recv", &recv, "ccl recv");
-    // m.def("all_to_all_single", &all_to_all_single, "ccl alltoall");
-    // m.def("all_toall_list", &all_to_all, "ccl alltoall list");
-    // m.def("all_gather_base", &all_gather_base, "ccl all_gather_base");
-    // m.def("all_gather", &all_gather, "ccl all_gather");
-    // m.def("reduce", &reduce, "ccl reduce");
-    // m.def("reduce_scatter", &reduce_scatter, "ccl reduce scatter");
-    // m.def("create_comm_group", &create_comm_group, "manually create comm group");
-    // m.def("test_set", &test_set, "manually create comm group");
-    // m.def("get_world_group", &get_world_group, "Returns the WORLD process group");
 }
