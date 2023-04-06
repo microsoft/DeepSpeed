4740aec2b1b8da707d4704e14cec702a8c964439
Logan Adams <loadams@microsoft.com>
2023-04-06 13:43:34 -0700

    Update triton check

--- op_builder/sparse_attn.py
+++ op_builder/sparse_attn.py
@@ -69,10 +69,10 @@ class SparseAttnBuilder(OpBuilder):
 
         if pkg_version:
             installed_triton = pkg_version.parse(triton.__version__)
-            triton_mismatch = installed_triton != pkg_version.parse("1.0.0")
+            triton_mismatch = installed_triton != pkg_version.parse("1.0.0") and installed_triton != pkg_version.parse("2.0.0")
         else:
             installed_triton = triton.__version__
-            triton_mismatch = installed_triton != "1.0.0"
+            triton_mismatch = installed_triton != "1.0.0" or installed_triton != "2.0.0"
 
         if triton_mismatch:
             self.warning(f"using untested triton version ({installed_triton}), only 1.0.0 is known to be compatible")
