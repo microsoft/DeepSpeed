bd74c32a6640dd61b4cf0e48fc30d5e871d7ebe5
Connor Holmes <connorholmes@microsoft.com>
2023-04-12 18:27:51 +0000

    Further generalize rotate half rotary position embeddings

--- csrc/transformer/inference/csrc/apply_rotary_pos_emb.cu
+++ csrc/transformer/inference/csrc/apply_rotary_pos_emb.cu
@@ -16,11 +16,10 @@
 namespace cg = cooperative_groups;
 
 namespace rot_half {
-constexpr int granularity = 16;
 constexpr int threads = 256;
 }  // namespace rot_half
 
-template <typename T, int threadsPerHead>
+template <typename T, int threadsPerHead, int granularity>
 __global__ void apply_rotary_pos_half(T* mixed_query,
                                       T* key_layer,
                                       unsigned rotary_dim,
@@ -31,7 +30,7 @@ __global__ void apply_rotary_pos_half(T* mixed_query,
                                       unsigned total_count,
                                       int max_out_tokens)
 {
-    constexpr int T_per_thread = rot_half::granularity / sizeof(T);
+    constexpr int T_per_thread = granularity / sizeof(T);
     constexpr int heads_per_block = rot_half::threads / threadsPerHead;
 
     cg::thread_block tb = cg::this_thread_block();
@@ -50,8 +49,8 @@ __global__ void apply_rotary_pos_half(T* mixed_query,
         const int base_neuron_idx = head_group.thread_rank() * T_per_thread;
 
         T q[T_per_thread], k[T_per_thread];
-        mem_access::load_global<rot_half::granularity>(q, mixed_query + offset + base_neuron_idx);
-        mem_access::load_global<rot_half::granularity>(k, key_layer + k_offset + base_neuron_idx);
+        mem_access::load_global<granularity>(q, mixed_query + offset + base_neuron_idx);
+        mem_access::load_global<granularity>(k, key_layer + k_offset + base_neuron_idx);
 
 #pragma unroll
         for (int i = 0; i < T_per_thread; i++) {
@@ -78,21 +77,51 @@ __global__ void apply_rotary_pos_half(T* mixed_query,
             }
         }
 
-        mem_access::store_global<rot_half::granularity>(mixed_query + offset + base_neuron_idx, q);
-        mem_access::store_global<rot_half::granularity>(key_layer + k_offset + base_neuron_idx, k);
+        mem_access::store_global<granularity>(mixed_query + offset + base_neuron_idx, q);
+        mem_access::store_global<granularity>(key_layer + k_offset + base_neuron_idx, k);
     }
 }
 
-#define LAUNCH_ROT_POS_EMB_HALF(HEAD_THREADS)                                       \
-    apply_rotary_pos_half<T, HEAD_THREADS><<<grid, block, 0, stream>>>(mixed_query, \
-                                                                       key_layer,   \
-                                                                       rotary_dim,  \
-                                                                       seq_len,     \
-                                                                       offset,      \
-                                                                       num_heads,   \
-                                                                       head_size,   \
-                                                                       total_count, \
-                                                                       max_out_tokens);
+#define LAUNCH_ROT_POS_EMB_HALF(HEAD_THREADS, ALIGNMENT)                                       \
+    apply_rotary_pos_half<T, HEAD_THREADS, ALIGNMENT><<<grid, block, 0, stream>>>(mixed_query, \
+                                                                                  key_layer,   \
+                                                                                  rotary_dim,  \
+                                                                                  seq_len,     \
+                                                                                  offset,      \
+                                                                                  num_heads,   \
+                                                                                  head_size,   \
+                                                                                  total_count, \
+                                                                                  max_out_tokens);
+
+#ifdef __HIP_PLATFORM_HCC__
+#define LAUNCH_FOR_ALIGNMENT(ALIGNMENT)         \
+    if (threads_per_head == 4) {                \
+        LAUNCH_ROT_POS_EMB_HALF(4, ALIGNMENT);  \
+    } else if (threads_per_head == 8) {         \
+        LAUNCH_ROT_POS_EMB_HALF(8, ALIGNMENT);  \
+    } else if (threads_per_head == 16) {        \
+        LAUNCH_ROT_POS_EMB_HALF(16, ALIGNMENT); \
+    } else if (threads_per_head == 32) {        \
+        LAUNCH_ROT_POS_EMB_HALF(32, ALIGNMENT); \
+    } else if (threads_per_head == 64) {        \
+        LAUNCH_ROT_POS_EMB_HALF(64, ALIGNMENT); \
+    } else {                                    \
+        assert(false);                          \
+    }
+#else
+#define LAUNCH_FOR_ALIGNMENT(ALIGNMENT)         \
+    if (threads_per_head == 4) {                \
+        LAUNCH_ROT_POS_EMB_HALF(4, ALIGNMENT);  \
+    } else if (threads_per_head == 8) {         \
+        LAUNCH_ROT_POS_EMB_HALF(8, ALIGNMENT);  \
+    } else if (threads_per_head == 16) {        \
+        LAUNCH_ROT_POS_EMB_HALF(16, ALIGNMENT); \
+    } else if (threads_per_head == 32) {        \
+        LAUNCH_ROT_POS_EMB_HALF(32, ALIGNMENT); \
+    } else {                                    \
+        assert(false);                          \
+    }
+#endif
 
 template <typename T>
 void launch_apply_rotary_pos_emb(T* mixed_query,
@@ -106,7 +135,19 @@ void launch_apply_rotary_pos_emb(T* mixed_query,
                                  cudaStream_t stream,
                                  int max_out_tokens)
 {
-    constexpr int T_per_elem = rot_half::granularity / sizeof(T);
+    const int half_dim = rotary_dim >> 1;
+
+    int alignment = sizeof(T);
+    if (half_dim % (16 / sizeof(T)) == 0) {
+        alignment = 16;
+    } else if (half_dim % (8 / sizeof(T)) == 0) {
+        alignment = 8;
+    } else if (half_dim % (4 / sizeof(T)) == 0) {
+        alignment = 4;
+    } else {
+        assert(false);
+    }
+    const int T_per_elem = alignment / sizeof(T);
 
     int total_count = batch * num_heads * seq_len;
 
@@ -120,21 +161,13 @@ void launch_apply_rotary_pos_emb(T* mixed_query,
     dim3 block(rot_half::threads);
     dim3 grid((total_count + heads_per_block - 1) / heads_per_block);
 
-    if (threads_per_head == 4) {
-        LAUNCH_ROT_POS_EMB_HALF(4);
-    } else if (threads_per_head == 8) {
-        LAUNCH_ROT_POS_EMB_HALF(8);
-    } else if (threads_per_head == 16) {
-        LAUNCH_ROT_POS_EMB_HALF(16);
-    } else if (threads_per_head == 32) {
-        LAUNCH_ROT_POS_EMB_HALF(32);
-    }
-#ifdef __HIP_PLATFORM_HCC__
-    else if (threads_per_head == 64) {
-        LAUNCH_ROT_POS_EMB_HALF(64);
-    }
-#endif
-    else {
+    if (alignment == 4) {
+        LAUNCH_FOR_ALIGNMENT(4);
+    } else if (alignment == 8) {
+        LAUNCH_FOR_ALIGNMENT(8);
+    } else if (alignment == 16) {
+        LAUNCH_FOR_ALIGNMENT(16);
+    } else {
         assert(false);
     }
 }
