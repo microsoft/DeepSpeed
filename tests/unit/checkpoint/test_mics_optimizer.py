9bbc91a05658687ee7484489e53d6cf773f52687
Zhen Zhang <zhzhn@amazon.com>
2023-05-04 06:48:58 +0000

    copyright

--- tests/unit/checkpoint/test_mics_optimizer.py
+++ tests/unit/checkpoint/test_mics_optimizer.py
@@ -2,6 +2,10 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # DeepSpeed Team
+
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 import deepspeed
 
 from unit.common import DistributedTest
