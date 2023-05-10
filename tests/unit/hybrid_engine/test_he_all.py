7cfde0c5e0011348f044827868001a6321d0fecf
Ammar Ahmad Awan <ammar.awan@microsoft.com>
2023-05-10 13:46:50 -0700

    Add/rename unit tests.

--- tests/unit/hybrid_engine/test_he_all.py
+++ tests/unit/hybrid_engine/test_he_all.py
@@ -12,8 +12,6 @@ from unit.common import DistributedTest
 
 from transformers import (AutoConfig, AutoTokenizer, AutoModelForCausalLM)
 
-pytest.skip("skip test for now, will fix in follow-up PR", allow_module_level=True)
-
 rocm_version = OpBuilder.installed_rocm_version()
 if rocm_version != (0, 0):
     pytest.skip("skip inference tests on rocm for now", allow_module_level=True)
@@ -35,24 +33,34 @@ class TestHybridEngineTextGen(DistributedTest):
         outputs = tokenizer.batch_decode(output, skip_special_tokens=True)
         return outputs
 
-    def test(self, batch_size, model_name):
+    def get_model(self, model_name):
         local_rank = int(os.getenv("LOCAL_RANK", "0"))
-
         model_config = AutoConfig.from_pretrained(model_name)
         model_config.dropout = 0.0
         model = AutoModelForCausalLM.from_pretrained(model_name, config=model_config)
         model = model.to(f'cuda:{local_rank}')
         model = model.half()
+        return model
 
+    def get_tokenizer(self, model_name):
         tokenizer = AutoTokenizer.from_pretrained(model_name)
         tokenizer.pad_token = tokenizer.eos_token
-
+        return tokenizer
+    
+    def get_prompt(self, batch_size):
         if batch_size == 1:
             prompt = ["Microsoft is in Washington"]
         elif batch_size == 2:
             prompt = ["DeepSpeed is", "Microsoft is in Washington"]
         else:
             raise NotImplementedError(f"batch_size {batch_size} not implemented")
+        return prompt
+    
+    def test_correctness(self, batch_size, model_name):
+        pytest.skip("skip test for now, will fix in follow-up PR")
+        model = self.get_model(model_name)
+        tokenizer = self.get_tokenizer(model_name)
+        prompt = self.get_prompt(batch_size)
 
         base_out = self._generate(model, tokenizer, prompt)
 
@@ -67,3 +75,20 @@ class TestHybridEngineTextGen(DistributedTest):
         model.eval()
         ds2_out = self._generate(model, tokenizer, prompt)
         assert base_out == ds2_out
+    
+    def test_functionality(self, batch_size, model_name):        
+        model = self.get_model(model_name)
+        tokenizer = self.get_tokenizer(model_name)
+        prompt = self.get_prompt(batch_size)
+
+        ds_config = {"train_batch_size": 1, "fp16": {"enabled": True}, "hybrid_engine": {"enabled": True}}
+        model, *_ = deepspeed.initialize(model=model, config=ds_config)
+
+        model.eval()
+        ds1_out = self._generate(model, tokenizer, prompt)
+        
+        model.train()
+        model.eval()
+        ds2_out = self._generate(model, tokenizer, prompt)
+        
+        assert ds1_out == ds2_out, f"ds1_out: {ds1_out}, ds2_out: {ds2_out}"
