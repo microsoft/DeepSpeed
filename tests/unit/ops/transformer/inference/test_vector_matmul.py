84d189c029a788c8ee49b165a59455e1fb8cc0c7
Molly Smith <mosm@microsoft.com>
2023-06-08 02:41:53 +0000

    Passing matmul unit test

--- tests/unit/ops/transformer/inference/test_vector_matmul.py
+++ tests/unit/ops/transformer/inference/test_vector_matmul.py
@@ -18,7 +18,7 @@ torch_minor_version = None
 
 
 def run_vector_matmul_reference(input_tensor, weight):
-    return torch.matmul(input_tensor, weight.transpose(0, 1)).to('cuda:0')
+    return torch.matmul(input_tensor, weight.transpose(0, 1))
 
 
 def run_vector_matmul_ds(input_tensor, weight):
@@ -29,12 +29,19 @@ def run_vector_matmul_ds(input_tensor, weight):
     q_scale = weight.scale if hasattr(weight, 'scale') else torch.empty(1)
     q_int8 = False
     async_op = False
-    transposed_mode = False
+    transposed_mode = True
 
     #input_tensor = input_tensor.flatten()
     #weight = weight.flatten()
     print(input_tensor.size())
     print(weight.size())
+
+    inference_module.allocate_workspace_fp32(input_tensor.size()[1], 1,
+                                    weight.size()[1],
+                                    weight.size()[0], 0, 1,
+                                    False,
+                                    0, 100,
+                                    50)
 
     if input_tensor.dtype == torch.float16:
         return inference_module.vector_matmul_fp16(input_tensor, weight, async_op, q_scale, q_int8, transposed_mode)
@@ -48,11 +55,11 @@ def run_vector_matmul_ds(input_tensor, weight):
 @pytest.mark.parametrize("batch_size", [4])
 @pytest.mark.parametrize("in_features", [128])
 @pytest.mark.parametrize("out_features", [128])
-@pytest.mark.parametrize("dtype", [torch.float16])
+@pytest.mark.parametrize("dtype", get_dtypes())
 def test_vector_matmul(batch_size, in_features, out_features, dtype):
 
-    input_tensor_ds = torch.randn(1, batch_size, in_features, dtype=dtype).to('cuda:0')
-    weight_tensor_ds = torch.randn(out_features, in_features, dtype=dtype).to('cuda:0')
+    input_tensor_ds = torch.randn(1, batch_size, in_features, dtype=dtype, device=get_accelerator().device_name())
+    weight_tensor_ds = torch.randn(out_features, in_features, dtype=dtype, device=get_accelerator().device_name())
 
     print(input_tensor_ds.device)
     print(weight_tensor_ds.device)
