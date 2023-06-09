c2818eac6cabe972ec523653ed1fcc603edb8809
Joe Mayer <jomayeri@microsoft.com>
2023-06-09 18:48:32 +0000

    full fp8 test

--- tests/unit/runtime/half_precision/test_fp8.py
+++ tests/unit/runtime/half_precision/test_fp8.py
@@ -11,32 +11,32 @@ from unit.simple_model import random_dataloader
 import transformer_engine.pytorch as te
 from transformer_engine.common import recipe
 
-@pytest.mark.parametrize("base_datatype", ["fp16"])
+
+@pytest.mark.parametrize("base_datatype", ["fp16", "bf16", "fp32"])
 class TestFp8ComposabilityAcrossZero(DistributedTest):
     world_size = 1
 
     def test(self, base_datatype):
-        hidden_dim = 4096
-        if base_datatype == "fp16":
-            model_dtype = torch.float16
-        elif base_datatype == "bf16":
-            model_dtype = torch.bfloat16
-        else:
-            model_dtype = torch.float32
-
-        torch.random.manual_seed(15)
-        # TransformerEngine Model
-        model = te.Linear(hidden_dim, hidden_dim, bias=True, params_dtype=model_dtype).cuda()
-
-        # Create FP8 recipe. Note: All input args are optional.
-        fp8_recipe = recipe.DelayedScaling(fp8_format=recipe.Format.HYBRID,
-                                           amax_history_len=16,
-                                           amax_compute_algo="max")
-        # config
-        zero_stage = [0, 1, 2, 3]
-        for stage in zero_stage:
+        def run_zero(stage, model_dtype):
+            num_batches = 128
+            batch_size = 16
+            hidden_dim = 768
+            torch.random.manual_seed(42)
+            enable_fp16 = False
+            enable_bf16 = False
+            if model_dtype == torch.float16:
+                enable_fp16 = True
+            elif model_dtype == torch.bfloat16:
+                enable_bf16 = True
+            # TransformerEngine Model
+            model = te.Linear(hidden_dim, hidden_dim, bias=True, params_dtype=model_dtype)
+        
+            # Create FP8 recipe. Note: All input args are optional.
+            fp8_recipe = recipe.DelayedScaling(fp8_format=recipe.Format.HYBRID,
+                                                   amax_history_len=16,
+                                                   amax_compute_algo="max")
             config = {
-                "train_batch_size": 8,
+                "train_batch_size": batch_size,
                 "gradient_accumulation_steps": 1,
                 "optimizer": {
                     "type": "Adam",
@@ -45,26 +45,42 @@ class TestFp8ComposabilityAcrossZero(DistributedTest):
                     }
                 },
                 "zero_optimization": {
-                    "stage": stage,
+                    "stage": stage
                 },
                 "fp16": {
-                    "enabled": True,
+                    "enabled": enable_fp16,
                     "loss_scale": 0.1
                 },
                 "bf16": {
-                    "enabled": False
+                    "enabled": enable_bf16
                 }
             }
             # Init DeepSpeed
             model, optimizer, _, _ = deepspeed.initialize(args=None, model=model,
                                                           model_parameters=model.parameters(), config=config)
-
-            data = torch.randn(128, hidden_dim, device=model.device, dtype=model_dtype)
-            for datum in data:
+            
+            batches = torch.randn(num_batches, batch_size, hidden_dim, device=model.device, dtype=model_dtype)
+            for batch in batches:
                 # Enables autocasting for the forward pass
                 with te.fp8_autocast(enabled=True, fp8_recipe=fp8_recipe):
-                    out = model(datum)
+                    out = model(batch)
                 loss = out.mean()
                 model.backward(loss)
                 model.step()
-            print(loss)
+            return loss
+
+        if base_datatype == "fp16":
+            model_dtype = torch.float16
+        elif base_datatype == "bf16":
+            model_dtype = torch.bfloat16
+        else:
+            model_dtype = torch.float32
+
+        # config
+        zero_stage = [0,1,2,3]
+        losses = []
+        for stage in zero_stage:
+            loss = run_zero(stage, model_dtype)
+            losses.append(loss)
+        all_equal = all(torch.allclose(loss, losses[0], 1e-07, 1e-05) for loss in losses)
+        assert (all_equal)
