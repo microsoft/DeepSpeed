1a11bc7929167d092d6b2bb16c8fd98e21490afc
Guillaume Sautiere <gsautie@qti.qualcomm.com>
2023-06-28 10:46:00 +0200

    fix rnn flop profiler to compute flops instead of macs

--- deepspeed/profiling/flops_profiler/profiler.py
+++ deepspeed/profiling/flops_profiler/profiler.py
@@ -960,10 +960,11 @@ def _reload_tensor_methods():
 
 
 def _rnn_flops(flops, rnn_module, w_ih, w_hh, input_size):
+    input_size, hidden_size = w_ih.shape
     # matrix matrix mult ih state and internal state
-    flops += w_ih.shape[0] * w_ih.shape[1]
+    flops += 2 * input_size * hidden_size - hidden_size
     # matrix matrix mult hh state and internal state
-    flops += w_hh.shape[0] * w_hh.shape[1]
+    flops += 2 * hidden_size * hidden_size - hidden_size
     if isinstance(rnn_module, (nn.RNN, nn.RNNCell)):
         # add both operations
         flops += rnn_module.hidden_size
