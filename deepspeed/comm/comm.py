c5e55d3d1477eeb839cf84362fb41954712118a6
hipudding <huafengchun@gmail.com>
2023-07-07 05:56:33 +0800

    Fix a typo of global variable in comm.py(#3852) (#3852)
    This is a bugfix for #3851, global variable cdb is mistakenly written as
    cbd. There's no local variable of the same name in this function, so no
    effect on functionality yet.
    
    Co-authored-by: Michael Wyatt <michaelwyatt@microsoft.com>

--- deepspeed/comm/comm.py
+++ deepspeed/comm/comm.py
@@ -490,7 +490,7 @@ def all_reduce_coalesced(tensors,
                          prof=False,
                          log_name='all_reduce',
                          debug=get_caller_func()):
-    global cbd
+    global cdb
     return cdb.all_reduce_coalesced(tensors, op, group, async_op)
 
 
