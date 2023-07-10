0d2699ace9d474880b724591c146bd5966af25b6
Ma, Guokai <guokai.ma@intel.com>
2023-07-10 10:38:11 +0800

    add support for fakenuma support

--- deepspeed/utils/numa.py
+++ deepspeed/utils/numa.py
@@ -126,6 +126,9 @@ def get_numactl_cmd(bind_core_list, num_local_procs, local_rank):
 
     non_empty_numa_list = []
     empty_numa_list = []
+    previous_numa_cores = []
+    numa_node_list = []
+    numa_node_list_list = []
     for i in range(num_numas):
         # look for empty numa which is HBM numa
         if numa_cores[i] == []:
@@ -133,9 +136,25 @@ def get_numactl_cmd(bind_core_list, num_local_procs, local_rank):
         else:
             non_empty_numa_list.append(i)
 
+            # check for fakenuma
+            if numa_cores[i] == previous_numa_cores:
+                if numa_node_list == []:
+                    #first duplication, add previous node into list
+                    numa_node_list.append(i - 1)
+                numa_node_list.append(i)
+            else:
+                if numa_node_list != []:
+                    numa_node_list_list.append(numa_node_list)
+                    numa_node_list = []
+        previous_numa_cores = numa_cores[i]
+    if numa_node_list != []:
+        numa_node_list_list.append(numa_node_list)
+
     if empty_numa_list != [] and len(empty_numa_list) == len(non_empty_numa_list):
         numa_mode = "flat_hbm"
         numa_dict = dict(zip(non_empty_numa_list, empty_numa_list))
+    elif numa_node_list_list != []:
+        numa_mode = "fake"
 
     if numa_mode == "normal":
         for i in range(num_numas):
@@ -148,6 +167,18 @@ def get_numactl_cmd(bind_core_list, num_local_procs, local_rank):
             if set(core_list_for_rank) <= set(numa_cores[i]):
                 numactl_cmd.append("-p")
                 numactl_cmd.append(f"{numa_dict[i]}")
+                break
+    elif numa_mode == "fake":
+        for i in range(num_numas):
+            if set(core_list_for_rank) <= set(numa_cores[i]):
+                for nodes in numa_node_list_list:
+                    if i in nodes:
+                        numactl_cmd.append("-m")
+                        numactl_cmd.append(f"{','.join(map(str, nodes))}")
+                        break
+                # the following construct break the outer loop if inner loop breaks
+                else:
+                    continue
                 break
 
     numactl_cmd.append("-C")
