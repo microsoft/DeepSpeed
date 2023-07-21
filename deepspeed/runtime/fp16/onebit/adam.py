ce782ff4eeb1becdfec9ca63c9f9eb49ad0a69c5
Logan Adams <loadams@microsoft.com>
2023-07-21 10:28:22 -0700

    Remove additional torch_major/minor

--- deepspeed/runtime/fp16/onebit/adam.py
+++ deepspeed/runtime/fp16/onebit/adam.py
@@ -7,6 +7,7 @@ import types
 import torch
 import numpy as np
 from deepspeed.accelerator import get_accelerator
+from deepspeed.runtime.utils import required_torch_version
 from deepspeed import comm as dist
 
 
@@ -91,7 +92,7 @@ class OnebitAdam(torch.optim.Optimizer):
         if self.comm_backend_name == 'nccl':
             assert (
                 required_torch_version(min_version=1.8)
-                ), "Please use torch 1.8 or greater to enable NCCL backend in 1-bit Adam. Alternatively, please specify 'mpi' as the 'comm_backend_name' in config file to proceed with the MPI backend"
+            ), "Please use torch 1.8 or greater to enable NCCL backend in 1-bit Adam. Alternatively, please specify 'mpi' as the 'comm_backend_name' in config file to proceed with the MPI backend"
             assert dist.is_initialized() == True, "Please initialize the torch distributed backend."
             from deepspeed.runtime.comm.nccl import NcclBackend
             self.using_pipeline = hasattr(self.deepspeed, 'pipeline_enable_backward_allreduce')
