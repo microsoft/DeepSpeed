37642aa37d40629f0509d25b053ffa5324143fa6
rraminen <rraminen@amd.com>
2023-08-09 05:15:38 +0000

    Enable quantizer extension on ROCm

--- op_builder/quantizer.py
+++ op_builder/quantizer.py
@@ -28,7 +28,14 @@ class QuantizerBuilder(CUDAOpBuilder):
         ]
 
     def include_paths(self):
-        return ['csrc/includes']
+        includes = ['csrc/includes']
+        if self.is_rocm_pytorch():
+            from torch.utils.cpp_extension import ROCM_HOME
+            includes += ['{}/hiprand/include'.format(ROCM_HOME), '{}/rocrand/include'.format(ROCM_HOME)]
+        return includes
 
     def extra_ldflags(self):
-        return ['-lcurand']
+        if not self.is_rocm_pytorch():
+            return ['-lcurand']
+        else:
+            return []
