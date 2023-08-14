8e1957782c372ee001ecd57cc61ececd028ab2ff
Olatunji Ruwase <olruwase@microsoft.com>
2023-08-14 09:04:50 -0400

    Use super() __exit__

--- deepspeed/inference/quantization/quantization_context.py
+++ deepspeed/inference/quantization/quantization_context.py
@@ -11,14 +11,3 @@ class QuantizationContext(partition_parameters.Init):
 
     def __init__(self, config_dict_or_path, param_swapper: AsyncPartitionedParameterSwapper = None) -> None:
         super().__init__(config_dict_or_path=config_dict_or_path, param_swapper=param_swapper)
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        if not self.enabled:
-            return
-
-        super().remove_wrappers()
-
-        partition_parameters.zero_init_context.pop()
-
-        if exc_type is not None:
-            return False
