f19a89a769ed2d56dc95445a9da7aa988171b43e
Logan Adams <loadams@microsoft.com>
2023-08-21 09:54:20 -0700

    Accidently dropped pytestmark from other test

--- tests/unit/inference/test_inference.py
+++ tests/unit/inference/test_inference.py
@@ -425,6 +425,7 @@ class TestLowCpuMemUsage(DistributedTest):
         assert assert_fn(bs_output, ds_output)
 
 
+@pytest.mark.seq_inference
 @pytest.mark.parametrize("model_w_task", [("tiiuae/falcon-7b", "text-generation")], ids=["falcon"])
 class TestAutoTP(DistributedTest):
     world_size = 1
