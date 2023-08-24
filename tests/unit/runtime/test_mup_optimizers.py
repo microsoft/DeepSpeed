c33e45aa53331e2a062b685b8fce15da15b434e2
Logan Adams <loadams@microsoft.com>
2023-08-24 12:46:11 -0700

    Rework to match new format

--- tests/unit/runtime/test_mup_optimizers.py
+++ tests/unit/runtime/test_mup_optimizers.py
@@ -7,41 +7,47 @@ import deepspeed
 import torch
 import pytest
 
-from unit.simple_model import SimpleModel, args_from_dict, random_dataloader
+from unit.common import DistributedTest
+from unit.simple_model import SimpleModel, random_dataloader
 from mup.shape import set_base_shapes
 
 
 @pytest.mark.parametrize("optimizer, expected_opt_class", [("MuAdam", torch.optim.Adam),
-                                                           ("MuAdamW", torch.optim.AdamW), ("MuSGD", torch.optim.SGD)])
-@pytest.mark.parametrize("zero_offload", [True, False])
-def test_mup_optimizers(tmpdir, optimizer, zero_offload, expected_opt_class):
-    config_dict = {
-        "train_batch_size": 2,
-        "steps_per_print": 1,
-        "zero_allow_untested_optimizer": True,
-        "optimizer": {
-            "type": optimizer,
-            "params": {
-                "lr": 0.00015,
-            }
-        },
-        "gradient_clipping": 1.0,
-        "fp16": {
-            "enabled": True
-        },
-        "zero_optimization": {
-            "stage": 2,
-            "cpu_offload": zero_offload
-        }
-    }
-    args = args_from_dict(tmpdir, config_dict)
+                                                           ("MuAdamW", torch.optim.AdamW), ("MuSGD", torch.optim.SGD)]) # yapf: disable
+@pytest.mark.parametrize("zero_offload", [True, False]) # yapf: disable
+class TestMuPOptimizers(DistributedTest):
+    world_size = 1
+    hidden_dim = 10
+    reuse_dist_env = True
 
-    def helper(args):
-        world_size = 1
-        hidden_dim = 10
+    def test(self,
+             optimizer,
+             expected_opt_class
+             zero_offload):
+        config_dict = {
+            "train_batch_size": 2,
+            "steps_per_print": 1,
+            "zero_allow_untested_optimizer": True,
+            "optimizer": {
+                "type": optimizer,
+                "params": {
+                    "lr": 0.00015,
+                }
+            },
+            "gradient_clipping": 1.0,
+            "fp16": {
+                "enabled": True
+            },
+            "zero_optimization": {
+                "stage": 2,
+                "cpu_offload": zero_offload
+            }
+        }
         model = SimpleModel(hidden_dim)
         set_base_shapes(model, None)
-        model, _, _, _ = deepspeed.initialize(args=args, model=model, model_parameters=model.parameters())
+        model, _, _, _ = deepspeed.initialize(config=config_dict,
+                                              model=model,
+                                              model_parameters=model.parameters())
         data_loader = random_dataloader(model=model, total_samples=50, hidden_dim=hidden_dim, device=model.device)
         for n, batch in enumerate(data_loader):
             loss = model(batch[0], batch[1])
@@ -50,5 +56,3 @@ def test_mup_optimizers(tmpdir, optimizer, zero_offload, expected_opt_class):
 
         ds_optimizer = model.optimizer.optimizer
         assert isinstance(ds_optimizer, expected_opt_class)
-
-    helper(args)
