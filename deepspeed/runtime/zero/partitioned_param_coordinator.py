b55304009bda88d8f0743e9639e09dd6fcdb399b
Logan Adams <loadams@microsoft.com>
2023-08-31 11:44:08 -0700

    Trigger a failures

--- deepspeed/runtime/zero/partitioned_param_coordinator.py
+++ deepspeed/runtime/zero/partitioned_param_coordinator.py
@@ -107,6 +107,7 @@ class PartitionedParameterCoordinator:
         self.hierarchy: int = 0
         self.zero_quantized_weights = zero_quantized_weights
         self.zero_quantized_nontrainable_weights = zero_quantized_nontrainable_weights
+        self.failure = True
 
         # stream that will be used for allgather operations
         self.__allgather_stream: get_accelerator().Stream = allgather_stream
