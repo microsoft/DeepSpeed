07eba9eda7666b60b591c6a6885fd6db47028ce9
Dom <97384583+tosemml@users.noreply.github.com>
2023-08-30 20:01:42 -0700

    use dict update

--- tests/unit/modeling.py
+++ tests/unit/modeling.py
@@ -323,8 +323,7 @@ class BertConfig(object):
     def from_dict(cls, json_object):
         """Constructs a `BertConfig` from a Python dictionary of parameters."""
         config = BertConfig(vocab_size_or_config_json_file=-1)
-        for key, value in json_object.items():
-            config.__dict__[key] = value
+        config.__dict__.update(json_object)
         return config
 
     @classmethod
