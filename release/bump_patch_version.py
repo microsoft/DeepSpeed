67dffc1f3c6e8842d2a2def2a8e860ef680c6998
Logan Adams <loadams@microsoft.com>
2023-09-05 10:34:04 -0700

    Fix bump_patch_version.py to update version.txt post GH release

--- release/bump_patch_version.py
+++ release/bump_patch_version.py
@@ -5,10 +5,10 @@
 
 from packaging import version as pkg_version
 
-with open('../version.txt') as fd:
+with open('./version.txt') as fd:
     version = pkg_version.parse(fd.read())
 
-with open('../version.txt', 'w') as fd:
+with open('./version.txt', 'w') as fd:
     fd.write(f'{version.major}.{version.minor}.{version.micro + 1}\n')
 
 print(f'{version} -> {version.major}.{version.minor}.{version.micro + 1}')
