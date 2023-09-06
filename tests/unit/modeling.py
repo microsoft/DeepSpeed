3f4a08a6a136858303d72b4fdd10bfabb4c67ec9
Logan Adams <loadams@microsoft.com>
2023-09-06 11:42:57 -0700

    Formatting

--- tests/unit/modeling.py
+++ tests/unit/modeling.py
@@ -832,23 +832,24 @@ class BertPreTrainedModel(nn.Module):
                 resolved_archive_file,  # noqa: F821 # type: ignore
                 tempdir))
             with tarfile.open(resolved_archive_file, 'r:gz') as archive:  # noqa: F821 # type: ignore
+
                 def is_within_directory(directory, target):
-                    
+
                     abs_directory = os.path.abspath(directory)
                     abs_target = os.path.abspath(target)
-                
+
                     prefix = os.path.commonprefix([abs_directory, abs_target])
-                    
+
                     return prefix == abs_directory
-                
+
                 def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
-                
+
                     for member in tar.getmembers():
                         member_path = os.path.join(path, member.name)
                         if not is_within_directory(path, member_path):
                             raise Exception("Attempted Path Traversal in Tar File")
-                
-                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+
+                    tar.extractall(path, members, numeric_owner=numeric_owner)
 
                 safe_extract(archive, tempdir)
             serialization_dir = tempdir
