7a39ffbc242ad8e9bc3ca29b209df9b06e42f273
Logan Adams <loadams@microsoft.com>
2023-10-06 13:50:14 -0700

    Formatting and GH token

--- release/bump_patch_version.py
+++ release/bump_patch_version.py
@@ -8,19 +8,15 @@ from packaging import version as pkg_version
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument(
-    "--current_version",
-    type=str,
-    help="The current version being published to help set the next version.")
+parser.add_argument("--current_version",
+                    type=str,
+                    help="The current version being published to help set the next version.")
 
 args = parser.parse_args()
 
 current_version = pkg_version.parse(args.current_version)
 
 with open('./version.txt', 'w') as fd:
-    fd.write(
-        f'{current_version.major}.{current_version.minor}.{current_version.micro + 1}\n')
+    fd.write(f'{current_version.major}.{current_version.minor}.{current_version.micro + 1}\n')
 
-print(
-    f'{current_version} -> {current_version.major}.{current_version.minor}.{current_version.micro + 1}'
-)
+print(f'{current_version} -> {current_version.major}.{current_version.minor}.{current_version.micro + 1}')
