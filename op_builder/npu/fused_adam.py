620b85e76a32fed23226f0dbeec04074cd8a7e71
CurryRice233 <nmeia@qq.com>
2023-10-14 14:41:20 +0800

    Update op_builder/npu/fused_adam.py
    Co-authored-by: Hz, Ji <hzji210@gmail.com>

--- op_builder/npu/fused_adam.py
+++ op_builder/npu/fused_adam.py
@@ -16,7 +16,7 @@ class NPUFusedAdam:
     def multi_tensor_adam(chunk_size, noop_flag_buffer, tensor_lists, lr, beta1, beta2, epsilon, step, adam_w_mode,
                           bias_correction, weight_decay, *args):
         bias_correction1 = beta1 ** step
-        bias_correction2 = beta2 ** step
+        bias_correction2 = beta2**step
 
         # iteration group['params']
         for i in range(len(tensor_lists[0])):
