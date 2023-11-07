f65a85748b46938ac068cb49f44467de49df7ae5
Logan Adams <loadams@microsoft.com>
2023-11-06 16:17:52 -0800

    Set this flag for CUDAOpBuilder too

--- op_builder/builder.py
+++ op_builder/builder.py
@@ -597,12 +597,7 @@ class CUDAOpBuilder(OpBuilder):
         return super().is_compatible(verbose)
 
     def builder(self):
-        try:
-            if not self.is_rocm_pytorch():
-                assert_no_cuda_mismatch(self.name)
-            self.build_for_cpu = False
-        except BaseException:
-            self.build_for_cpu = True
+        self.build_for_cpu = not torch.cuda.is_available()
 
         if self.build_for_cpu:
             from torch.utils.cpp_extension import CppExtension as ExtensionBuilder
