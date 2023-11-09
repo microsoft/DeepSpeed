b746e3cee7ff67c777ada4840969b60cf21375ef
Lev Kurilenko <lekurile@microsoft.com>
2023-11-09 22:23:44 +0000

    Fix syntax

--- download_sd_model.py
+++ download_sd_model.py
@@ -2,6 +2,6 @@ import torch
 from diffusers import DiffusionPipeline
 
 model = "prompthero/midjourney-v4-diffusion"
-print("Started downloading SD model"
+print("Started downloading SD model")
 pipe = DiffusionPipeline.from_pretrained(model, torch_dtype=torch.half)
-print("Finsihed downloading SD model"
+print("Finsihed downloading SD model")
