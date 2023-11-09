10cc2431f9dbabc9a1e1cff0048650fa662fe12d
Lev Kurilenko <lekurile@microsoft.com>
2023-11-09 22:21:57 +0000

    Add model download script

---
+++ download_sd_model.py
@@ -0,0 +1,7 @@
+import torch
+from diffusers import DiffusionPipeline
+
+model = "prompthero/midjourney-v4-diffusion"
+print("Started downloading SD model"
+pipe = DiffusionPipeline.from_pretrained(model, torch_dtype=torch.half)
+print("Finsihed downloading SD model"
