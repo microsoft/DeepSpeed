4460de9f51820e539d55bfca5e1f70fb5c71ce24
pomchi <ro12847@gmail.com>
2023-11-15 11:22:49 +0800

    new argument prioritize_safetensors; no more duplicate downloads

--- deepspeed/inference/v2/checkpoint/huggingface_engine.py
+++ deepspeed/inference/v2/checkpoint/huggingface_engine.py
@@ -14,7 +14,7 @@ from ..logging import inference_logger
 
 class HuggingFaceCheckpointEngine(CheckpointEngineBase):
 
-    def __init__(self, model_name_or_path: str, auth_token: str = None) -> None:
+    def __init__(self, model_name_or_path: str, auth_token: str = None, prioritize_safetensors: bool = True) -> None:
         super().__init__()
         from transformers import AutoConfig, GenerationConfig
 
@@ -28,9 +28,9 @@ class HuggingFaceCheckpointEngine(CheckpointEngineBase):
         else:
             self.model_config.max_seq_length = self.generation_config.max_length
 
-        self._all_ckpt_paths = self._fetch_checkpoint_files()
+        self._all_ckpt_paths = self._fetch_checkpoint_files(prioritize_safetensors)
 
-    def _fetch_checkpoint_files(self):
+    def _fetch_checkpoint_files(self, prioritize_safetensors):
         """
         Fetch the checkpoint files from the HuggingFace Hub.
         """
@@ -42,44 +42,84 @@ class HuggingFaceCheckpointEngine(CheckpointEngineBase):
         # checkpoint files that may be present. Example of all files in the llama-2-7b
         # repo here: https://huggingface.co/meta-llama/Llama-2-7b-hf/tree/main
 
-        # UPDATE: allow safetensors now
+        # UPDATE: allow safetensors now. Will auto fetch `*.safetensors` if there's no `*.bin`;
+        # vice versa.
         from huggingface_hub import snapshot_download
+
+        def snapshot_download_bin():
+            return snapshot_download(self.model_name_or_path,
+                                                            allow_patterns=[
+                                                                "*.bin",
+                                                                "*.json",
+                                                                "*.pt",
+                                                            ],
+                                                            revision=None,
+                                                            token=self.auth_token)
+
+        def snapshot_download_safetensors():
+            return snapshot_download(self.model_name_or_path,
+                                                            allow_patterns=[
+                                                                "*.json",
+                                                                "*.pt",
+                                                                "*.safetensors",
+                                                            ],
+                                                            revision=None,
+                                                            token=self.auth_token)
 
         if os.path.isdir(self.model_name_or_path):
             self._local_checkpoint_dir = self.model_name_or_path
         else:
-            self._local_checkpoint_dir = snapshot_download(self.model_name_or_path,
-                                                           allow_patterns=[
-                                                               "*.bin",
-                                                               "*.json",
-                                                               "*.pt",
-                                                               "*.safetensors",
-                                                           ],
-                                                           revision=None,
-                                                           token=self.auth_token)
+            if not prioritize_safetensors:
+                self._local_checkpoint_dir = snapshot_download_bin()
+            else:
+                self._local_checkpoint_dir = snapshot_download_safetensors()
 
         assert os.path.isdir(
             self._local_checkpoint_dir
         ), f"Checkpoint dir {self._local_checkpoint_dir} is not a directory, cannot load checkpoint."
 
-        # Prioritize .safetensors over .bin
+        model_param_json = os.path.join(self._local_checkpoint_dir, "pytorch_model.bin.index.json")
+        model_file = os.path.join(self._local_checkpoint_dir, 'pytorch_model.bin')
         safe_model_param_json = os.path.join(self._local_checkpoint_dir, "model.safetensors.index.json")
+        safe_model_file = os.path.join(self._local_checkpoint_dir, 'model.safetensors')
 
-        if os.path.isfile(safe_model_param_json):
-            param_map = json.load(open(safe_model_param_json, "r"))
-        else:
-            model_param_json = os.path.join(self._local_checkpoint_dir, "pytorch_model.bin.index.json")
+        # Prioritize `*.bin` over `*.safetensors`
+        if not prioritize_safetensors:
             if os.path.isfile(model_param_json):
                 param_map = json.load(open(model_param_json, "r"))
             else:
-                # We don't need any json as all such HF models will have one model file
-                safe_model_file = os.path.join(self._local_checkpoint_dir, 'model.safetensors')
+                if os.path.isfile(model_file):
+                    all_checkpoint_files = [model_file]
+
+                    return all_checkpoint_files
+                else:
+                    snapshot_download_safetensors()
+
+                    if os.path.isfile(safe_model_param_json):
+                        param_map = json.load(open(safe_model_param_json, "r"))
+                    else:
+                        all_checkpoint_files = [safe_model_file]
+
+                        return all_checkpoint_files
+
+        # Prioritize `*.safetensors` over `*.bin`
+        else:
+            if os.path.isfile(safe_model_param_json):
+                param_map = json.load(open(safe_model_param_json, "r"))
+            else:
                 if os.path.isfile(safe_model_file):
                     all_checkpoint_files = [safe_model_file]
-                else:
-                    all_checkpoint_files = [os.path.join(self._local_checkpoint_dir, 'pytorch_model.bin')]
 
-                return all_checkpoint_files
+                    return all_checkpoint_files
+                else:
+                    snapshot_download_bin()
+
+                    if os.path.isfile(model_param_json):
+                        param_map = json.load(open(model_param_json, "r"))
+                    else:
+                        all_checkpoint_files = [model_file]
+
+                        return all_checkpoint_files
 
         # weight_map -> { "lm_head.weight": "pytorch_model-00002-of-00002.bin", ... }
         weight_map = param_map["weight_map"]
