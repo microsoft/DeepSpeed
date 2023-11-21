75f81ca98c375df58a3561bf9b21949da4f52b16
Reza Yazdani <reyazda@microsoft.com>
2023-11-20 17:44:48 -0800

    refactor layout

--- deepspeed/comm/layout.py
+++ deepspeed/comm/layout.py
@@ -1,26 +1,37 @@
 
 import numpy as np
+from dataclasses import dataclass
+from enum import Enum
 # This class creates the layout for all the devices used for the training system;
 # It gets the world-size, together wit all the different parallelism degree (tp, pp, ep, sp, and dp)
 # Then it creates a multi-dimensional list based on the order of the parallelism provided
 # We use this list later to create the process groups for each of the paralllel ranks
+# the order specifies from outer to inner parallel dimension
+
+class DimName(Enum):
+    TP = 't'
+    PP = 'p'
+    EP = 'e'
+    DP = 'd'
+    SP = 's'
+
+@dataclass
+class Dim_p:
+    tag: str
+    size: int
 
 class Layout:
-    def __init__(self, world_size,
-                 tp_size=1,
-                 pp_size=1,
-                 ep_size=1,
-                 dp_size=1,
-                 sp_size=1,
-                 order="despt"):
+    def __init__(self, 
+                 world_size: int,
+                 parallel_dims: tuple(Dim_p)):
         self.world_size_ = world_size
         ranks = np.arange(0, world_size)
+        
+        layout_order = ''.join([dim.tag for dim in parallel_dims])
+        self.layout_ = np.reshape(ranks, [dim.size for dim in parallel_dims])
 
-        initial_layout = np.reshape(ranks, [dp_size, ep_size, sp_size, pp_size, tp_size])
-
-        self.layout_ = np.einsum(f"despt->{order}", initial_layout)
         shape_list = self.layout_.shape
-        self.layout_order_ = order
+        self.layout_order_ = layout_order
         self.parallel_keys_ = [order[i] for i in range(len(order))]
         self.parallel_map_ = dict(zip(self.parallel_keys_, shape_list))
 
@@ -34,18 +45,6 @@ class Layout:
         neighbors = neighbors.reshape(paralle_size, all_rest_dim)
         return [neighbors[:,i].tolist() for i in range(all_rest_dim)]
 
-    def get_tp_ranks(self):
-        return self.get_neighbors('t')
+    def get_ranks_for_parallel_dim(self, dim: Dim):
+        return self.get_neighbors(dim.value)
     
-    def get_sp_ranks(self):
-        return self.get_neighbors('s')
-        
-    def get_dp_ranks(self):
-        return self.get_neighbors('d')
-        
-    def get_pp_ranks(self):
-        return self.get_neighbors('p')
-
-    def get_ep_ranks(self):
-        return self.get_neighbors('e')
-
