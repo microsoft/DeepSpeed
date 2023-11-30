95e7e6b27c2dfba88a39aa97026e20d4ca0f7efc
Lev Kurilenko <lekurile@microsoft.com>
2023-11-30 01:19:49 +0000

    Add MII pipeline to human eval

--- human_eval_script/run_human_eval_inf_v2.py
+++ human_eval_script/run_human_eval_inf_v2.py
@@ -1,30 +1,33 @@
 import os
 import torch
-from transformers import pipeline
+#from transformers import pipeline
+import mii
 from human_eval.data import write_jsonl, read_problems
 from deepspeed.accelerator import get_accelerator
 
-#pipe = pipeline(model="facebook/opt-6.7b", torch_dtype=torch.bfloat16, device_map="auto", max_length=252)
-
-local_rank = os.getenv("LOCAL_RANK", "0")
-#device = torch.device(get_accelerator().device_name(local_rank))
 
 print("Initializing Pipeline")
-pipe = pipeline(model="EleutherAI/gpt-j-6b", device=torch.device(get_accelerator().device_name(local_rank)), max_length=252)
+#pipe = pipeline(model="facebook/opt-6.7b", torch_dtype=torch.bfloat16, device_map="auto", max_length=256)
+pipe = mii.pipeline("facebook/opt-6.7b")
+
+#local_rank = os.getenv("LOCAL_RANK", "0")
+#device = torch.device(get_accelerator().device_name(local_rank))
+#pipe = pipeline(model="EleutherAI/gpt-j-6b", device=torch.device(get_accelerator().device_name(local_rank)), max_length=256)
 
 def generate_one_completion(problem_prompt: str) -> str:
-    #import pdb; pdb.set_trace()
-    #print(f"Generating code for: {problem_prompt}")
     #output = pipe(problem_prompt, do_sample=True)
-    #import pdb; pdb.set_trace()
-    return pipe(problem_prompt, do_sample=True)[0]["generated_text"]
+    #return pipe(problem_prompt, do_sample=True)[0]["generated_text"]
+
+    #output = pipe(problem_prompt, max_new_tokens=256)
+    return pipe(problem_prompt, max_new_tokens=256).generated_texts[0]
 
 print("Loading Problems")
-problems = read_problems("HumanEvalTest.jsonl.gz")
+#problems = read_problems("HumanEvalTest.jsonl.gz")
+problems = read_problems()
 #import pdb; pdb.set_trace()
 
-#num_samples_per_task = 200
-num_samples_per_task = 100
+num_samples_per_task = 200
+#num_samples_per_task = 20
 print("Generating Samples")
 samples = [
     dict(task_id=task_id, completion=generate_one_completion(problems[task_id]["prompt"]))
