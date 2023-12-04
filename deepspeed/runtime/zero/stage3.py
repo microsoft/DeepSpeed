25470abf5504682740aa946407603d3baee3be21
hablb <lbachar@habana.ai>
2023-12-04 17:56:13 +0200

    Update deepspeed/runtime/zero/stage3.py
    Co-authored-by: Olatunji Ruwase <olruwase@microsoft.com>

--- deepspeed/runtime/zero/stage3.py
+++ deepspeed/runtime/zero/stage3.py
@@ -1214,7 +1214,8 @@ class DeepSpeedZeroOptimizer_Stage3(ZeROOptimizer):
 
     @instrument_w_nvtx
     def __avg_scatter_contiguous_grads(self, buffer_to_reduce: Tensor) -> List[Tensor]:
-        if self.communication_data_type != self.dtype:
+        dtype = buffer_to_reduce.dtype
+        if self.communication_data_type != dtype:
             buffer_to_reduce = buffer_to_reduce.to(self.communication_data_type)
         if self.postscale_gradients and self.gradient_predivide_factor != 1.0:
             buffer_to_reduce = buffer_to_reduce.div_(self.gradient_predivide_factor)
