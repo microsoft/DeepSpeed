10c23277e5d564071cc6f9c295ae1b57840315c0
Wang, Yi A <yi.a.wang@intel.com>
2023-12-08 01:48:13 -0800

    fix autoTP issue for mpt (trust_remote_code=True)
    Signed-off-by: Wang, Yi A <yi.a.wang@intel.com>

--- deepspeed/module_inject/auto_tp.py
+++ deepspeed/module_inject/auto_tp.py
@@ -385,7 +385,8 @@ class AutoTP():
             return
         for param in [
                 "n_heads", "inner_dim", "num_heads", "num_kv", "num_attention_heads", "num_attn_heads",
-                "all_head_size", "embed_dim", "hidden_size", "num_key_value_heads", "num_kv_heads"
+                "all_head_size", "embed_dim", "hidden_size", "num_key_value_heads", "num_kv_heads", "kv_n_heads",
+                "d_model"
         ]:
             if hasattr(child, param):
                 param_val = getattr(child, param)
