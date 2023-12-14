509796a0f21ec0b3c5538a4735c8155aaf9fdde7
Shiyang Chen <csycfl@gmail.com>
2023-12-14 01:01:57 +0000

    fix

--- deepspeed/inference/v2/kernels/core_ops/core_ops.cpp
+++ deepspeed/inference/v2/kernels/core_ops/core_ops.cpp
@@ -14,18 +14,6 @@
 #include "rms_norm.h"
 #include "cuda_linear_kernels.h"
 
-void cuda_wf6af16_linear(torch::Tensor& output,
-                         torch::Tensor& hidden_states,
-                         torch::Tensor& weights_4bit,
-                         torch::Tensor& weights_2bit,
-                         torch::Tensor& scale,
-                         torch::Tensor& workspace,
-                         int M,
-                         int N,
-                         int K,
-                         int split_k);
-{
-
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
 {
     // bias_activation.h
