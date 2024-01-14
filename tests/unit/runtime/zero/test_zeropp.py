6bbb2ae9b81b870c83c6b57f5fb1facae3a3b712
HeyangQin <heyangqin@microsoft.com>
2024-01-14 14:07:17 +0000

    update unit test

--- tests/unit/runtime/zero/test_zeropp.py
+++ tests/unit/runtime/zero/test_zeropp.py
@@ -14,6 +14,7 @@ import deepspeed
 from deepspeed.runtime.zero.config import DeepSpeedZeroConfig
 
 import torch.nn as nn
+import torch
 
 
 class NNModel(nn.Module):
@@ -40,9 +41,15 @@ def _assert_no_secondary_tensor_group(model: Module) -> None:
         assert param.ds_zero_param_process_group is None
 
 
-def _assert_secondary_tensor_size(model: Module) -> None:
+def _check_secondary_tensor_existence(model: Module) -> None:
     for _, param in model.named_parameters():
-        assert param.ds_secondary_tensor is not None
+        if param.ds_secondary_tensor is not None:
+            return True
+    return False
+
+def _assert_secondary_tensor_size(model: Module) -> None:
+    for name, param in model.named_parameters():
+        assert param.ds_secondary_tensor is not None, f"param {param.ds_id}:{name} does not have secondary tensor"
         assert param.ds_secondary_tensor.size()[0] % param.ds_tensor.size()[0] == 0
 
 
@@ -50,7 +57,7 @@ def _assert_secondary_tensor_size(model: Module) -> None:
 #Assert when zpg=1 that secondary group and tensors are invalid
 @pytest.mark.sequential
 @pytest.mark.parametrize("h_dim", [1024])
-@pytest.mark.parametrize("n_layers", [4, 9])
+@pytest.mark.parametrize("n_layers", [9])
 @pytest.mark.parametrize("zpg", [1, 2, 4])
 class TestZeroPPConfigSweep(DistributedTest):
     world_size = 4
@@ -89,6 +96,88 @@ class TestZeroPPConfigSweep(DistributedTest):
         for n, batch in enumerate(data_loader):
             if n == 0 and zpg != 1:
                 _assert_secondary_tensor_size(model)
+            loss = model(batch[0], batch[1])
+            model.backward(loss)
+            model.step()
+
+    def test_eval(self, h_dim: int, n_layers: int, zpg: int) -> None:
+        # in this test case, we are testing that hpz should be enabled when eval mode is on
+        config_dict = {
+            "train_micro_batch_size_per_gpu": 1,
+            "zero_optimization": {
+                "stage": 3,
+                "stage3_max_reuse_distance": 0,
+                "zero_hpz_partition_size": zpg,
+                "contiguous_gradients": True,
+                "overlap_comm": True,
+            },
+            "optimizer": {
+                "type": "Adam",
+                "params": {
+                    "lr": 1.
+                }
+            },
+            "fp16": {
+                "enabled": True,
+                "loss_scale": 1.,
+            }
+        }
+
+        model = NNModel(h_dim, n_layers)
+        model, _, _, _ = deepspeed.initialize(model=model, model_parameters=model.parameters(), config=config_dict)
+        data_loader = random_dataloader(model=model, total_samples=20, hidden_dim=h_dim, device=model.device)
+        dist.barrier()
+        if zpg == 1:
+            _assert_no_secondary_tensor_group(model)
+
+        for n, batch in enumerate(data_loader):
+            if zpg != 1:
+                # here we check that the hpz is enabled when the previous iteration does not update the model
+                _assert_secondary_tensor_size(model)
+            with torch.no_grad():
+                loss = model(batch[0], batch[1])
+
+
+
+    def test_gradient_accumulation(self, h_dim: int, n_layers: int, zpg: int) -> None:
+        # in this test case, we are testing that hpz should be enabled for the intermediate gradient accumulation steps
+        # In this test, we should disable loss_scale
+        config_dict = {
+            "train_micro_batch_size_per_gpu": 1,
+            "gradient_accumulation_steps": 3,
+            "zero_optimization": {
+                "stage": 3,
+                "stage3_max_reuse_distance": 0,
+                "zero_hpz_partition_size": zpg,
+                "contiguous_gradients": True,
+                "overlap_comm": True,
+            },
+            "optimizer": {
+                "type": "Adam",
+                "params": {
+                    "lr": 1.
+                }
+            },
+            "fp16": {
+                "enabled": True,
+                "loss_scale": 0.,
+            }
+        }
+
+        model = NNModel(h_dim, n_layers)
+        model, _, _, _ = deepspeed.initialize(model=model, model_parameters=model.parameters(), config=config_dict)
+        data_loader = random_dataloader(model=model, total_samples=20, hidden_dim=h_dim, device=model.device)
+        dist.barrier()
+        if zpg == 1:
+            _assert_no_secondary_tensor_group(model)
+
+        for n, batch in enumerate(data_loader):
+            if n == 0 and zpg != 1:
+                _assert_secondary_tensor_size(model)
+            # here we cannot assert that secondary tensor does not exist because the gradient is likely overflowed as we use random data
+            if n>0 and n % 3 != 0 and zpg != 1:
+                # if the previous iteration does not update the model, then the hpz should be enabled
+                assert _check_secondary_tensor_existence(model), f"n={n}"
             loss = model(batch[0], batch[1])
             model.backward(loss)
             model.step()
