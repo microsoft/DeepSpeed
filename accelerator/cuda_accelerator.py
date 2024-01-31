52e3c541122e9f6050f67664f51ed8c28af96efd
Logan Adams <loadams@microsoft.com>
2024-01-31 09:27:19 -0800

    Test changes in util.py

--- accelerator/cuda_accelerator.py
+++ accelerator/cuda_accelerator.py
@@ -195,7 +195,7 @@ class CUDA_Accelerator(DeepSpeedAccelerator):
             return False
 
     def supported_dtypes(self):
-        supported_dtypes = [torch.float, torch.half, torch.bfloat16]
+        supported_dtypes = [torch.float]
         if self.is_fp16_supported():
             supported_dtypes.append(torch.half)
         if self.is_bf16_supported():
