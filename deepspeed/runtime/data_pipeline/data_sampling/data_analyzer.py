ba65f2aa358ba8b4689906675b0cacd0960b6a20
Bruno Magalhaes <bruno.magalhaes@synthesia.io>
2024-02-12 23:12:19 +0000

    format

--- deepspeed/runtime/data_pipeline/data_sampling/data_analyzer.py
+++ deepspeed/runtime/data_pipeline/data_sampling/data_analyzer.py
@@ -88,9 +88,9 @@ class DataAnalyzer(object):
                 metric_dtypes[m_idx], metric_functions[m_idx], metric_results[m_idx]
             metric_values = metric_function(data)
             assert torch.is_tensor(metric_values) or isinstance(metric_values, np.ndarray), \
-                "metric_function must return a tensor or array"
+                    "metric_function must return a tensor or array"
             assert metric_values.dtype == metric_dtype, \
-                f"metric_function result dtype {metric_values.dtype} doesnt match metric_dtype {metric_dtype}"
+                    f"metric_function result dtype {metric_values.dtype} does not match metric_dtype {metric_dtype}"
             if isinstance(metric_values, np.ndarray):
                 metric_values = torch.from_numpy(metric_values)
 
