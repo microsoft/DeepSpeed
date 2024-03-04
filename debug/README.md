0ddbfd116ef573aa66e6dd90a3c8893214eebeb0
ZHENG, Zhen <zhengzhen.z@qq.com>
2024-03-04 12:03:55 +0000

    Remove debug files.

--- debug/README.md
+++
@@ -1,1 +0,0 @@
-The files in this directory is only for debugging of FP6 quantization kernel integration. Will not merge.
--- debug/clean.sh
+++
@@ -1,2 +0,0 @@
-rm ~/.cache/torch_extensions/py38_cu118/inference_core_ops/*.o
-rm ~/.cache/torch_extensions/py38_cu118/inference_core_ops/*.so
