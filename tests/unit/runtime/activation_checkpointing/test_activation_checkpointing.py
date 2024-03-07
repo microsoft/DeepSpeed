a317fe8b15108e8288a2595faed9d44c79d52f8c
Ma, Guokai <guokai.ma@intel.com>
2024-03-07 01:42:57 -0500

    skip more tests requires bf16

--- tests/unit/runtime/activation_checkpointing/test_activation_checkpointing.py
+++ tests/unit/runtime/activation_checkpointing/test_activation_checkpointing.py
@@ -62,6 +62,8 @@ def _match_outputs(ref, tgt):
 
 
 def _test_activation_checkpoint(module, *inputs):
+    if not get_accelerator().is_fp16_supported():
+        pytest.skip("fp16 is not supported")
     # Move to device
     module.to(get_accelerator().device_name())
 
@@ -82,6 +84,8 @@ def _test_activation_checkpoint(module, *inputs):
 
 
 def _test_activation_checkpoint_ordering(module, expected_ordering, *inputs):
+    if not get_accelerator().is_fp16_supported():
+        pytest.skip("fp16 is not supported")
     # Move to device
     module.to(get_accelerator().device_name())
 
