a436b36c6e5722e40bb44082d8ef11d6f1021fcd
Logan Adams <loadams@microsoft.com>
2024-03-15 12:17:45 -0700

    Reset common

--- tests/unit/common.py
+++ tests/unit/common.py
@@ -1,6 +1,8 @@
 # Copyright (c) Microsoft Corporation.
 # SPDX-License-Identifier: Apache-2.0
+
 # DeepSpeed Team
+
 import os
 import re
 import time
@@ -9,11 +11,13 @@ import socket
 import subprocess
 from abc import ABC, abstractmethod
 from pathlib import Path
+
 import torch
 import torch.multiprocessing as mp
 import deepspeed
 from deepspeed.accelerator import get_accelerator
 import deepspeed.comm as dist
+
 import pytest
 from _pytest.outcomes import Skipped
 from _pytest.fixtures import FixtureLookupError, FixtureFunctionMarker
@@ -39,6 +43,7 @@ def get_master_port(base_port=29500, port_range_size=1000):
     if xdist_worker_id is not None:
         # Make xdist workers use different port ranges to avoid race conditions
         base_port += port_range_size * xdist_worker_id
+
     # Select first open port in range
     port = base_port
     max_port = base_port + port_range_size
@@ -129,6 +134,7 @@ class DistributedExec(ABC):
         world_size = self.world_size
         if self.requires_cuda_env and not get_accelerator().is_available():
             pytest.skip("only supported in accelerator environments.")
+
         if isinstance(world_size, int):
             world_size = [world_size]
         for procs in world_size:
@@ -165,9 +171,11 @@ class DistributedExec(ABC):
         else:
             pool = mp.Pool(processes=num_procs)
             master_port = get_master_port()
+
         # Run the test
         args = [(local_rank, num_procs, master_port) for local_rank in range(num_procs)]
         skip_msgs_async = pool.starmap_async(self._dist_run, args)
+
         try:
             skip_msgs = skip_msgs_async.get(self.exec_timeout)
         except mp.TimeoutError:
@@ -187,6 +195,7 @@ class DistributedExec(ABC):
 
     def _launch_non_daemonic_procs(self, num_procs):
         assert not self.reuse_dist_env, "Cannot reuse distributed environment with non-daemonic processes"
+
         master_port = get_master_port()
         skip_msg = mp.Queue()  # Allows forked processes to share pytest.skip reason
         processes = []
@@ -194,6 +203,7 @@ class DistributedExec(ABC):
             p = mp.Process(target=self._dist_run, args=(local_rank, num_procs, master_port, skip_msg))
             p.start()
             processes.append(p)
+
         # Now loop and wait for a test to complete. The spin-wait here isn't a big
         # deal because the number of processes will be O(#GPUs) << O(#CPUs).
         any_done = False
@@ -204,14 +214,17 @@ class DistributedExec(ABC):
                     any_done = True
                     break
             time.sleep(.1)  # So we don't hog CPU
+
         # If we hit the timeout, then presume a test is hanged
         if not any_done:
             for p in processes:
                 p.terminate()
             pytest.exit("Test hanged, exiting", returncode=1)
+
         # Wait for all other processes to complete
         for p in processes:
             p.join(self.exec_timeout)
+
         failed = [(rank, p) for rank, p in enumerate(processes) if p.exitcode != 0]
         for rank, p in failed:
             # If it still hasn't terminated, kill it because it hung.
@@ -222,6 +235,7 @@ class DistributedExec(ABC):
                 pytest.fail(f'Worker {rank} killed by signal {-p.exitcode}', pytrace=False)
             if p.exitcode > 0:
                 pytest.fail(f'Worker {rank} exited with code {p.exitcode}', pytrace=False)
+
         if not skip_msg.empty():
             # This assumed all skip messages are the same, it may be useful to
             # add a check here to assert all exit messages are equal
@@ -233,8 +247,10 @@ class DistributedExec(ABC):
             pytest.skip(
                 f"Skipping test because not enough GPUs are available: {num_procs} required, {get_accelerator().device_count()} available"
             )
+
         # Set start method to `forkserver` (or `fork`)
         mp.set_start_method('forkserver', force=True)
+
         if self.non_daemonic_procs:
             self._launch_non_daemonic_procs(num_procs)
         else:
@@ -253,15 +269,20 @@ class DistributedExec(ABC):
                 # DeepSpeed single node launcher would also set LOCAL_SIZE accordingly
                 os.environ['LOCAL_SIZE'] = str(num_procs)
                 os.environ['WORLD_SIZE'] = str(num_procs)
+
             # turn off NCCL logging if set
             os.environ.pop('NCCL_DEBUG', None)
+
             if get_accelerator().is_available():
                 set_accelerator_visible()
+
             if get_accelerator().is_available():
                 get_accelerator().set_device(local_rank)
+
             if self.init_distributed:
                 deepspeed.init_distributed(dist_backend=self.backend)
                 dist.barrier()
+
         try:
             self.run(**self._fixture_kwargs)
         except BaseException as e:
@@ -272,6 +293,7 @@ class DistributedExec(ABC):
                     skip_msg = e.msg
             else:
                 raise e
+
         return skip_msg
 
     def _dist_destroy(self):
@@ -291,36 +313,45 @@ class DistributedFixture(DistributedExec):
     Implementation that extends @pytest.fixture to allow for distributed execution.
     This is primarily meant to be used when a test requires executing two pieces of
     code with different world sizes.
+
     There are 2 parameters that can be modified:
         - world_size: int = 2 -- the number of processes to launch
         - backend: Literal['nccl','mpi','gloo'] = 'nccl' -- which backend to use
+
     Features:
         - able to call pytest.skip() inside fixture
         - can be reused by multiple tests
         - can accept other fixtures as input
+
     Limitations:
         - cannot use @pytest.mark.parametrize
         - world_size cannot be modified after definition and only one world_size value is accepted
         - any fixtures used must also be used in the test that uses this fixture (see example below)
         - return values cannot be returned. Passing values to a DistributedTest
           object can be achieved using class_tmpdir and writing to file (see example below)
+
     Usage:
         - must implement a run(self, ...) method
         - fixture can be used by making the class name input to a test function
+
     Example:
         @pytest.fixture(params=[10,20])
         def regular_pytest_fixture(request):
             return request.param
+
         class distributed_fixture_example(DistributedFixture):
             world_size = 4
+
             def run(self, regular_pytest_fixture, class_tmpdir):
                 assert int(os.environ["WORLD_SIZE"]) == self.world_size
                 local_rank = os.environ["LOCAL_RANK"]
                 print(f"Rank {local_rank} with value {regular_pytest_fixture}")
                 with open(os.path.join(class_tmpdir, f"{local_rank}.txt"), "w") as f:
                     f.write(f"{local_rank},{regular_pytest_fixture}")
+
         class TestExample(DistributedTest):
             world_size = 1
+
             def test(self, distributed_fixture_example, regular_pytest_fixture, class_tmpdir):
                 assert int(os.environ["WORLD_SIZE"]) == self.world_size
                 for rank in range(4):
@@ -328,6 +359,7 @@ class DistributedFixture(DistributedExec):
                         assert f.read() == f"{rank},{regular_pytest_fixture}"
     """
     is_dist_fixture = True
+
     # These values are just placeholders so that pytest recognizes this as a fixture
     _pytestfixturefunction = FixtureFunctionMarker(scope="function", params=None)
     __name__ = ""
@@ -341,9 +373,11 @@ class DistributedFixture(DistributedExec):
 class DistributedTest(DistributedExec):
     """
     Implementation for running pytest with distributed execution.
+
     There are 2 parameters that can be modified:
         - world_size: Union[int,List[int]] = 2 -- the number of processes to launch
         - backend: Literal['nccl','mpi','gloo'] = 'nccl' -- which backend to use
+
     Features:
         - able to call pytest.skip() inside tests
         - works with pytest fixtures, parametrize, mark, etc.
@@ -352,23 +386,29 @@ class DistributedTest(DistributedExec):
         - world_size can be changed for individual tests using @pytest.mark.world_size(world_size)
         - class_tmpdir is a fixture that can be used to get a tmpdir shared among
           all tests (including DistributedFixture)
+
     Usage:
         - class name must start with "Test"
         - must implement one or more test*(self, ...) methods
+
     Example:
         @pytest.fixture(params=[10,20])
         def val1(request):
             return request.param
+
         @pytest.mark.fast
         @pytest.mark.parametrize("val2", [30,40])
         class TestExample(DistributedTest):
             world_size = 2
+
             @pytest.fixture(params=[50,60])
             def val3(self, request):
                 return request.param
+
             def test_1(self, val1, val2, str1="hello world"):
                 assert int(os.environ["WORLD_SIZE"]) == self.world_size
                 assert all(val1, val2, str1)
+
             @pytest.mark.world_size(1)
             @pytest.mark.parametrize("val4", [70,80])
             def test_2(self, val1, val2, val3, val4):
@@ -376,6 +416,7 @@ class DistributedTest(DistributedExec):
                 assert all(val1, val2, val3, val4)
     """
     is_dist_test = True
+
     # Temporary directory that is shared among test methods in a class
     @pytest.fixture(autouse=True, scope="class")
     def class_tmpdir(self, tmpdir_factory):
@@ -388,8 +429,10 @@ class DistributedTest(DistributedExec):
     def __call__(self, request):
         self._current_test = self._get_current_test_func(request)
         self._fixture_kwargs = self._get_fixture_kwargs(request, self._current_test)
+
         if self.requires_cuda_env and not get_accelerator().is_available():
             pytest.skip("only supported in accelerator environments.")
+
         # Catch world_size override pytest mark
         for mark in getattr(request.function, "pytestmark", []):
             if mark.name == "world_size":
@@ -397,6 +440,7 @@ class DistributedTest(DistributedExec):
                 break
         else:
             world_size = self.world_size
+
         if isinstance(world_size, int):
             world_size = [world_size]
         for procs in world_size:
