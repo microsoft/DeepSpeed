62a46154f58c3fc8d88b0026b6a9e330ccd84eaa
Masahiro Tanaka <mtanaka@microsoft.com>
2024-03-28 00:33:13 +0000

    add comple.disable to z3 function

--- deepspeed/runtime/zero/partitioned_param_coordinator.py
+++ deepspeed/runtime/zero/partitioned_param_coordinator.py
@@ -186,6 +186,7 @@ class PartitionedParameterCoordinator:
         self.__submodule_order.append(sub_module)
         self.__step_id_module_fetched_for[sub_module.id].append(self.__step_id)
 
+    @compiler.disable
     def record_parameters(self, sub_module: Module) -> None:
         """adds sub module to trace"""
         if not self.is_record_trace():
