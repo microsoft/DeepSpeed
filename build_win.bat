381601f14e8d760f982d88bcdb9f60ddd317dcd5
Logan Adams <114770087+loadams@users.noreply.github.com>
2024-04-25 14:44:39 -0700

    Update with ops not supported on Windows (#5468)

--- build_win.bat
+++ build_win.bat
@@ -1,6 +1,10 @@
 @echo off
 
 set DS_BUILD_AIO=0
+set DS_BUILD_CUTLASS_OPS=0
+set DS_BUILD_EVOFORMER_ATTN=0
+set DS_BUILD_FP_QUANTIZER=0
+set DS_BUILD_RAGGED_DEVICE_OPS=0
 set DS_BUILD_SPARSE_ATTN=0
 
 echo Administrative permissions required. Detecting permissions...
