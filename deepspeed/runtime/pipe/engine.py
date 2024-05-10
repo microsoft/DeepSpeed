29419dc69dafed1fa4052b40293a9af27751b171
fc <i4never@163.com>
2024-05-10 10:16:31 +0800

    [FIX] fix deadlock in PipeEngine._exec_recv_grads

--- deepspeed/runtime/pipe/engine.py
+++ deepspeed/runtime/pipe/engine.py
@@ -1189,9 +1189,7 @@ class PipelineEngine(DeepSpeedEngine):
                 # branches on is_grad_partitioned so we don't filter out the
                 # metadata tensor.
                 if self.is_grad_partitioned:
-                    sizes_and_dtypes = [(list(t.size()), t.dtype)
-                                        for t in outputs[:2]] + [(list(t.size()), t.dtype)
-                                                                 for t in outputs[2:] if t.is_floating_point()]
+                    sizes_and_dtypes = [(list(t.size()), t.dtype) for t in outputs[:2]]
                 else:
                     sizes_and_dtypes = [(list(t.size()), t.dtype) for t in outputs if t.is_floating_point()]
 
