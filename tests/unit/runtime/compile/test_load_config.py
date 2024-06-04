f58a2544975c2911679d29dccb1464fb18a64846
vikram singh shekhawat <vshekhawat@habana.ai>
2024-06-04 13:25:11 +0300

    Add compile backend for test_set_compiler_fn
    Fix the test_set_compiler_fn UT, current test_set_compiler_fn
     uses inductor as a backend, which is incompatible with HPU. Added backend arg to torch.compile.

--- tests/unit/runtime/compile/test_load_config.py
+++ tests/unit/runtime/compile/test_load_config.py
@@ -29,7 +29,7 @@ if deepspeed.is_compile_supported():
     def custom_compiler_fn(module: torch.nn.Module):
         global custom_compler_fn_called
         custom_compler_fn_called = True
-        return torch.compile(module)
+        return torch.compile(module, backend=get_accelerator().get_compile_backend())
 
 
 @pytest.fixture
