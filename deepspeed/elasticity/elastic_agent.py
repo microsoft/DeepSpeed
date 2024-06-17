72c6cd42ea3ecf7b9e5860fef9c1cf3969227a7e
QiaoZhennn <qiaozhenchina@gmail.com>
2024-06-17 14:42:15 -0700

    Update elastic_agent.py
    cannot import _get_socket_with_port from torch.distributed.elastic.agent.server.api

--- deepspeed/elasticity/elastic_agent.py
+++ deepspeed/elasticity/elastic_agent.py
@@ -6,7 +6,7 @@
 from torch.distributed.elastic.agent.server.local_elastic_agent import LocalElasticAgent
 from typing import Any, Dict, Optional, Tuple
 from datetime import datetime
-from torch.distributed.elastic.agent.server.api import _get_socket_with_port
+from torch.distributed.elastic.utils.distributed import get_free_port as _get_free_port
 from torch.distributed.elastic.metrics import put_metric
 from torch.distributed.elastic.agent.server.api import (
     RunResult,
@@ -48,9 +48,7 @@ class DSElasticAgent(LocalElasticAgent):
                               master_port: Optional[int],
                               local_addr: Optional[str] = None):
         if master_port is None:
-            sock = _get_socket_with_port()
-            with closing(sock):
-                master_port = sock.getsockname()[1]
+            master_port  = _get_free_port()
 
         if master_addr is None:
             # master_addr = _get_fq_hostname()
