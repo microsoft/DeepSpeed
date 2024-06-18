a037a53fb9687c1d39d6a6eaee8d0b5850340794
inkcherry <mingzhi.liu@intel.com>
2024-06-18 13:38:41 +0000

    enable o compute async

--- deepspeed/sequence/layer.py
+++ deepspeed/sequence/layer.py
@@ -10,9 +10,16 @@ from torch import Tensor
 from torch.nn import Module
 
 import deepspeed.comm as dist
+from deepspeed.accelerator import get_accelerator
 
 
-def single_all_to_all(input, scatter_idx, gather_idx, group):
+def wait_stream(stream):
+    get_accelerator().wait_stream(stream)
+    
+def print0(msg):
+    if dist.get_rank()==0:
+        print(msg)
+def single_all_to_all(input, scatter_idx, gather_idx, group, async_op=False):
     seq_world_size = dist.get_world_size(group)
     inp_shape = list(input.shape)
     inp_shape[scatter_idx] = inp_shape[scatter_idx] // seq_world_size
@@ -29,8 +36,8 @@ def single_all_to_all(input, scatter_idx, gather_idx, group):
         ).transpose(0, 1).contiguous()
 
     output = torch.empty_like(input_t)
-    dist.all_to_all_single(output, input_t, group=group)
-
+    work= dist.all_to_all_single(output, input_t, group=group, async_op=async_op)
+   
     # if scattering the seq-dim, transpose the heads back to the original dimension
     if scatter_idx < 2:
         output = output.transpose(0, 1).contiguous()
@@ -38,23 +45,45 @@ def single_all_to_all(input, scatter_idx, gather_idx, group):
     return output.reshape(
         inp_shape[: gather_idx] + \
         [inp_shape[gather_idx] * seq_world_size,] + \
-        inp_shape[gather_idx + 1:]).contiguous()
+        inp_shape[gather_idx + 1:]).contiguous() ,work
 
 
 class _SeqAllToAll(torch.autograd.Function):
 
     @staticmethod
-    def forward(ctx: Any, group: dist.ProcessGroup, input: Tensor, scatter_idx: int, gather_idx: int) -> Tensor:
+    def forward(ctx: Any, group: dist.ProcessGroup, input: Tensor, scatter_idx: int, gather_idx: int, stream=None, fwd_async=False,bwd_async=False) -> Tensor:
 
         ctx.group = group
         ctx.scatter_idx = scatter_idx
+        
         ctx.gather_idx = gather_idx
+        ctx.stream=stream
+        ctx.bwd_async=bwd_async
+        # if stream != None:
+        #     with get_accelerator().stream(stream):
+        #         res, work=single_all_to_all(input, scatter_idx, gather_idx, group,fwd_async)
+        # else:
+        #     res , work=single_all_to_all(input, scatter_idx, gather_idx, group,fwd_async)
+        res , work=single_all_to_all(input, scatter_idx, gather_idx, group,False)
+        # def single_all_to_all(input, scatter_idx, gather_idx, group, async_op=False):
 
-        return single_all_to_all(input, scatter_idx, gather_idx, group)
+        if fwd_async:
+            get_accelerator().current_stream().wait_stream(ctx.stream)
+
+        return  res 
+    
 
     @staticmethod
     def backward(ctx: Any, *grad_output: Tensor) -> Tuple[None, Tensor, None, None]:
-        return (None, _SeqAllToAll.apply(ctx.group, *grad_output, ctx.gather_idx, ctx.scatter_idx), None, None)
+        # print0("all2all o before")
+        # import pydevd  
+        # pydevd.settrace()
+        
+        #def forward(ctx: Any, group: dist.ProcessGroup, input: Tensor, scatter_idx: int, gather_idx: int, stream=None, fwd_async=False,bwd_async=False) -> Tensor:
+        q= (None, _SeqAllToAll.apply(ctx.group, *grad_output, ctx.gather_idx, ctx.scatter_idx, ctx.stream,ctx.bwd_async,False), None, None,None,None,None)
+        # print0("all2all o after")
+
+        return q
 
 
 class DistributedAttention(torch.nn.Module):
@@ -73,6 +102,7 @@ class DistributedAttention(torch.nn.Module):
         sequence_process_group: dist.ProcessGroup,
         scatter_idx: int = 2,
         gather_idx: int = 0,
+        sp_stream=None
     ) -> None:
 
         super(DistributedAttention, self).__init__()
@@ -80,7 +110,13 @@ class DistributedAttention(torch.nn.Module):
         self.spg = sequence_process_group
         self.scatter_idx = scatter_idx
         self.gather_idx = gather_idx
+        # self.q_stream=get_accelerator().Stream()
+        # self.k_stream=get_accelerator().Stream()
+        # self.v_stream=get_accelerator().Stream()
+        self.sp_stream=sp_stream
+        b=0
 
+    # query = slef.linearq(hidden)
     def forward(self, query: Tensor, key: Tensor, value: Tensor, *args: Any) -> Tensor:
         """ forward
 
@@ -96,14 +132,30 @@ class DistributedAttention(torch.nn.Module):
         # TODO Merge three alltoall calls into one
         # TODO (Reza): change the api on the megatron-deepspeed side so that we only receive all data (q,k, and v) together!
         #in shape : e.g.,  [s/p:h:]
-        query_layer = _SeqAllToAll.apply(self.spg, query, self.scatter_idx, self.gather_idx)
-        key_layer = _SeqAllToAll.apply(self.spg, key, self.scatter_idx, self.gather_idx)
-        value_layer = _SeqAllToAll.apply(self.spg, value, self.scatter_idx, self.gather_idx)
+        
+        
+        #step1   get q ,k ,v outside out this function
+        # def forward(ctx: Any, group: dist.ProcessGroup, input: Tensor, scatter_idx: int, gather_idx: int, stream=None, fwd_async=False,bwd_async=False) -> Tensor:
+
+        query_layer = _SeqAllToAll.apply(self.spg, query, self.scatter_idx, self.gather_idx) #[1,512,32,32]
+        key_layer = _SeqAllToAll.apply(self.spg, key, self.scatter_idx, self.gather_idx) #[1,512,32,32]
+        value_layer= _SeqAllToAll.apply(self.spg, value, self.scatter_idx, self.gather_idx) #[1,512,32,32]
 
         #out shape : e.g., [s:h/p:]
-        context_layer = self.local_attn(query_layer, key_layer, value_layer, *args)
+        # print(query_layer) #2,8, 2,4  sp=2 2gpus
+        #                    #
+        # print(key_layer)
+        # print(value_layer)  #seq_len 16 , sp 2 , head_dim = 4, num_heads=4, hidding=16
+        
+        context_layer = self.local_attn(query_layer, key_layer, value_layer, *args)       #[8,512,4,32]
+        bwd_o_async=False
+        if self.sp_stream is not None:
+            bwd_o_async=True
+        output = _SeqAllToAll.apply(self.spg, context_layer, self.gather_idx, self.scatter_idx,self.sp_stream,False,bwd_o_async)
 
-        output = _SeqAllToAll.apply(self.spg, context_layer, self.gather_idx, self.scatter_idx)
+        # dO=wdY        
 
         #out e.g., [s/p::h]
         return output
+
+        #o= self.dense(output)
