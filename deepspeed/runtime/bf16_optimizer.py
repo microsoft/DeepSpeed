2d5054db7cbc9d8489d53ed5e58b0f7d253393e0
Xinyu Lian <lian7@illinois.edu>
2024-06-27 03:47:51 +0900

    Universal checkpoint for zero stage 3 (#5475)
    This PR enables the universal checkpoint for zero stage 3.
    
    Notes:
    
    - The current implementation supports Data parallelism.
    - Development is ongoing for universal checkpoint Stage 3 with
    tensor-slicing model parallelism.
    - Pipeline parallelism is not supported by ZeRO Stage 3, and hence is
    not included in this universal checkpoint implementation.
    
    In this PR:
    
    - I've updated `deepspeed/checkpoint/ds_to_universal.py ` to support
    converting Zero checkpoints into Universal checkpoints.
    - I've updated `deepspeed/runtime/zero/stage3.py` to enable loading
    Universal checkpoints using the Stage 3 optimizer.
    
    ---------
    
    Co-authored-by: Olatunji Ruwase <olruwase@microsoft.com>
    Co-authored-by: Masahiro Tanaka <81312776+tohtana@users.noreply.github.com>

--- deepspeed/runtime/bf16_optimizer.py
+++ deepspeed/runtime/bf16_optimizer.py
@@ -498,7 +498,8 @@ class BF16_Optimizer(ZeROOptimizer):
                         checkpoint_folder,
                         load_optimizer_states=True,
                         load_from_fp32_weights=False,
-                        load_serial=None):
+                        load_serial=None,
+                        param_shapes=None):
         if checkpoint_folder:
             self._load_universal_checkpoint(checkpoint_folder, load_optimizer_states, load_from_fp32_weights)
         else:
