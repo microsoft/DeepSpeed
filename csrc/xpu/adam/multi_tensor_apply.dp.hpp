40af2d8221a4d253971bd63cd8645684fe71cf4e
Logan Adams <loadams@microsoft.com>
2024-06-28 12:37:38 -0700

    formatting fix

--- csrc/xpu/adam/multi_tensor_apply.dp.hpp
+++ csrc/xpu/adam/multi_tensor_apply.dp.hpp
@@ -10,8 +10,8 @@ This file is adapted from fused adam in NVIDIA/apex, commit a109f85
 
 #include <ATen/ATen.h>
 #include <ATen/AccumulateType.h>
-#include <ipex.h>
 #include <c10/xpu/XPUStream.h>
+#include <ipex.h>
 #include <sycl/sycl.hpp>
 #include "compat.h"
 
