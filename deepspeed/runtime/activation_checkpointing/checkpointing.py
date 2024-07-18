98972b42436751e6d2698ea66d07004118c4bd62
ic-synth <ivan.chelombiev@synthesia.io>
2024-07-18 15:54:51 +0200

    added condition to non-reentrant checkpointing fn

--- deepspeed/runtime/activation_checkpointing/checkpointing.py
+++ deepspeed/runtime/activation_checkpointing/checkpointing.py
@@ -965,8 +965,9 @@ def non_reentrant_checkpoint(function, *args):
 
     with torch.autograd.graph.saved_tensors_hooks(checkpoint_pack, checkpoint_unpack):
         outputs = function(*inputs_cuda)
-    for leaf_tensor in leaf_tensors:
-        leaf_tensor.register_hook(after_backward_hook)
+    if PROFILE_TIME or SYNCHRONIZE:
+        for leaf_tensor in leaf_tensors:
+            leaf_tensor.register_hook(after_backward_hook)
 
     see_memory_usage("After running forward on the layer", force=False)
 
