249c1db2fb7a98e0c7baeb1bee0548e9bc5f986b
Perry Zou <zonepg666@gmail.com>
2024-08-02 01:27:24 +0800

    Add fp16 support of Qwen1.5MoE models (A2.7B) to DeepSpeed-FastGen (#5403)
    This PR adds support for Qwen1.5MoE-A2.7B models.
    
    support for https://github.com/microsoft/DeepSpeed-MII/issues/457
    
    ### Test Code
    
    for mii pipeline:
    ```python
    import mii
    
    pipe = mii.pipeline("/data/zonepg/models/Qwen/Qwen1.5-MoE-A2.7B")
    responses = pipe("DeepSpeed is", max_new_tokens=128, do_sample=False)
    if pipe.is_rank_0:
        print(responses[0])
    ```
    for huggingface:
    ```python
    import mii
    
    from transformers import AutoModelForCausalLM, AutoTokenizer
    from transformers.generation import GenerationConfig
    import torch
    tokenizer = AutoTokenizer.from_pretrained("/data/zonepg/models/Qwen/Qwen1.5-MoE-A2.7B")
    model = AutoModelForCausalLM.from_pretrained("/data/zonepg/models/Qwen/Qwen1.5-MoE-A2.7B", device_map="auto", torch_dtype=torch.float16, trust_remote_code=True).eval()
    print(model)
    inputs = tokenizer('DeepSpeed is', return_tensors='pt')
    inputs = inputs.to(model.device)
    pred = model.generate(**inputs, max_new_tokens=128, do_sample=False, repetition_penalty=1.0)
    test = tokenizer.decode(pred.cpu()[0], skip_special_tokens=False)
    print(test)
    ```
    
    ### Qwen1.5-MoE-A2.7B
    Huggingface output with prompt "DeepSpeed is":
    ```
     a deep learning framework that is designed to accelerate the training of large-scale neural networks. It is built on top of PyTorch and provides a set of tools and techniques for optimizing the performance of deep learning models.
    
    DeepSpeed supports a variety of hardware accelerators, including GPUs, TPUs, and FPGAs, and can be used to train models on distributed systems, such as clusters of GPUs or TPUs.
    
    One of the key features of DeepSpeed is its ability to automatically parallelize the training of deep learning models across multiple GPUs or TPUs. This can significantly reduce the time required to train large models, as it allows the
    ```
    DeepSpeed-FastGen output with prompt "DeepSpeed is":
    ```
     a deep learning framework that is designed to accelerate the training of large-scale neural networks. It is built on top of PyTorch and provides a set of tools and techniques for optimizing the performance of deep learning models.
    
    DeepSpeed supports a variety of hardware accelerators, including GPUs, TPUs, and FPGAs, and can be used to train models on distributed systems, such as clusters of GPUs or TPUs.
    
    One of the key features of DeepSpeed is its ability to automatically parallelize the training of deep learning models across multiple GPUs or TPUs. This can significantly reduce the time required to train large models, as it allows the
    ```
    
    DeepSpeed-FastGen output with prompt "DeepSpeed is" with 8-way sharding:
    ```
     a deep learning framework that is designed to accelerate the training of large-scale neural networks. It is built on top of PyTorch and provides a set of tools and techniques for optimizing the performance of deep learning models.
    
    DeepSpeed supports a variety of hardware accelerators, including GPUs, TPUs, and FPGAs, and can be used to train models on distributed systems, such as clusters of GPUs or TPUs.
    
    One of the key features of DeepSpeed is its ability to automatically parallelize the training of deep learning models across multiple GPUs or TPUs. This can significantly reduce the time required to train large models, as it allows the
    ```
    
    Co-authored-by: Logan Adams <114770087+loadams@users.noreply.github.com>
    Co-authored-by: Heyang Qin <heyangqin@microsoft.com>
    Co-authored-by: Abhishek Kulkarni <11399+adk9@users.noreply.github.com>

--- deepspeed/inference/v2/engine_factory.py
+++ deepspeed/inference/v2/engine_factory.py
@@ -23,6 +23,7 @@ from .model_implementations import (
     Phi3Policy,
     QwenPolicy,
     Qwen2Policy,
+    Qwen2MoePolicy,
 )
 from .model_implementations.inference_policy_base import POLICIES, InferenceV2Policy
 from .model_implementations.flat_model_helpers import make_metadata_filename, ModelMetadata
@@ -126,6 +127,8 @@ def build_hf_engine(path: str,
             policy = QwenPolicy(model_config, checkpoint_engine=checkpoint_engine)
         elif model_config.model_type == "qwen2":
             policy = Qwen2Policy(model_config, checkpoint_engine=checkpoint_engine)
+        elif model_config.model_type == "qwen2_moe":
+            policy = Qwen2MoePolicy(model_config, checkpoint_engine=checkpoint_engine)
         else:
             raise ValueError(f"Unsupported model type {model_config.model_type}")
 
