1d9aeaf69ccd07c0e94654eedec3fe55e58c77e3
Omar Elayan <oelayan@habana.ai>
2024-09-18 10:04:20 +0300

    revert workspace changes

--- deepspeed/model_implementations/transformers/ds_llama2.py
+++ deepspeed/model_implementations/transformers/ds_llama2.py
@@ -4,7 +4,10 @@
 # DeepSpeed Team
 
 import torch
+from deepspeed import comm as dist
 from deepspeed.model_implementations.transformers.ds_transformer import DeepSpeedTransformerInference
+
+inference_module = None
 
 
 class DeepSpeedLlama2Inference(DeepSpeedTransformerInference):
@@ -24,9 +27,17 @@ class DeepSpeedLlama2Inference(DeepSpeedTransformerInference):
 
         input = args[0]
         input_mask = None
-        get_present = True
+        # Allocate memory only on first layer forward
+        if self.config.layer_id == 0 and self._alloc_workspace:
+            self.allocate_workspace(self.config.hidden_size, self.config.heads,
+                                    input.size()[1],
+                                    input.size()[0], DeepSpeedTransformerInference.layer_id, self.config.mp_size,
+                                    self.config.bigscience_bloom,
+                                    dist.get_rank() if dist.is_initialized() else 0, self.config.max_out_tokens,
+                                    self.config.min_out_tokens)
+            self._alloc_workspace = False
 
-        self.allocate_workspace(input.size())
+        get_present = True
 
         # We set the prev key/value to None when there is a prompt
         if input.shape[1] > 1:
