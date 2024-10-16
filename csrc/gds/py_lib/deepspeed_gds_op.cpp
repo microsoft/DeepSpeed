aa86469bd692b1c800a78f206e1a9c6eca0ac1dd
Ubuntu <jomayeri@microsoft.com>
2024-10-16 21:26:42 +0000

    buffer to correct device

--- csrc/gds/py_lib/deepspeed_gds_op.cpp
+++ csrc/gds/py_lib/deepspeed_gds_op.cpp
@@ -58,7 +58,6 @@ void gds_op_desc_t::add_buffer_to_registry(const torch::Tensor& buffer)
     const int64_t device = buffer.get_device();
     void* reg_ptr = buffer.data_ptr();
 
-    // std::cout << "REG PTR " <<  reg_ptr << std::endl;
     // TODO: add checking to make sure pointer isn't already in set
     const auto it = base_ptr_registry.find(device);
     if (it == base_ptr_registry.end()) {
