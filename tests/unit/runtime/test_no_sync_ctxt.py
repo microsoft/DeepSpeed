f6237c91d3724fb8658ebd2d935c38f5cf499cf0
Olatunji Ruwase <olruwase@microsoft.com>
2024-10-28 12:46:06 -0400

    Format fixes

--- tests/unit/runtime/test_no_sync_ctxt.py
+++ tests/unit/runtime/test_no_sync_ctxt.py
@@ -12,8 +12,9 @@ import deepspeed
 from unit.common import DistributedTest
 from contextlib import nullcontext
 
+
 class TestNoSyncCtxt(DistributedTest):
-    world_size = 2 
+    world_size = 2
 
     @pytest.mark.parametrize("dtype", [torch.float16, torch.bfloat16, torch.float32])
     @pytest.mark.parametrize("zero_stage", [0, 1, 2, 3])
@@ -43,16 +44,20 @@ class TestNoSyncCtxt(DistributedTest):
         total_samples = 32
         model = SimpleModel(hidden_dim)
         model, _, _, _ = deepspeed.initialize(model=model, model_parameters=model.parameters(), config=config_dict)
-        data_loader = random_dataloader(model=model, total_samples=total_samples, hidden_dim=hidden_dim, device=model.device, dtype=dtype)
+        data_loader = random_dataloader(model=model,
+                                        total_samples=total_samples,
+                                        hidden_dim=hidden_dim,
+                                        device=model.device,
+                                        dtype=dtype)
         dist.barrier()
 
-        with pytest.raises(AssertionError) if invalid_cfg else nullcontext() as assertinfo: 
+        with pytest.raises(AssertionError) if invalid_cfg else nullcontext() as assertinfo:
             with model.no_sync():
                 for _, batch in enumerate(data_loader):
                     loss = model(batch[0], batch[1])
-                    model.backward(loss)          
+                    model.backward(loss)
         if invalid_cfg:
-            assert("no_sync context manager is incompatible" in str(assertinfo))
+            assert ("no_sync context manager is incompatible" in str(assertinfo))
 
     @pytest.mark.parametrize("dtype", [torch.float16, torch.bfloat16, torch.float32])
     @pytest.mark.parametrize("zero_stage", [0, 1])
@@ -81,16 +86,20 @@ class TestNoSyncCtxt(DistributedTest):
         total_samples = 32
         model = SimpleModel(hidden_dim)
         model, _, _, _ = deepspeed.initialize(model=model, model_parameters=model.parameters(), config=config_dict)
-        data_loader = random_dataloader(model=model, total_samples=total_samples, hidden_dim=hidden_dim, device=model.device, dtype=dtype)
+        data_loader = random_dataloader(model=model,
+                                        total_samples=total_samples,
+                                        hidden_dim=hidden_dim,
+                                        device=model.device,
+                                        dtype=dtype)
         dist.barrier()
 
         with model.no_sync():
             for _, batch in enumerate(data_loader):
                 loss = model(batch[0], batch[1])
                 model.backward(loss)
-                with pytest.raises(AssertionError) as assertinfo: 
+                with pytest.raises(AssertionError) as assertinfo:
                     model.step()
-                assert("It is illegal to call Engine.step() inside no_sync context manager" in str(assertinfo))
+                assert ("It is illegal to call Engine.step() inside no_sync context manager" in str(assertinfo))
 
     def test_reentry(self):
         config_dict = {
@@ -114,7 +123,7 @@ class TestNoSyncCtxt(DistributedTest):
         dist.barrier()
 
         with model.no_sync():
-            with pytest.raises(AssertionError) as assertinfo: 
+            with pytest.raises(AssertionError) as assertinfo:
                 with model.no_sync():
-                    pass 
-            assert("no_sync context manager reentry is unsupported" in str(assertinfo))
+                    pass
+            assert ("no_sync context manager reentry is unsupported" in str(assertinfo))
