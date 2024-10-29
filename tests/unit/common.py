44fb6fe26e3138ba62bfcb967bf65c8083d21ffc
Masahiro Tanaka <mtanaka@microsoft.com>
2024-10-29 16:03:05 +0000

    stop using init_process_group_exclusively

--- tests/unit/common.py
+++ tests/unit/common.py
@@ -491,6 +491,14 @@ class DistributedExec(ABC):
                     set_accelerator_visible()
 
             if self.init_distributed:
+                from datetime import timedelta
+                deepspeed.init_distributed(dist_backend=self.backend,
+                                           init_method=init_method,
+                                           rank=local_rank,
+                                           world_size=num_procs,
+                                           timeout=timedelta(seconds=60))
+                dist.broadcast(torch.tensor([0], device=get_accelerator().current_device()), 0)
+                dist.barrier()
                 self.init_process_group_exclusively(local_rank, num_procs, init_method)
 
             current_device = get_accelerator().current_device()
