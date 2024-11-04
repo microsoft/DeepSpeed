33db9595756b8a50efea91699bc44ab2261cacd4
Logan Adams <loadams@microsoft.com>
2024-11-04 09:01:53 -0800

    Fix formatting issues

--- tests/unit/ops/transformer/inference/test_rope.py
+++ tests/unit/ops/transformer/inference/test_rope.py
@@ -10,6 +10,7 @@ from deepspeed.accelerator import get_accelerator
 if not deepspeed.ops.__compatible_ops__[InferenceBuilder.NAME]:
     pytest.skip("Inference ops are not available on this system", allow_module_level=True)
 
+
 @pytest.mark.inference_ops
 @pytest.mark.parametrize("num_heads", [64, 32, 16, 8])
 def test_rope_warp_size_alignment(num_heads):
@@ -32,7 +33,4 @@ def test_rope_warp_size_alignment(num_heads):
     inference = InferenceBuilder().load()
     # For num_heads values of 64, 32, 16, 8
     # corresponding threads_per_head (defined in apply_rotary_pos_emb.cu) values are 4, 8, 16, 32
-    inference.apply_rotary_pos_emb(query, key, rotary_dim, offset, num_heads,
-            rotate_half, rope_theta)
-
-
+    inference.apply_rotary_pos_emb(query, key, rotary_dim, offset, num_heads, rotate_half, rope_theta)
