af1402345b99adc37ef494b5eb99dd23816df712
Logan Adams <loadams@microsoft.com>
2024-11-15 12:04:33 -0800

    Pre-commit formatting

--- tests/unit/checkpoint/test_zero_optimizer.py
+++ tests/unit/checkpoint/test_zero_optimizer.py
@@ -618,8 +618,8 @@ class TestSaveTensorClone(DistributedTest):
         clone_ckpt_file = os.path.join(tmpdir, 'clone_ckpt.pt')
         torch.save(clone_state_dict, clone_ckpt_file)
 
-        compare_state_dicts(torch.load(ref_ckpt_file, weights_only=False), torch.load(clone_ckpt_file,
-                                                                                     weights_only=False))
+        compare_state_dicts(torch.load(ref_ckpt_file, weights_only=False),
+                            torch.load(clone_ckpt_file, weights_only=False))
 
 
 class TestZeRONonDistributed(DistributedTest):
