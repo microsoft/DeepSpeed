485492dbcb998532d472563e2921a0f71d57c686
Omar Elayan <oelayan@habana.ai>
2025-01-08 11:22:13 +0200

    generalize deepspeed linear and implement it for non cuda systems

--- op_builder/fp_quantizer.py
+++ op_builder/fp_quantizer.py
@@ -9,6 +9,7 @@ except ImportError:
     pkg_version = None
 
 from .builder import CUDAOpBuilder, installed_cuda_version
+import torch
 
 
 class FPQuantizerBuilder(CUDAOpBuilder):
@@ -98,3 +99,19 @@ class FPQuantizerBuilder(CUDAOpBuilder):
 
     def include_paths(self):
         return ['csrc/fp_quantizer/includes', 'csrc/includes']
+
+    @staticmethod
+    def get_default_quant_dtype():
+        return torch.uint8
+
+    @staticmethod
+    def get_quant_range(q_bits=None):
+        if q_bits == 8:
+            return 480
+        elif q_bits == 6:
+            return 28.
+        elif q_bits == 12:
+            return 510.
+        else:
+            assert (0), \
+                "Please specify the right quantization range for the selected precision!"
