6628127a379e3c3fd70e48534c4b5952c5c23a72
Logan Adams <114770087+loadams@users.noreply.github.com>
2025-01-08 10:43:06 -0800

    Update python version classifiers (#6933)
    Update python version classifiers in setup.py to reflect python versions
    currently supported.

--- setup.py
+++ setup.py
@@ -321,9 +321,9 @@ setup(name='deepspeed',
       include_package_data=True,
       scripts=scripts,
       classifiers=[
-          'Programming Language :: Python :: 3.6', 'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3.8', 'Programming Language :: Python :: 3.9',
-          'Programming Language :: Python :: 3.10'
+          'Programming Language :: Python :: 3.10', 'Programming Language :: Python :: 3.11',
+          'Programming Language :: Python :: 3.12'
       ],
       license='Apache Software License 2.0',
       ext_modules=ext_modules,
