ab21cd7c79bd2adba545676e8873d08888ad6b2a
siqi <siqi@tecorigin.com>
2025-02-08 10:38:57 +0800

    fix formatting error
    Signed-off-by: siqi <siqi@tecorigin.com>

--- op_builder/sdaa/fused_adam.py
+++ op_builder/sdaa/fused_adam.py
@@ -43,8 +43,8 @@ class SDAAFusedAdam:
     def multi_tensor_adam(chunk_size, noop_flag_buffer, tensor_lists, lr, beta1, beta2, epsilon, step, adam_w_mode,
                           bias_correction, weight_decay, *args):
         g_tensor_lis, p_tensor_lis, m_tensor_lis, v_tensor_lis = tensor_lists
-        torch.ops.sdaa.fused_adam(g_tensor_lis, p_tensor_lis, m_tensor_lis, v_tensor_lis,
-                                [], beta1, beta2, epsilon, lr, weight_decay, adam_w_mode, step, bias_correction)
+        torch.ops.sdaa.fused_adam(g_tensor_lis, p_tensor_lis, m_tensor_lis, v_tensor_lis, [], beta1, beta2, epsilon,
+                                  lr, weight_decay, adam_w_mode, step, bias_correction)
 
 
 class FusedAdamBuilder(SDAAOpBuilder):
