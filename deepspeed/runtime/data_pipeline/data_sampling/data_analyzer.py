0f68839536c8dfedacb58bf93afdb713a7354f41
Bruno Magalhaes <bruno.magalhaes@synthesia.io>
2025-02-09 16:14:15 +0000

    added destroy_process_group and barrier to end of run script

--- deepspeed/runtime/data_pipeline/data_sampling/data_analyzer.py
+++ deepspeed/runtime/data_pipeline/data_sampling/data_analyzer.py
@@ -862,15 +862,20 @@ def test_compare_both_data_analyzers(dataset):
         for path in output_paths:
             with open(os.path.join(da.save_path, path), 'rb') as f1, \
                 open(os.path.join(dda.save_path, path), 'rb') as f2:
-                if f1.read() != f2.read():
+                # if files have suffix .bin, they should be identical
+                if path.endswith(".bin"):
+                    assert f1.read() == f2.read(), f"files {path} are not identical."
+                elif f1.read() != f2.read():
                     print(f"files {path} are not identical.")
+    dist.barrier()
+    dist.destroy_process_group()
 
 
 if __name__ == "__main__":
 
     class TestDataset(torch.utils.data.Dataset):
 
-        def __init__(self, size=10_000_000):
+        def __init__(self, size=10_000):
             self.values = [(x + 7) % 10_000 for x in range(size)]
             self.size = size
 
