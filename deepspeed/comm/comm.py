549e11d2cf9164c3b4604db7ca5ed59a5d4039a1
Roman Fitzjalen <romaactor@gmail.com>
2025-02-12 19:17:23 +0100

    fix hostname -I for macOS #6497 (#6990)
    BUGFIX for Apple Silicon hostname
    https://github.com/microsoft/DeepSpeed/issues/6497
    
    ---------
    
    Signed-off-by: Fabien Dupont <fdupont@redhat.com>
    Signed-off-by: Olatunji Ruwase <olruwase@microsoft.com>
    Signed-off-by: Logan Adams <loadams@microsoft.com>
    Signed-off-by: inkcherry <mingzhi.liu@intel.com>
    Signed-off-by: Roman Fitzjalen <romaactor@gmail.com>
    Co-authored-by: Logan Adams <114770087+loadams@users.noreply.github.com>
    Co-authored-by: Fabien Dupont <fabiendupont@fabiendupont.fr>
    Co-authored-by: Olatunji Ruwase <olruwase@microsoft.com>
    Co-authored-by: Liangliang Ma <1906710196@qq.com>
    Co-authored-by: inkcherry <mingzhi.liu@intel.com>

--- deepspeed/comm/comm.py
+++ deepspeed/comm/comm.py
@@ -704,9 +704,13 @@ def mpi_discovery(distributed_port=TORCH_DISTRIBUTED_DEFAULT_PORT, verbose=True)
     master_addr = None
     if rank == 0:
         import shlex
-        hostname_cmd = shlex.split("hostname -I")
-        result = subprocess.check_output(hostname_cmd)
-        master_addr = result.decode('utf-8').split()[0]
+        try:
+            hostname_cmd = shlex.split("hostname -I")
+            result = subprocess.check_output(hostname_cmd)
+            master_addr = result.decode('utf-8').split()[0]
+        except subprocess.CalledProcessError:  # hostname -I not available (e.g. on macOS)
+            import socket
+            master_addr = socket.gethostbyname(socket.gethostname())
     master_addr = comm.bcast(master_addr, root=0)
 
     # Determine local rank by assuming hostnames are unique
