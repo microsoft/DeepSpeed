1ffdc44f0e7c27dc8438a715d5e2e6e51da368c5
Logan Adams <loadams@microsoft.com>
2025-02-20 11:12:19 -0800

    missed not in if statement

--- deepspeed/comm/torch.py
+++ deepspeed/comm/torch.py
@@ -145,7 +145,7 @@ class TorchBackend(Backend):
 
     def init_process_group(self, backend, timeout, init_method, rank, world_size):
         if not torch.distributed.is_initialized():
-            if required_torch_version(min_version=2.4):
+            if not required_torch_version(min_version=2.4):
                 # Windows torch builds do not come with lib_uv by default.
                 # More information here: https://pytorch.org/tutorials/intermediate/TCPStore_libuv_backend.html
                 use_libuv = False if os.name == "nt" else True
